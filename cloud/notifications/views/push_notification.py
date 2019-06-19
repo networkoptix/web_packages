bc92e490c5d4d463dbf57038dee6ee38c567c5d1
Roman Barsegian <rbarsegian@networkoptix.com>
2019-06-19 02:34:52 -0700

    CLOUD-3105, CLOUD-3106: Use Basic Authentication with cloud credentials for push methods
    <==== Mercurial data ====>
    Branch: x_CLOUD-3097_push_notifications

--- cloud/notifications/views/push_notification.py
+++ cloud/notifications/views/push_notification.py
@@ -1,13 +1,35 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import exceptions
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
+from api.controllers.cloud_api import Account as Clouddb_Account
 from api.helpers.exceptions import handle_exceptions, APIRequestException, APIServiceException,\
-    api_success, ErrorCodes, get_client_ip
-from notifications.tasks import send_push_notification
+    api_success, get_client_ip, APINotAuthorisedException
+from api.models import Account
 
+from notifications.tasks import send_push_notification
 from notifications.models import PushNotification
+from notifications.serializers import NotificationSerializer
 
 import json
+
+
+class CloudBasicAuthentication(BasicAuthentication):
+    def authenticate_credentials(self, user, password, request=None):
+        try:
+            ip = get_client_ip(request)
+            clouddb_account = Clouddb_Account.get(user, password, ip)
+        except APINotAuthorisedException:
+            raise exceptions.AuthenticationFailed('Invalid email/password.')
+
+        if 'email' in clouddb_account:
+            account = Account.objects.filter(email=clouddb_account['email']).first()
+
+        request.data['clouddb_account'] = clouddb_account
+        request.data['username'] = user
+        request.data['password'] = password
+
+        return (account, None)
 
 
 @api_view(['POST'])
