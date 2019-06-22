7ccb14dd0657ce62bca83d944ff8bf6ef293442d
Roman Barsegian <rbarsegian@networkoptix.com>
2019-06-21 19:03:03 -0700

    CLOUD-3154: Remove __future__ imports
    <==== Mercurial data ====>
    Branch: x_CLOUD-3154_future_imports

--- cloud/cms/admin.py
+++ cloud/cms/admin.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 from django.contrib import admin, messages
 from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.models import Permission
