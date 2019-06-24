9b5681dec20b98a0dd206dfa063f39eee00ad64b
Nick Hartleb <nhartleb@networkoptix.com>
2019-06-24 12:57:43 -0700

    CLOUD-3153: End point for getting asset package (zip) for automation
    changed how args are used
    
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- get_zip_from_cloud.py
+++ get_zip_from_cloud.py
@@ -30,19 +30,18 @@ def download_packages(session, instance, product_ids):
 
 
 def get_cmd_args():
-    description = "How to use this script:\n" \
+    description = "This script will download zip packages for products." \
+                  "How to use this script:\n" \
                   "- python get_zip_from_cloud.py noptix@networkoptix.com password123 " \
                   "\t\t\t(Downloads all packages for products related to the default vms ProductType)\n" \
                   "- python get_zip_from_cloud.py noptix@networkoptix.com password123 --product_id=30 " \
                   "\t(Downloads a specific package base on the product_id)"
     parser = argparse.ArgumentParser("get_zip_from_cloud", description=description,
                                      formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("email",  help="User Email")
-    parser.add_argument("password", help="User Password")
+    parser.add_argument("login",  help="User's login")
+    parser.add_argument("password", help="User's Password")
 
-    parser.add_argument("-pi", "--product_id", nargs="?",
-                        help="The product id for the package you want to download. Do not use with type and name")
-    parser.add_argument("-t", "--product_type", nargs="?", default="vms",
+    parser.add_argument("-t", "--type", nargs="?", default="vms",
                         help="The type of the ProductType you are trying to get. Must use with name.")
     parser.add_argument("-n", "--name", nargs="?", default="",
                         help="The name of the ProductType you are trying to get. Must use with type")
@@ -50,25 +49,33 @@ def get_cmd_args():
     parser.add_argument("-i", "--instance", nargs="?", default=DEFAULT_INSTANCE,
                         help=f"The url of the instance that you want to download packages from. "
                         f"Default is {DEFAULT_INSTANCE}")
-    args = parser.parse_args()
 
-    return args.email, args.password, args.product_id, args.product_type, args.name, args.instance
+    parser.add_argument("-pi", "--product_id", nargs="?",
+                        help="The product id for the package you want to download.This argument "
+                             "overrides product_type type and name")
+
+    args = parser.parse_args()
+    product_types = ["cloud_portal", "vms", "integration", "other"]
+    if args.product_id is None and args.type not in product_types:
+        parser.error(f"You must either pass in a product id or use a type that is either {', '.join(product_types)}")
+    return args
 
 
 def main():
-    email, password, product_id, product_type, product_type_name, instance = get_cmd_args()
+    args = get_cmd_args()
     with requests.Session() as session:
         # Login and start session
-        session.post(f"{instance}/api/account/login", json={"email": email, "password": password})
+        session.post(f"{args.instance}/api/account/login", json={"email": args.login, "password": args.password})
 
         # Get all of the products of a specific product_type
-        if not product_id:
-            res = session.get(f"{instance}/admin/cms/get_product_ids/?type={product_type}&name={product_type_name}")
+        if not args.product_id:
+            url = f"{args.instance}/admin/cms/get_product_ids/?type={args.product_type}&name={args.product_type_name}"
+            res = session.get(url)
             product_ids = res.json()
         else:
-            product_ids = [product_id]
+            product_ids = [args.product_id]
 
-        download_packages(session, instance, product_ids)
+        download_packages(session, args.instance, product_ids)
 
 
 if __name__ == "__main__":
