82d20f833d4be0bc5dc834172967beffcfdcd54e
Tsanko Tsolov <ttsolov@networkoptix.com>
2019-07-03 12:59:37 -0700

    CLOUD-3274: Uncontrolled redirect after login
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- front_end/app/src/services/utils.service.ts
+++ front_end/app/src/services/utils.service.ts
@@ -50,4 +50,21 @@ export class NxUtilsService {
 
         return bVal ? 'Yes' : 'No';
     }
+
+    static getRelativeLocation(href) {
+        /*
+         * Parse url string to:
+         *   href,
+         *   protocol -> match[1],
+         *   host     -> match[2],
+         *   hostname -> match[3],
+         *   port     -> match[4],
+         *   pathname -> match[5],
+         *   search   -> match[6],
+         *   hash     -> match[7]
+         *
+         * */
+        const match = href.match(/^(https?:)\/\/(([^:\/?#]*)(?::([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+        return match && match[5] + match[6] + match[7];
+    }
 }
