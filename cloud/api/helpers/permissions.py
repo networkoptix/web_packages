b670c2b3b2241f2303cd9b6189e2a124843e5754
Nick Hartleb <nhartleb@networkoptix.com>
2019-07-11 17:04:50 -0700

    CLOUD-3300: Making customization visible could give a user portal manager permissions
    find can view group by name instead of perm
    
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- cloud/api/helpers/permissions.py
+++ cloud/api/helpers/permissions.py
@@ -2,14 +2,17 @@ from django.contrib.auth.models import Group, Permission
 from cms.models import UserGroupsToProductPermissions
 
 
+CAN_ACCESS_CUSTOMIZATION = "Can View"
+
+
 def make_customization_visible_to_user(cloud_portal, user):
     customization_group = UserGroupsToProductPermissions.objects.\
-        filter(product=cloud_portal, group__permissions__codename='access_customization').first()
+        filter(group__name__icontains=CAN_ACCESS_CUSTOMIZATION, product=cloud_portal).first()
 
     if customization_group:
         customization_group.group.user_set.add(user)
     else:
-        group_name = "Can View - {}".format(cloud_portal.customizations.first().name)
+        group_name = f"{CAN_ACCESS_CUSTOMIZATION} - {cloud_portal.customizations.first().name}"
         can_view_permission = Permission.objects.get(codename='access_customization')
         can_view_group = Group(name=group_name)
         can_view_group.save()
