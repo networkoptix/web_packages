244bc801c13ac3b9e4c4c503c826169256e1d770
Tsanko Tsolov <ttsolov@networkoptix.com>
2019-07-17 18:14:30 -0700

    CLOUD-3320: Firmware version is broken in supported devices page
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- front_end/webpack.dev.js
+++ front_end/webpack.dev.js
@@ -71,6 +71,10 @@ module.exports = merge(common, {
                 from: 'images',
                 to  : 'static/images'
             },
+            {
+                from: 'language_i18n.json',
+                to: '../../translations/en_US/language_i18n.json'
+            },
             // Local test for i18n *********************
             {
                 from: '../../translations/ru_RU/',
