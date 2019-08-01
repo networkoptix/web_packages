2ef1fc8f9eb5487671076561ccc843d19603d2e9
Tsanko Tsolov <ttsolov@networkoptix.com>
2019-08-01 12:25:07 -0700

    CLOUD-3402: Custom role name is not displayed on System page
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- front_end/app/scripts/controllers/system.js
+++ front_end/app/scripts/controllers/system.js
@@ -91,7 +91,7 @@ angular.module('cloudApp')
             $scope.$watch('system', function () {
                 // system.accessRole set by backend my differ by key in LANG
                 // don't show it before system.accessRoles are defined by user's permissions
-                if ($scope.system && $scope.system.accessRoles) {
+                if ($scope.system && $scope.system.accessRole) {
                     $scope.permission = L.accessRoles[$scope.system.accessRole].label || $scope.system.accessRole;
                 }
             }, true);
