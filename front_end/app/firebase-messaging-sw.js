8a6180d2c38646e69d3027bc84e99af3ff3daa29
Roman Barsegian <rbarsegian@networkoptix.com>
2019-08-12 15:32:03 -0700

    CLOUD-3425: Don't use cdn for firebase import in service worker
    <==== Mercurial data ====>
    Branch: x_CLOUD-3097_push_notifications

--- front_end/app/firebase-messaging-sw.js
+++ front_end/app/firebase-messaging-sw.js
@@ -1,9 +1,6 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// Firebase messaging service worker
 
+// Get environment config
 fetch('/api/utils/settings')
     .then(
         function (response) {
--- front_end/app/vendor.ts
+++ front_end/app/vendor.ts
@@ -11,4 +11,5 @@ import 'rxjs';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
+import * as firebase from 'firebase/app';
 import 'firebase/messaging';
