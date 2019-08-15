383f03c4376087d0a9af7a43c2ce50c253e6fb15
Nick Hartleb <nhartleb@networkoptix.com>
2019-08-15 13:31:47 -0700

    CLOUD-3448: Closing login dialog when navigating directly to /account doesn't redirect
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- front_end/app/scripts/controllers/account.js
+++ front_end/app/scripts/controllers/account.js
@@ -57,8 +57,8 @@
                     });
                     $scope.userEmail = account.getEmail();
                 }, function () {
-                    dialogs.login(true);
-                })
+                    dialogs.login(true, true);
+                });
         }
     
         if ($scope.accountMode) {
