5bfe2cdc56d1ea60f91d6ce7dfd331b4456b146f
Tsanko Tsolov <ttsolov@networkoptix.com>
2019-09-03 13:53:52 -0700

    CLOUD-3500: If user has no systems page is empty
    <==== Mercurial data ====>
    Branch: cloud_19.2

--- front_end/app/src/pages/systems/no-systems/no-systems.component.ts
+++ front_end/app/src/pages/systems/no-systems/no-systems.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit }         from '@angular/core';
-import { Location }                  from '@angular/common';
-import { ActivatedRoute }            from '@angular/router';
-import { NxConfigService }           from '../../../services/nx-config';
-import { NxLanguageProviderService } from '../../../services/nx-language-provider';
 
-import { NxPageService }        from '../../../services/page.service';
-import { NxDialogsService }     from '../../../dialogs/dialogs.service';
-import { NxSystemsService }     from '../../../services/systems.service';
-import { NxAccountService }     from '../../../services/account.service';
-import { NxUrlProtocolService } from '../../../services/url-protocol.service';
-import { NxProcessService }     from '../../../services/process.service';
+import { NxLanguageProviderService } from '../../../services/nx-language-provider';
+import { NxPageService }             from '../../../services/page.service';
 
 @Component({
     selector   : 'nx-no-systems',
@@ -18,34 +10,21 @@ import { NxProcessService }     from '../../../services/process.service';
 })
 
 export class NxNoSystemsComponent implements OnInit {
-    CONFIG: any = {};
     LANG: any = {};
 
     private setupDefaults() {
-        this.CONFIG = this.configService.getConfig();
         this.LANG = this.language.getTranslations();
 
         this.pageService.setPageTitle(this.LANG.pageTitles.systems);
     }
 
-    constructor(
-                private urlProtocol: NxUrlProtocolService,
-                private route: ActivatedRoute,
-                private configService: NxConfigService,
-                private language: NxLanguageProviderService,
+    constructor(private language: NxLanguageProviderService,
                 private pageService: NxPageService,
-                private dialogs: NxDialogsService,
-                private systemsService: NxSystemsService,
-                private accountService: NxAccountService,
-                private processService: NxProcessService,
-                private location: Location,
     ) {
-        // this.location = location;
         this.setupDefaults();
     }
 
     ngOnInit(): void {
-        this.CONFIG = this.configService.getConfig();
     }
 }
 
