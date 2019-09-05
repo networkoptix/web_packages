a659f31e9ce7c1b3673a2e39d26e382ee75f3462
Nick Hartleb <nhartleb@networkoptix.com>
2019-09-04 17:31:53 -0700

    Added helper function to login to local mysql server
    <==== Mercurial data ====>
    Branch: cloud_19.2

--- cloud_helper.sh
+++ cloud_helper.sh
@@ -47,6 +47,10 @@ function setup_db(){
     fi
 }
 
+function login_db(){
+    mysql -h 0.0.0.0 --port=3306 -uroot cloudportal
+}
+
 function setup_env(){
     printf "Setting up cloud portal locally\n\n"
     [[ ! -d "env" ]] && printf "Creating virtualenv named 'env'\n\n" && virtualenv env -p python3.7
@@ -115,6 +119,10 @@ do
             echo 'Generated files are created in ./cloud/cms'
             ;;
 
+        login_db)
+            login_db
+            ;;
+
         rebuild_frontend)
             printf "Installing cloud requirements\n\n"
             pip install -r cloud/requirements.txt
@@ -152,12 +160,13 @@ do
             stop_docker_containers
             ;;
         *)
-            echo Usage: cloud_shortcuts '[init|add_env|build_frontend|rebuild_frontend|set_cloud_instance|setup_cms|setup_db|setup_env|start_celery|start_docker|stop_docker]'
+            echo Usage: cloud_shortcuts '[init|add_env|build_frontend|login_db|rebuild_frontend|set_cloud_instance|setup_cms|setup_db|setup_env|start_celery|start_docker|stop_docker]'
             echo 'init - Does everything. Only run this once'
             echo 'add_env - Adds LOCAL_ENV to your bash profile'
             echo 'build_frontend - Builds the frontend'
             echo 'generate_cms_docs - Creates an html file for each product in cms/cms_structure.json'
-            echo 'rebuild_frontend - Rebuilds the frontend and runs readstructre and filldata commands'
+            echo 'login_db - Login to docker db'
+            echo 'rebuild_frontend - Rebuilds the frontend and runs readstructure and filldata commands'
             echo 'set_cloud_instance - Sets the cloud instance env. Usage "source ./cloud_helper.sh set_cloud_instance $instance".'
             echo 'setup_cms - Fills in the cms. Runs migrate, readstructure and filldata commands'
             echo 'setup_db - Loads local db with sql file in ~/develop/nx_vms/cloud_portal/'
