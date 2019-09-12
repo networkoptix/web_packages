989ce4681d8aa144e1ce5a9b8828a7a2ab75a314
Nick Hartleb <nhartleb@networkoptix.com>
2019-09-12 12:41:03 -0700

    CLOUD-3593: Deployment gets stuck
    Expanded one liners to improve readability
    Removed inversion for exit code.
    
    <==== Mercurial data ====>
    Branch: cloud_19.1

--- cloud/api/management/commands/healthcheck.py
+++ cloud/api/management/commands/healthcheck.py
@@ -11,8 +11,16 @@ class Command(BaseCommand):
     help = "Check if migrations are done."
 
     def handle(self, *args, **options):
+        ready = False
+        exit_code = 0
         executor = MigrationExecutor(connections[DEFAULT_DB_ALIAS])
         plan = executor.migration_plan(executor.loader.graph.leaf_nodes())
         deploymentStatus = DeploymentStatus.objects.first()
-        ready = deploymentStatus.ready if deploymentStatus else False
-        sys.exit(0 if len(plan) > 0 or not ready else 1)
+
+        if deploymentStatus:
+            ready = deploymentStatus.ready
+
+        if len(plan) > 0 or not ready:
+            exit_code = 1
+
+        sys.exit(exit_code)
