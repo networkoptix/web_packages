619cf8cea8451a1ddc1b3f9d32f7c8725bea1d6f
Nick Hartleb <nhartleb@networkoptix.com>
2019-09-20 13:44:49 -0700

    CLOUD-3641: Updating data for templates does not set the right values.
    <==== Mercurial data ====>
    Branch: cloud_19.2

--- cloud/cms/controllers/structure.py
+++ cloud/cms/controllers/structure.py
@@ -206,11 +206,15 @@ def process_zip(file_descriptor, user, asset, update_structure, update_content):
                     log_messages.append(('error', f'Template does not exist for context {context.name}'))
                     continue
 
-                # normalise json file
-                # here is the problem: we parse json as text file to extract values, which might be not the best
-                # course of action, but it lets us parse any tags, not only json-name-based
+                """
+                    1. Load file content as json so we can use it for finding the correct value for duplicate nested
+                       keys.
+                    2. Using the normalized template and content files we can verify that structure exist in both files.
+                """
+                file_json = None
                 if name.endswith('json'):
-                    file_content = json.dumps(json.loads(file_content), indent=4, separators=(',', ': '))
+                    file_json = json.loads(file_content)
+                    file_content = json.dumps(file_json, indent=4, separators=(',', ': '))
                     context_template = json.dumps(json.loads(context_template), indent=4, separators=(',', ': '))
 
                 context_template_lines = context_template.split("\n")
@@ -236,15 +240,38 @@ def process_zip(file_descriptor, user, asset, update_structure, update_content):
                     if structure.type != structure.DATA_TYPES.html:
                         template_line += "$"
 
-                    # try to parse file_content with regex
-                    result = re.search(template_line, file_content, re.MULTILINE)
-                    if not result:
+                    # 3. Get all matches for a key. We dont care about how far its nested.
+                    results = re.findall(template_line, file_content, re.MULTILINE)
+                    if not len(results):
                         log_messages.append(('warning', f'No line in file {name} for data structure {structure.name}, '
                                                         f'template: {template_line}'))
                         continue
 
+                    value = results[0]
+                    # If our context is a json and we got multiple results we need to find its true value.
+                    if file_json and len(results) > 1:
+                        # We need a temporary copy so that we can keep moving through a nested dictionary which has an
+                        # unknown depth.
+                        tmp_dict = file_json
+                        """
+                            Split up the structure name and use it as keys.
+                            1. Remove the % from the start and end.
+                            2. Split the string by . to get all of the keys.
+                            
+                            Ex: %mobile.ios.bundleIdentifier% -> ['mobile', 'ios', 'bundleIdentifier']
+                            1. %mobile.ios.bundleIdentifier% -> mobile.ios.bundleIdentifier
+                            2. mobile.ios.bundleIdentifier -> ['mobile', 'ios', 'bundleIdentifier']
+                        """
+                        keys = escape_name.strip('%').split('\\.')
+                        for key in keys:
+                            if key not in tmp_dict:
+                                break
+                            tmp_dict = tmp_dict[key]
+                        else:
+                            # If all of the keys are found then tmp_dict has our value
+                            value = tmp_dict
+
                     # if there is a value - compare it with latest draft
-                    value = result.group(1)
                     current_value = structure.find_actual_value(asset, draft=True)
                     if value == current_value:
                         continue
