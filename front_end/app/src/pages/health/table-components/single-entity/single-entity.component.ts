8ebfb3bba226d96ae338f90d0f156bed23d4ca74
Roman Barsegian <rbarsegian@networkoptix.com>
2019-10-24 19:52:36 -0700

    CLOUD-3735: Single entity view (functional)
    <==== Mercurial data ====>
    Branch: cloud_19.2_health_monitor

--- front_end/app/src/pages/health/table-components/single-entity/single-entity.component.ts
+++ front_end/app/src/pages/health/table-components/single-entity/single-entity.component.ts
@@ -1,12 +1,6 @@
-import {
-    Component, Input, Output, EventEmitter,
-    OnChanges, SimpleChanges,
-    OnInit, ViewEncapsulation, Inject, PLATFORM_ID
-}                                                         from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { NxConfigService }                                from '../../../../services/nx-config';
 import { NxUtilsService }                                 from '../../../../services/utils.service';
-import { NxUriService }                                   from '../../../../services/uri.service';
 
 @Component({
     selector     : 'nx-single-entity',
@@ -14,7 +8,7 @@ import { NxUriService }                                   from '../../../../serv
     styleUrls    : ['./single-entity.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class NxSingleEntityComponent implements OnChanges{
+export class NxSingleEntityComponent {
     @Input() params: any;
     @Input() entity: any;
 
@@ -23,9 +17,5 @@ export class NxSingleEntityComponent implements OnChanges{
     constructor(private configService: NxConfigService,
                 private utilsService: NxUtilsService) {
         this.CONFIG = this.configService.getConfig();
-    }
-
-    ngOnChanges(changes) {
-        console.log(this.entity);
     }
 }
