e84cb806187c1cf6e59d7e9128873c8d73eb7bc1
Nick Hartleb <nhartleb@networkoptix.com>
2019-10-30 19:23:56 -0700

    CLOUD-3836: Do not send x-frame-options header for  /embed/***  on cloud portal
    Removed x-frame middleware nginx handles this already
    
    <==== Mercurial data ====>
    Branch: cloud_19.2

--- cloud/cloud/settings.py
+++ cloud/cloud/settings.py
@@ -89,7 +89,6 @@ MIDDLEWARE = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'cloud.logger.CatchExceptionMiddleware',
 )
