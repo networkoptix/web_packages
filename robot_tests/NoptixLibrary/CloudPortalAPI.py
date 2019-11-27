59d62bb16355610d0013f05ae09b8a77d74a544c
Kamil Bagautdinov <kamilb@networkoptix.com>
2019-11-26 17:04:21 -0800

    CLOUD-3600: Switch to using APIs for language check and change instead of the UI.
    Custom keywords added.
    
    <==== Mercurial data ====>
    Branch: cloud_19.2

--- robot_tests/NoptixLibrary/CloudPortalAPI.py
+++ robot_tests/NoptixLibrary/CloudPortalAPI.py
@@ -36,3 +36,22 @@ class CloudPortalAPI(object):
             resp = restore_pass_session.post(env + '/api/account/restorePassword', data)
             restore_pass_session.close()
             return resp.status_code
+
+    def get_account_language(self, env, email, password):
+        get_acc_lang_session = self.log_in(env, email, password)
+        with get_acc_lang_session:
+            get_acc_lang_session.headers.update({'X-CSRFToken': get_acc_lang_session.cookies['csrftoken']})
+            resp = get_acc_lang_session.get(env + '/api/utils/language')
+            get_acc_lang_session.close()
+            return resp.json()['ajs']['language']
+
+    def set_account_language(self, env, email, password, new_language='en_US'):
+        set_acc_lang_session = self.log_in(env, email, password)
+        with set_acc_lang_session:
+            lang_data = {
+                'language': new_language,
+            }
+            set_acc_lang_session.headers.update({'X-CSRFToken': set_acc_lang_session.cookies['csrftoken']})
+            change_lang = set_acc_lang_session.post(env + '/api/utils/language', lang_data)
+            set_acc_lang_session.close()
+            return change_lang.status_code
