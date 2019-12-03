a80962b20790755a89045f1cdc830d7cc54c3fb0
Tsanko Tsolov <ttsolov@networkoptix.com>
2019-12-02 17:50:57 -0800

    CLOUD-4042: Account with no systems looks bad
    <==== Mercurial data ====>
    Branch: cloud_19.3

--- front_end/app/src/pages/systems/no-systems/no-systems.component.ts
+++ front_end/app/src/pages/systems/no-systems/no-systems.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }         from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { NxLanguageProviderService } from '../../../services/nx-language-provider';
 import { NxPageService }             from '../../../services/page.service';
@@ -6,7 +6,8 @@ import { NxPageService }             from '../../../services/page.service';
 @Component({
     selector   : 'nx-no-systems',
     templateUrl: 'no-systems.component.html',
-    styleUrls  : ['no-systems.component.scss']
+    styleUrls  : ['no-systems.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class NxNoSystemsComponent implements OnInit {
