0810adfaf70c5a8012b51e93856d25c2b4c178ab
Nick Hartleb <nhartleb@networkoptix.com>
2020-01-06 19:11:02 -0800

    CLOUD-4209: Translation extract fails on project run
    Changed the compile process so that it merges the dynamic and static i18n files.
    Removed merge-lang command and file
    
    Branch: cloud_19.3

--- front_end/compile-lang.py
+++ front_end/compile-lang.py
@@ -14,8 +14,11 @@ with open('app/language.json', 'r') as ajs_language:
 with open('app/language_i18n.json', 'r') as auto_language:
     base_file = json.load(auto_language)
 
+with open('app/language_i18n_static.json', 'r') as static_language:
+    static_file = json.load(static_language)
+
 ajs_section = {"ajs": ajs_file}
-i18n_section = {"i18n": base_file}
+i18n_section = {"i18n": merge_two_json(base_file, static_file)}
 
 with open("./app/language_compiled.json", "w") as outfile:
     json.dump(merge_two_json(ajs_section, i18n_section), outfile, indent=4, sort_keys=True, separators=(',', ': '))
