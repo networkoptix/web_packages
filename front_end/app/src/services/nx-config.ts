2f18b24f535da3f602363416e759f115c8d2c741
Nick Hartleb <nhartleb@networkoptix.com>
2020-01-21 13:56:30 -0800

    CLOUD-4303: Going into an offline system not as owner fails
    Handle first load of system in check permissions.
    
    Branch: cloud_19.3

--- front_end/app/src/services/nx-config.ts
+++ front_end/app/src/services/nx-config.ts
@@ -86,7 +86,7 @@ export class NxConfigService {
                 slave       : 'slave'
             },
             accessRoles                   : {
-                adminAccess              : ['cloudadmin', 'owner', 'administrator'],
+                adminAccess              : ['cloudAdmin', 'owner', 'administrator'],
                 unshare                  : 'none',
                 default                  : 'Viewer',
                 disabled                 : 'disabled',
--- front_end/app/src/services/system.service.ts
+++ front_end/app/src/services/system.service.ts
@@ -150,6 +150,9 @@ class UserManager {
         if (!isMine && this.currentUser) {
             permissions.editUsers = this.currentUser.permissions.indexOf(this.CONFIG.accessRoles.editUserPermissionFlag) >= 0;
             permissions.isAdmin = this.isAdmin(this.currentUser);
+        } else if (this.CONFIG.accessRoles.adminAccess.indexOf(this._accessRole) > -1) {
+            permissions.editUsers = true;
+            permissions.isAdmin = true;
         }
         this.permissions = permissions;
     }
