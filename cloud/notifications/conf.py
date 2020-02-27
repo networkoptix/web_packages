218a1a85d6768852c8365d553a59fb507bf7a3b5
Roman Barsegian <rbarsegian@networkoptix.com>
2020-02-26 18:01:45 -0800

    CLOUD-4505: Drop environment config in favor of CMS
    Branch: cloud_19.3

--- cloud/notifications/conf.py
+++ cloud/notifications/conf.py
@@ -6,12 +6,17 @@ from cms.models import Asset, AssetType
 from push_notifications.conf.legacy import LegacyConfig
 
 
+class empty(object):
+    pass
+
+
 class PushConfig(LegacyConfig):
     PUSH_CONFIG_CACHE = caches['push_config']
+    DEFAULT_SETTINGS = django_settings.PUSH_NOTIFICATIONS_SETTINGS
 
     def _get_application_settings(self, application_id, settings_key, error_message):
-        value = self._read_from_cache(application_id, settings_key)
-        if value is None:
+        value = self._read_from_cache(application_id, settings_key) or self.DEFAULT_SETTINGS.get(settings_key, empty)
+        if value is empty:
             raise ImproperlyConfigured(error_message)
         return value
 
