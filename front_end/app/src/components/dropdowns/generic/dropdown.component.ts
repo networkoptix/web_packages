0cff2d79b53f7121787f82da80767dbd1278b48f
Chris Czach <czach@networkoptix.com>
2020-03-05 12:27:24 -0800

    NX-CR-1342: WIP move cloud dialog
    Branch: cloud_20.1_cloud_storage

--- front_end/app/src/components/dropdowns/generic/dropdown.component.ts
+++ front_end/app/src/components/dropdowns/generic/dropdown.component.ts
@@ -35,7 +35,7 @@ import { BaseDropdown }      from '../injDropdown';
 export class NxGenericDropdown extends BaseDropdown {
     // items should have at least name ex [{name: 'a', id: 1}, {name: 'b', id:3}]
     @Input() id: any;
-    @Input() items: any;
+    @Input() items: DropdownItem[];
     @Input() selected: any;
     @Output() onSelected = new EventEmitter<string>();
 
@@ -46,6 +46,7 @@ export class NxGenericDropdown extends BaseDropdown {
     }
 
     ngOnChanges(changes: SimpleChanges) {
+        console.log(this.items)
         // detect changes in list of items and changes in selected to support clear option
         if (changes.selected.currentValue) {
             this._selected = changes.selected.currentValue;
@@ -55,3 +56,9 @@ export class NxGenericDropdown extends BaseDropdown {
     }
 }
 
+export class DropdownItem {
+    constructor(
+        public name: string,
+        public value?: string
+    ) {}
+};
