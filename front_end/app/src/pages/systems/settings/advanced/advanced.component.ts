74170e36173123d7625d789fcafc87ba0bf4178c
Cliff Kang <ckang@networkoptix.com>
2020-03-18 23:11:54 -0700

    CLOUD-4569: Create "Server advanced settings" in settings
    - reworked apply service
    
    Branch: cloud_20.1_adv-settings

--- front_end/app/src/pages/systems/settings/advanced/advanced.component.ts
+++ front_end/app/src/pages/systems/settings/advanced/advanced.component.ts
@@ -15,7 +15,7 @@ import { NxLanguageProviderService }  from '../../../../services/nx-language-pro
 import { NxMenuService }              from '../../../../components/menu/menu.service';
 import { NxProcessService }           from '../../../../services/process.service';
 import { NxSystem }                   from '../../../../services/system.service';
-import { NxApplyService, ObjWatcher } from '../../../../services/apply.service';
+import { NxApplyService, Watcher }    from '../../../../services/apply.service';
 import { NxUriService }               from '../../../../services/uri.service';
 import { AutoUnsubscribe }            from 'ngx-auto-unsubscribe';
 import { LanguageI18NStaticTypes }    from '../../../../../language_i18n_static_types';
@@ -35,13 +35,103 @@ export class NxSystemServerAdvancedComponent implements OnInit, OnDestroy {
     serverIdFromParams: any;
     selectedServer: any;
 
-    systemSettings: any;
+    systemSettings: any = {
+        additionalLocalFsTypes                          : new Watcher<any>(),
+        arecontRtspEnabled                              : new Watcher<boolean>(),
+        auditTrailEnabled                               : new Watcher<boolean>(),
+        auditTrailPeriodDays                            : new Watcher<number>(),
+        autoDiscoveryEnabled                            : new Watcher<boolean>(),
+        autoDiscoveryResponseEnabled                    : new Watcher<boolean>(),
+        autoUpdateThumbnails                            : new Watcher<boolean>(),
+        backupNewCamerasByDefault                       : new Watcher<boolean>(),
+        backupQualities                                 : new Watcher<string>(),
+        cameraSettingsOptimization                      : new Watcher<boolean>(),
+        clientStatisticsSettingsUrl                     : new Watcher<any>(),
+        cloudAccountName                                : new Watcher<string>(),
+        cloudConnectRelayingEnabled                     : new Watcher<boolean>(),
+        cloudConnectUdpHolePunchingEnabled              : new Watcher<boolean>(),
+        cloudHost                                       : new Watcher<string>(),
+        cloudSystemID                                   : new Watcher<string>(),
+        crossdomainEnabled                              : new Watcher<boolean>(),
+        defaultExportVideoCodec                         : new Watcher<string>(),
+        defaultVideoCodec                               : new Watcher<string>(),
+        disabledVendors                                 : new Watcher<any>(),
+        downloaderPeers                                 : new Watcher<any>(),
+        ec2AliveUpdateIntervalSec                       : new Watcher<number>(),
+        ec2ConnectionKeepAliveTimeoutSec                : new Watcher<number>(),
+        ec2KeepAliveProbeCount                          : new Watcher<number>(),
+        emailFrom                                       : new Watcher<any>(),
+        emailSignature                                  : new Watcher<any>(),
+        emailSupportEmail                               : new Watcher<string>(),
+        enableEdgeRecording                             : new Watcher<boolean>(),
+        eventLogPeriodDays                              : new Watcher<number>(),
+        forceLiveCacheForPrimaryStream                  : new Watcher<string>(),
+        lastMergeMasterId                               : new Watcher<string>(),
+        lastMergeSlaveId                                : new Watcher<string>(),
+        ldapAdminDn                                     : new Watcher<any>(),
+        ldapSearchBase                                  : new Watcher<any>(),
+        ldapSearchFilter                                : new Watcher<any>(),
+        ldapSearchTimeoutS                              : new Watcher<number>(),
+        ldapUri                                         : new Watcher<any>(),
+        licenseServer                                   : new Watcher<string>(),
+        localSystemId                                   : new Watcher<string>(),
+        lowQualityScreenVideoCodec                      : new Watcher<string>(),
+        maxDifferenceBetweenSynchronizedAndInternetTime : new Watcher<number>(),
+        maxDifferenceBetweenSynchronizedAndLocalTimeMs  : new Watcher<number>(),
+        maxEventLogRecords                              : new Watcher<number>(),
+        maxP2pAllClientsSizeBytes                       : new Watcher<number>(),
+        maxP2pQueueSizeBytes                            : new Watcher<number>(),
+        maxRecordQueueSizeBytes                         : new Watcher<number>(),
+        maxRecordQueueSizeElements                      : new Watcher<number>(),
+        maxRemoteArchiveSynchronizationThreads          : new Watcher<number>(),
+        maxRtpRetryCount                                : new Watcher<number>(),
+        maxRtspConnectDurationSeconds                   : new Watcher<number>(),
+        maxSceneItems                                   : new Watcher<number>(),
+        maxWearableArchiveSynchronizationThreads        : new Watcher<number>(),
+        maxWebMTranscoders                              : new Watcher<number>(),
+        metadataStorageChangePolicy                     : new Watcher<string>(),
+        osTimeChangeCheckPeriodMs                       : new Watcher<number>(),
+        primaryTimeServer                               : new Watcher<string>(),
+        proxyConnectTimeoutSec                          : new Watcher<number>(),
+        pushNotificationsLanguage                       : new Watcher<any>(),
+        resourceFileUri                                 : new Watcher<string>(),
+        rtpTimeoutMs                                    : new Watcher<number>(),
+        sequentialFlirOnvifSearcherEnabled              : new Watcher<boolean>(),
+        serverDiscoveryPingTimeoutSec                   : new Watcher<number>(),
+        sessionLimitMinutes                             : new Watcher<number>(),
+        smtpConnectionType                              : new Watcher<string>(),
+        smtpHost                                        : new Watcher<any>(),
+        smtpPort                                        : new Watcher<number>(),
+        smtpSimple                                      : new Watcher<boolean>(),
+        smtpTimeout                                     : new Watcher<number>(),
+        smtpUser                                        : new Watcher<any>(),
+        specificFeatures                                : new Watcher<string>(),
+        statisticsAllowed                               : new Watcher<boolean>(),
+        statisticsReportLastNumber                      : new Watcher<number>(),
+        statisticsReportLastTime                        : new Watcher<string>(),
+        statisticsReportLastVersion                     : new Watcher<string>(),
+        statisticsReportServerApi                       : new Watcher<any>(),
+        statisticsReportTimeCycle                       : new Watcher<string>(),
+        statisticsReportUpdateDelay                     : new Watcher<any>(),
+        syncTimeEpsilon                                 : new Watcher<number>(),
+        syncTimeExchangePeriod                          : new Watcher<number>(),
+        systemName                                      : new Watcher<string>(),
+        takeCameraOwnershipWithoutLock                  : new Watcher<boolean>(),
+        timeSynchronizationEnabled                      : new Watcher<boolean>(),
+        trafficEncryptionForced                         : new Watcher<boolean>(),
+        updateNotificationsEnabled                      : new Watcher<boolean>(),
+        upnpPortMappingEnabled                          : new Watcher<boolean>(),
+        useTextEmailFormat                              : new Watcher<boolean>(),
+        useWindowsEmailLineFeed                         : new Watcher<boolean>(),
+        videoTrafficEncryptionForced                    : new Watcher<boolean>(),
+        watermarkSettings                               : new Watcher<string>(),
+        webSocketEnabled                                : new Watcher<boolean>()
+    };
 
     private serverSubscription: Subscription;
     private systemSubscription: Subscription;
     private routeParamsSubscription: Subscription;
 
-    settingsWatcher: any = new ObjWatcher<Object>();
     saveSettings: any;
     previousInputValue: number;
     checking: boolean;
@@ -150,76 +240,32 @@ export class NxSystemServerAdvancedComponent implements OnInit, OnDestroy {
         this.system.updateOrGetSystemSettings({ ignore: 'installedUpdateInformation,targetUpdateInformation' })
             .toPromise()
             .then(response => {
-                this.systemSettings = Object.keys(response.reply.settings)
-                    .map((key, value) => {
-                        return {
-                            key, value: response.reply.settings[key]
-                        };
-                    });
-
-                this.systemSettings.forEach(setting => {
-                    if (!this.CONFIG.settingsConfig[setting.key]) {
+                this.applyService.setVisible(false);
+                this.applyService.hardReset();
+                Object.keys(response.reply.settings).forEach((key) => {
+                    const value = response.reply.settings[key];
+                    if (!this.CONFIG.settingsConfig[key]) {
                         let type = 'text';
-                        if (setting.value === true ||
-                            setting.value === false ||
-                            setting.value === 'true' ||
-                            setting.value === 'false') {
+                        if (value === true || value === false ||
+                            value === 'true' || value === 'false') {
                             type = 'checkbox';
                         }
-                        this.CONFIG.settingsConfig[setting.key] = { label: setting.key, type: type };
+                        this.CONFIG.settingsConfig[key] = { label: key, type: type };
                     }
 
-                    if (this.CONFIG.settingsConfig[setting.key].type === 'number') {
-                        this.systemSettings[setting.key] = parseInt(setting.value);
-                    }
-                    if (this.systemSettings[setting.key] === 'true') {
-                        this.systemSettings[setting.key] = true;
-                    }
-                    if (this.systemSettings[setting.key] === 'false') {
-                        this.systemSettings[setting.key] = false;
+                    if (this.CONFIG.settingsConfig[key].type === 'number') {
+                        this.systemSettings[key].value = parseInt(value);
+                    } else if (this.CONFIG.settingsConfig[key].type === 'checkbox') {
+                        this.systemSettings[key].value = value === 'true';
+                    } else {
+                        this.systemSettings[key].value = value;
                     }
 
-                    this.CONFIG.settingsConfig[setting.key].oldValue = setting.value;
+                    this.CONFIG.settingsConfig[key].oldValue = value;
                 });
-
-                this.setWatcherValues(this.systemSettings);
+                this.applyService.reset();
+                this.applyService.setVisible(true);
             });
-    }
-
-    setWatcherValues(settings: {}) {
-        this.applyService.setVisible(false);
-        this.applyService.hardReset();
-
-        this.settingsWatcher = [ ...this.systemSettings ];
-
-        // const sw = this.settingsWatchers;
-        // Object.keys(sw).forEach(setting => {
-        //     if (setting in settings) {
-        //         let curr = settings[setting];
-        //         /**
-        //          * sets initial values for system & security settings
-        //          * sessionLimitMinutes is the only one that's a number & not a boolean,
-        //          * so it needs custom code to handle
-        //          */
-        //         if (isNaN(curr)) {
-        //             sw[setting].value = curr === 'true';
-        //         } else {
-        //             curr = parseInt(curr);
-        //             this.sessionLimitToggle = Boolean(curr);
-        //             this.selectedTimeUnit = this.limitSessionTimeUnits.minutes;
-        //
-        //             sw[setting].value = curr;
-        //             this.timeValue = curr;
-        //             if (this.timeValue % 60 === 0) {
-        //                 this.timeValue /= 60;
-        //                 this.selectedTimeUnit = this.limitSessionTimeUnits.hours;
-        //             }
-        //         }
-        //     }
-        // });
-        // this.settingsWatchersSet = true;
-        this.applyService.reset();
-        this.applyService.setVisible(true);
     }
 
     initApplyService(): void {
@@ -256,7 +302,7 @@ export class NxSystemServerAdvancedComponent implements OnInit, OnDestroy {
             .initPageWatcher(this.viewContainerRef, this.saveSettings, () => {
                 this.applyService.reset();
             },
-            [this.settingsWatcher]);
+            Object.values(this.systemSettings));
 
         this.applyService.setVisible(false);
     }
