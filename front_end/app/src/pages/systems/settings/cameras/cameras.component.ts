4907239037252659b5b82c8fcedaa28fe42b6727
Chris Czach <czach@networkoptix.com>
2020-03-25 19:36:15 -0700

    CLOUD-4615: Fix not loading on hard refresh
    Branch: cloud_20.1_camera_settings

--- front_end/app/src/pages/systems/settings/cameras/cameras.component.ts
+++ front_end/app/src/pages/systems/settings/cameras/cameras.component.ts
@@ -1,39 +1,40 @@
 import {
-    Component, OnDestroy, OnInit, OnChanges
+    Component, OnDestroy, OnInit
 }                                    from '@angular/core';
-import { NxConfigService, IConfig }           from '../../../../services/nx-config';
+import { NxConfigService, IConfig }  from '../../../../services/nx-config';
 import { NxSettingsService }         from '../settings.service';
 import { NxLanguageProviderService } from '../../../../services/nx-language-provider';
 import { NxMenuService }             from '../../../../components/menu/menu.service';
 import { AutoUnsubscribe }           from 'ngx-auto-unsubscribe';
-import { LanguageI18NStaticTypes } from '../../../../../language_i18n_static_types';
-import { NxSystem, ICamera } from '../../../../services/system.service';
-import { BehaviorSubject, Subscription, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { ActivatedRoute, Params } from '@angular/router';
-import { NxUriService } from '../../../../services/uri.service';
+import { LanguageI18NStaticTypes }   from '../../../../../language_i18n_static_types';
+import { NxSystem, ICamera }         from '../../../../services/system.service';
+import { Subscription }              from 'rxjs';
+import { filter, map,
+    retryWhen, delay }               from 'rxjs/operators';
+import { ActivatedRoute }            from '@angular/router';
+import { NxUriService }              from '../../../../services/uri.service';
 
 @AutoUnsubscribe()
 @Component({
-    selector : 'nx-cameras-component',
+    selector    : 'nx-cameras-component',
     templateUrl : 'cameras.component.html',
-    styleUrls : ['cameras.component.scss']
+    styleUrls   : ['cameras.component.scss']
 })
 
 export class NxCamerasComponent implements OnInit, OnDestroy {
     CONFIG: IConfig;
     LANG: LanguageI18NStaticTypes;
     system: NxSystem;
-    settings$: Observable<any>;
     settingsSubscription: Subscription;
-    params$: Observable<Params>;
-    routeSubscription: Subscription;
+    routeParamsSubscription: Subscription;
+    cameraSubscription: Subscription;
     cameraIdFromParams: string;
     parsedCameraId: string;
     selectedCamera: ICamera;
-    canSeeInfo: boolean;
     fullInfoPath: string;
     cameraViewPath: string;
+
+    canSeeInfo = false;
 
     constructor(
         configService: NxConfigService,
@@ -49,44 +50,57 @@ export class NxCamerasComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
-        this.initSettingsAndSystem();
-        this.routeSubscription = this.route.params.subscribe(params => {
-            // TODO: Need to find out how to get params when someone visits page directly
-            if (params.cameraId) {
-                this.cameraIdFromParams = params.cameraId;
-                this.parsedCameraId = (params.cameraId !== 'undefined' ? params.cameraId : this.system.cameras[0].id).replace(/\s|\{|\}/g, '');
-            }
-            this.setCamera();
-        });
+        this.routeParamsSubscription = this.route
+            .params
+            .subscribe(params => {
+                if (params.cameraId) {
+                    this.menuService.setDetailsSection(params.cameraId);
+                    this.cameraIdFromParams = params.cameraId;
+                    this.parsedCameraId = params.cameraId.replace(/\s|\{|\}/g, '');
+                    this.setCamera();
+                }
+            });
+
+        this.settingsSubscription = this.settingsService.systemSubject
+            .pipe(filter(data => data !== undefined))
+            .subscribe((system) => {
+                this.settingsService.footerSubject.next(true);
+                this.system = system;
+                if (this.system) {
+                    this.system.getInfoAndPermissions(false).catch(() => {}).then(system => {
+                        this.cameraViewPath = this.CONFIG.menus.systemSettings.baseUrl + system.id + '/view/' + this.parsedCameraId;
+                        this.canSeeInfo = (this.CONFIG.cloudCapabilities.healthMonitoring || system.info.capabilities && system.info.capabilities.vms_metrics) && this.system.canViewInfo();
+                        if (this.canSeeInfo) {
+                            this.fullInfoPath = this.CONFIG.menus.systemSettings.baseUrl + system.id + this.CONFIG.menus.systemHealth.baseUrl + this.CONFIG.menus.systemSettings.cameras.path;
+                        }
+                    });
+                }
+                if (this.cameraSubscription) {
+                    this.cameraSubscription.unsubscribe();
+                }
+                this.cameraSubscription = this.system.infoSubject
+                    .pipe(
+                        map(system => {
+                            if (!system.cameras || system.cameras.length === 0) {
+                                throw system;
+                            }
+                        }),
+                        retryWhen(err => err.pipe(delay(1000)))
+                    )
+                    .subscribe(() => {
+                        if (this.system.currentServerNotBusy) {
+                            if (this.system && this.system.cameras && this.system.cameras.length) {
+                                this.system.initSystemMediaServers();
+                            }
+                            this.setCamera();
+                        }
+                    });
+            });
     }
 
     ngOnDestroy() {}
 
-    // init methods
-
-    initSettingsAndSystem() {
-        this.canSeeInfo = false;
-        if (this.settingsSubscription) {
-            this.settingsSubscription.unsubscribe();
-        }
-        this.settings$ = this.settingsService
-            .systemSubject
-            .pipe(filter((system) => system !== undefined));
-        this.settingsSubscription = this.settings$.subscribe((system) => {
-            this.system = system;
-            if (this.system) {
-                this.system.getInfoAndPermissions(false).catch(() => {}).then(system => {
-                    this.cameraViewPath = this.CONFIG.menus.systemSettings.baseUrl + system.id + '/view/' + this.parsedCameraId;
-                    this.canSeeInfo = (this.CONFIG.cloudCapabilities.healthMonitoring || system.info.capabilities && system.info.capabilities.vms_metrics) && this.system.canViewInfo();
-                    if (this.canSeeInfo) {
-                        this.fullInfoPath = this.CONFIG.menus.systemSettings.baseUrl + system.id + this.CONFIG.menus.systemHealth.baseUrl + this.CONFIG.menus.systemSettings.cameras.path;
-                    }
-                });
-            }
-        });
-    }
-
-    setCamera(): void {
+    setCamera() {
         if (this.system && this.system.cameras && this.system.cameras.length > 0) {
             let cameraIndex = this.system.cameras.findIndex(camera => camera.id === `{${this.parsedCameraId}}`);
 
