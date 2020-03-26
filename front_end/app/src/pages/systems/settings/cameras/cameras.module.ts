4748720f2bc967027b2b25b8cc2c1d17085dcf95
Chris Czach <czach@networkoptix.com>
2020-03-25 19:43:24 -0700

    CLOUD-4615: Fix spacing on imports
    Branch: cloud_20.1_camera_settings

--- front_end/app/src/pages/systems/settings/cameras/cameras.module.ts
+++ front_end/app/src/pages/systems/settings/cameras/cameras.module.ts
@@ -1,18 +1,16 @@
-import { NgModule }                          from '@angular/core';
-import { CommonModule }                      from '@angular/common';
-import { BrowserModule }                     from '@angular/platform-browser';
-import { downgradeComponent, UpgradeModule } from '@angular/upgrade/static';
-import { AngularSvgIconModule }              from 'angular-svg-icon';
-import { RouterModule, Routes }              from '@angular/router';
-import { FormsModule }                       from '@angular/forms';
-import { NgbModule }                         from '@ng-bootstrap/ng-bootstrap';
-
-import { DirectivesModule }       from '../../../../directives/directives.module';
-import { NxCamerasComponent } from './cameras.component';
-
-import { TranslateModule }     from '@ngx-translate/core';
-import { ComponentsModule }    from '../../../../components/components.module';
-import { NxHealthModule } from '../../../health/health.module';
+import { NgModule }             from '@angular/core';
+import { CommonModule }         from '@angular/common';
+import { BrowserModule }        from '@angular/platform-browser';
+import { UpgradeModule }        from '@angular/upgrade/static';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { RouterModule }         from '@angular/router';
+import { FormsModule }          from '@angular/forms';
+import { NgbModule }            from '@ng-bootstrap/ng-bootstrap';
+import { DirectivesModule }     from '../../../../directives/directives.module';
+import { NxCamerasComponent }   from './cameras.component';
+import { TranslateModule }      from '@ngx-translate/core';
+import { ComponentsModule }     from '../../../../components/components.module';
+import { NxHealthModule }       from '../../../health/health.module';
 
 @NgModule({
     imports: [
@@ -28,15 +26,15 @@ import { NxHealthModule } from '../../../health/health.module';
         AngularSvgIconModule.forRoot(),
         NxHealthModule
     ],
-    providers : [],
-    declarations : [
+    providers       : [],
+    declarations    : [
         NxCamerasComponent
     ],
-    bootstrap : [],
+    bootstrap       : [],
     entryComponents : [
         NxCamerasComponent
     ],
-    exports: [
+    exports         : [
         NxCamerasComponent
     ]
 })
