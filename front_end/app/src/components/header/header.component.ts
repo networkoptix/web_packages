4a60f13338566bbb61986442d0bf4dc8075e7b87
Cliff Kang <ckang@networkoptix.com>
2020-04-20 17:09:34 -0700

    CLOUD-4773: IPVD: user gets redirected to systems page after logging in
    - login was redirecting for all urls that had a / in it

--- front_end/app/src/components/header/header.component.ts
+++ front_end/app/src/components/header/header.component.ts
@@ -236,8 +236,10 @@ export class NxHeaderComponent implements OnInit, OnDestroy {
     }
 
     login() {
-        const url = this.router.url;
-        const redirect = this.CONFIG.redirect.paths.some((path) => url.indexOf(path) > -1);
+        const { url } = this.router;
+        const redirect = this.CONFIG.redirect.paths.some((path) => {
+            return path === '/' ? url === '/' : url.includes(path);
+        });
         // Handling promise to satisfy the linter.
         this.dialogs
             .login(this.accountService, !redirect)
