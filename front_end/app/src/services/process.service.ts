628809e63ff401f8a3f1b5dbd8be65cae9df7c14
Cliff Kang <ckang@networkoptix.com>
2020-05-04 16:13:35 -0700

    CLOUD-4815: Add index.ts files
    - some errors, removed some index imports

--- front_end/app/src/services/process.service.ts
+++ front_end/app/src/services/process.service.ts
@@ -1,6 +1,6 @@
 import { Injectable }                          from '@angular/core';
 import { NxLanguageProviderService }           from './nx-language-provider';
-import { NxToastService }                      from '../dialogs';
+import { NxToastService }                      from '../dialogs/toast.service';
 import { NxCloudApiService }                   from './nx-cloud-api';
 import { NxConfigService, IConfig }            from './nx-config';
 import { NxSessionService }                    from './session.service';
