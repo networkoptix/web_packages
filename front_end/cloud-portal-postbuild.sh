dddca5d3cc6892d3809107a1dc3d743194e38336
Tsanko Tsolov <ttsolov@networkoptix.com>
2020-05-14 14:53:09 -0700

    CLOUD-5033: Kill the hybrid
    Fixes postbuild script

--- front_end/cloud-portal-postbuild.sh
+++ front_end/cloud-portal-postbuild.sh
@@ -2,4 +2,4 @@ mkdir ./dist/styles;
 # target only main style
 mv ./dist/*.css ./dist/styles; sed -i -e 's/href="styles./href="styles\/styles./g' dist/index.html;
 mv ./dist/languages.*.png ./dist/styles
-mv ./dist/*.js ./dist/scripts; sed -i -e 's/src="/src="scripts\//g' dist/index.html
+mv ./dist/*.js ./dist/scripts; sed -i -e 's/src="/type="text\/javascript" src="scripts\//g' dist/index.html;
