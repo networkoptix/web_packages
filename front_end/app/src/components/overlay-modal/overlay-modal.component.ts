4085f13518653c6a551de5e41b308a2e7d18a7c5
Chris Czach <czach@networkoptix.com>
2020-06-24 23:55:48 -0700

    Adding switchmap

--- front_end/app/src/components/overlay-modal/overlay-modal.component.ts
+++ front_end/app/src/components/overlay-modal/overlay-modal.component.ts
@@ -55,8 +55,6 @@ export class NxOverlayModalComponent implements OnInit {
         }
     ];
 
-    // intervals: number[] = [];
-    // interval: number;
     serverId: string;
     nextInterval = 10;
     // can remove once we can stop multiple logins upon system coming back online
