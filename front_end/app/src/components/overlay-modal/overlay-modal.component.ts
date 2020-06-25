c39fcb6a0092eed9d5af05e01d3ddc6e050896db
Chris Czach <czach@networkoptix.com>
2020-06-25 08:56:02 -0700

    Adding some comments

--- front_end/app/src/components/overlay-modal/overlay-modal.component.ts
+++ front_end/app/src/components/overlay-modal/overlay-modal.component.ts
@@ -56,11 +56,11 @@ export class NxOverlayModalComponent implements OnInit {
     ];
 
     // intervals: number[] = [];
-    // interval: number;
     serverId: string;
     // refreshText: string;
     // systemAvailable = false;
-
+    
+    timeoutUntilRefresh$ = new BehaviorSubject(0);
     checking$ = new BehaviorSubject(false);
     private refresh$ = new Subject();
 
@@ -125,17 +125,39 @@ export class NxOverlayModalComponent implements OnInit {
         //     });
     }
 
+    /**
+     * This is a pretty basic implementation but it should be a good starting point. Needs to be updated
+     * to handle triggering first refresh and is pretty verbose right now for readability but could probably
+     * be refactored or left verbose. Sometimes observables that get really abstracted out can do some really
+     * complex behavior in just a couple lines of code but it's easy to abstract it so much that it's hard to
+     * understand what's going on so it's kind of a balance.
+     */
     setupObservers() {
-        this.refresh$.subscribe(this.checkIfOnline);
+        this.refresh$.pipe(
+            // Whenever refresh emits this switches to a new interval observable.
+            switchMap(_ => interval(1000))
+        ).subscribe((timesIntervalCalled) => {
+            const untilRefresh = this.timeoutUntilRefresh$.value;
+
+            if (untilRefresh < 1) {
+                this.checkIfOnline().then(() => {
+                    // This restarts the interval after checkIfOnline
+                    this.refresh$.next('refresh');
+                    // Resets the refresh counter
+                    this.timeoutUntilRefresh$.next(5);
+                });
+            } else {
+                // This decrements the refresh counter
+                this.timeoutUntilRefresh$.next(untilRefresh ? untilRefresh - 1 : 5);
+            }
+        });
+
         this.appState.systemAvailable$.pipe(
-            distinctUntilChanged(),
+            // This starts an interval of the time between refresh attempts if systemAvailable
             switchMap((systemAvailable) => systemAvailable ? empty() : interval(5000))
-        ).subscribe(timesTriggered => {
-            // Here's kind of a really basic implementation
-            // I'm not sure how the spec expects manual refresh to work but if it's ok
-            // for the interval to not be affected by a manual refresh you could
-            // probaby use a simple implementation like this.
-            console.log(timesTriggered);
+        ).subscribe(timesIntervalTriggered => {
+            this.refresh$.next('refresh');
+            console.log(timesIntervalTriggered);
         });
     }
 
@@ -154,8 +176,9 @@ export class NxOverlayModalComponent implements OnInit {
 
     checkIfOnline() {
         this.checking$.next(true);
-        this.getServers().finally(() => {
+        return this.getServers().finally(() => {
             this.checking$.next(false);
+            this.timeoutUntilRefresh$.next(5);
         });
         // if (reset) {
         //     this.refreshTrackerSubscription.unsubscribe();
