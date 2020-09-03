1058734aedcc074a67e394b0b80d6b4b9ab5f72d
Kamil Bagautdinov <kamilb@networkoptix.com>
2020-09-03 09:16:56 -0700

    add 4.2 to lazy_builder.py

--- robot_tests/DockerQA/lazy_builder.py
+++ robot_tests/DockerQA/lazy_builder.py
@@ -6,8 +6,10 @@ for root, dirs, files in os.walk("."):
     for file in files:
         if 'nxwitness-server-4.0' in file:
             vms_4_0 = file
-        if 'nxwitness-server-4.1' in file:
+        elif 'nxwitness-server-4.1' in file:
             vms_4_1 = file
+        elif 'nxwitness-server-4.2' in file:
+            vms_4_2 = file
 
 cloud_hosts = {
     'prod': 'nxvms.com',
@@ -19,4 +21,5 @@ cloud_hosts = {
 for key, val in cloud_hosts.items():
     subprocess.run(f'docker build -t 4.0_{key} --build-arg mediaserver_deb={vms_4_0} --build-arg cloud_host={val} .', shell=True)
     subprocess.run(f'docker build -t 4.1_{key} --build-arg mediaserver_deb={vms_4_1} --build-arg cloud_host={val} .', shell=True)
+    subprocess.run(f'docker build -t 4.2_{key} --build-arg mediaserver_deb={vms_4_2} --build-arg cloud_host={val} .', shell=True)
 
