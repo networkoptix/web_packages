c3d2e2edf213032ffad6573966e75ff76d1580d9
Nick Hartleb <nhartleb@networkoptix.com>
2020-11-20 13:32:18 -0800

    CLOUD-6209: migratedb function fixes
    Logging for deployment flag being set to true.
    Sleep at the end of migratedb to finish work.
    
    Branch: x/CLOUD-6209_migratedb_fix

--- cloud/cms/management/commands/readstructure.py
+++ cloud/cms/management/commands/readstructure.py
@@ -241,3 +241,5 @@ class Command(BaseCommand):
             'Successfully initiated data structure for CMS'))
 
         caches['deployment'].set(settings.DEPLOYMENT_READY, True)
+        self.stdout.write(self.style.SUCCESS(
+            'Set deployment status to ready'))
