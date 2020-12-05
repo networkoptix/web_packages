44f403325f1dfa4597f5949ffa27889f21410d0d
Roman Barsegian <rbarsegian@networkoptix.com>
2020-12-04 18:31:22 -0800

    CLOUD-6186: Add branding variables in more places

--- cloud/cms/templatetags/util_tags.py
+++ cloud/cms/templatetags/util_tags.py
@@ -8,3 +8,8 @@ def url_replace(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
+
+
+@register.filter
+def escape_ds_name(name):
+    return name.replace('%', '\%')
