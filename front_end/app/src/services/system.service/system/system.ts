c28dfa179a6d65cbc9e1004afb9f457d96c66425
Chris Czach <czach@networkoptix.com>
2020-12-17 11:41:41 -0800

    fix camera and storage manager removed by merge

--- front_end/app/src/services/system.service/system/system.ts
+++ front_end/app/src/services/system.service/system/system.ts
@@ -44,6 +44,9 @@ export class NxSystem extends System {
 
     userManager: UserManager;
     serverManager: ServerManager;
+    cameraManager: CameraManager;
+    storageManager: StorageManager
+
     private _subscribersCount = new BehaviorSubject<number>(0);
 
     activeSubscription: Subscription;
