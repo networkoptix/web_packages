7bfde229944dd95f6bad43e1cca1c18fa98c4d67
Chris Czach <czach@networkoptix.com>
2021-01-05 10:08:24 -0800

    CLOUD-6453: Fix analytics db alert

--- front_end/app/src/pages/systems/settings/servers/standard/server-standard.component.ts
+++ front_end/app/src/pages/systems/settings/servers/standard/server-standard.component.ts
@@ -409,6 +409,8 @@ export class NxSystemStandardServerComponent implements OnInit, OnChanges, OnDes
     }
 
     async changeAnalyticsStorage(newStorage: Partial<DropdownStorage>) {
+        const hasMultipleStorages = this.dropdownStorages.length > 1;
+        this.systemStorageChosen = hasMultipleStorages && !newStorage.isNotSystem;
         if (newStorage.id === this.currentAnalyticsDbId) return;
         // check if analytics data exists
         this.checkingForDataAnalytics = true;
@@ -432,9 +434,11 @@ export class NxSystemStandardServerComponent implements OnInit, OnChanges, OnDes
                             autohide  : true,
                             delay     : this.CONFIG.alertTimeout
                         };
+                        this.systemStorageChosen = hasMultipleStorages && !this.selectedStorage.isNotSystem;
                         this.toastService.show(this.LANG.servers.analyticsDataPolicyError?.(), options);
                     } else if (closeRes === 'cancel') {
                         this.selectedStorage = { ...this.selectedStorage };
+                        this.systemStorageChosen = hasMultipleStorages && !this.selectedStorage.isNotSystem;
                     }
                     this.currentAnalyticsDbId = this.selectedStorage.id;
                     this.saveStorageWatcher.value = false;
@@ -442,9 +446,8 @@ export class NxSystemStandardServerComponent implements OnInit, OnChanges, OnDes
         } else {
             this.selectedStorage = newStorage;
             this.saveStorageWatcher.value = this.selectedStorage.id !== this.currentAnalyticsDbId;
+            this.systemStorageChosen = hasMultipleStorages && !this.selectedStorage.isNotSystem;
         }
-        const hasMultipleStorages = this.dropdownStorages.length > 1;
-        this.systemStorageChosen = hasMultipleStorages && !this.selectedStorage.isNotSystem;
         this.checkingForDataAnalytics = false;
     }
 
