0fc461d924bb7d1f977a0f2b4334903afc863660
Chris Czach <czach@networkoptix.com>
2021-01-05 20:12:39 +0000

    CLOUD-6204: Fix dropdown input cut off

--- front_end/app/src/pages/systems/settings/servers/storage/server-storage-standard.component.ts
+++ front_end/app/src/pages/systems/settings/servers/storage/server-storage-standard.component.ts
@@ -370,12 +370,13 @@ export class NxSystemStorageComponent implements OnInit {
             });
         } catch (error) {
             console.error('error while retrieving data checking server for default backup settings', error);
+            return false;
         }
     }
 
     updateCustom = () => {
         return this.doesCurrentServerHaveDefaultSettings().then(hasDefault => {
-            this.customSettings = !hasDefault && this.isBackupOn.originalValue;
+            this.customSettings = !hasDefault;
         });
     }
 
@@ -718,7 +719,7 @@ export class NxSystemStorageComponent implements OnInit {
         };
 
         const pollStorageStatus = (beingChecked) => this.system.storageManager.updateOrGetSystemStorage({ serverId: this.serverId }, false, maxTimeout).pipe(
-            map(({ reply: { storages } }) => storages),
+            map(({ reply }) => reply?.storages || []),
             tap(updateChangedStatus(beingChecked)),
             map(checkIfStoragesMissing(beingChecked)),
             takeWhile(storagesMissing => storagesMissing || filterBeingChecked(beingChecked)),
