12d5d5097d861d1294a737ce443b1e6bf5186060
Chris Czach <czach@networkoptix.com>
2021-03-01 09:22:38 -0800

    CLOUD-6820: Fix review preview

--- front_end/app/src/services/nx-cloud-api.ts
+++ front_end/app/src/services/nx-cloud-api.ts
@@ -436,7 +436,7 @@ export class NxCloudApiService {
             params = params.set('page', assetIdOrSearchObject.page ? assetIdOrSearchObject.page.toString() : '1');
         }
         if (state) {
-            params = params.set('state', state.replace('pending', 'review'));
+            params = params.set('state', state);
         }
         const route = `${this.CONFIG.apiBase}/documentation${endpoint}?${params.toString()}`;
         this.cacheService.addToCache(route);
