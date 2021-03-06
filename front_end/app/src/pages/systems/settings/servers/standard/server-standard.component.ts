0c7e428ff380cb9a52830f6c78914686240f7b39
Nick Hartleb <nhartleb@networkoptix.com>
2021-03-05 21:02:30 -0800

    CLOUD-6926: Fixed so admin can rename server
    Branch: x/cloud_20.2/CLOUD-6926_admin_rename_server

--- front_end/app/src/pages/systems/settings/servers/standard/server-standard.component.ts
+++ front_end/app/src/pages/systems/settings/servers/standard/server-standard.component.ts
@@ -182,8 +182,7 @@ export class NxSystemStandardServerComponent implements OnChanges, OnDestroy {
         this.selectedServer.ip = ip;
         this.parsedServerId = NxUtilsService.cleanId(this.selectedServer.id);
         this.selectedServer.osName = this.selectedServer.osInfo ? JSON.parse(this.selectedServer.osInfo).platform : this.LANG.common.unknown?.();
-
-        this.renameDisabled = !this.system.permissions.editAdmins;
+        this.renameDisabled = !this.system.permissions.isAdmin;
         this.restartDisabled = !this.system.permissions.isAdmin;
         this.detachDisabled = !this.system.permissions.editAdmins;
         this.resetDisabled = !this.system.permissions.editAdmins;
