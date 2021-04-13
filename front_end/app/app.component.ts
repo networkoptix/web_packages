3a91d0b8882b3ca9d83ae0be5fd778c132235a49
Cliff Kang <ckang@networkoptix.com>
2021-04-12 21:55:00 -0700

    trying just reload and not changing href

--- front_end/app/app.component.ts
+++ front_end/app/app.component.ts
@@ -86,12 +86,7 @@ export class AppComponent {
                 if (ev.url.startsWith('/?code=')) {
                     return this.cloudApiService.loginCode(ev.url.slice(7))
                         .then(() => {
-                            // this.window.location.href = '/systems';
-                            // this.window.location.reload();
-                            this.router.navigate(['systems']);
-                            // .then(() => {
-                            //     this.window.location.reload();
-                            // });
+                            this.window.location.reload();
                         });
                 }
             });
