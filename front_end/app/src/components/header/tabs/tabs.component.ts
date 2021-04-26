47fb5801104aeefa56d82a99f978cb9edb64580a
Tsanko Tsolov <ttsolov@networkoptix.com>
2021-04-26 17:56:41 +0000

    CLOUD-7173 No redirect to the correct url when header links are opened in new tab

--- front_end/app/src/components/header/tabs/tabs.component.ts
+++ front_end/app/src/components/header/tabs/tabs.component.ts
@@ -1,23 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-import { environment }     from '@environments/environment';
-import { MenuNode }        from '@services/menus.service';
-import { NxHeaderService } from '@services/nx-header.service';
+import { Component, Input } from '@angular/core';
+import { environment }      from '@environments/environment';
+import { MenuNode }         from '@services/menus.service';
+import { NxHeaderService }  from '@services/nx-header.service';
 
 @Component({
     selector    : 'nx-header-tabs',
     templateUrl : './tabs.component.html',
     styleUrls   : [environment.isLocal ? './tabs-webadmin.component.scss' : './tabs.component.scss']
 })
-export class NxTabsComponent implements OnInit {
+export class NxTabsComponent {
     @Input() node: MenuNode;
     constructor(
         public headerService: NxHeaderService
     ) {}
-
-    ngOnInit() {
-        if (environment.isLocal && !this.node.url.startsWith('/#')) {
-            this.node.url = '/#' + this.node.url;
-        }
-    }
 }
