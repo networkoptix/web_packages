128e35c107f5944459476c872cf1a841a8ab1097
Chris Czach <czach@networkoptix.com>
2021-04-29 16:16:19 -0700

    Revert "CLOUD-7213: Fix import all doc assets"
    This reverts commit 6e8ebc040f576cc336a463c9ed5032772f3a5bb0.

--- cloud/cms/views/asset.py
+++ cloud/cms/views/asset.py
@@ -430,7 +430,7 @@ def asset_settings(request, asset_id):
             elif import_assets_from_json:
                 json_cache_id = uuid.uuid4()
                 PACKAGE_CACHE[json_cache_id] = loaded_json
-                task = async_import_assets_from_json.apply_async(args=[loaded_json, request.user.id, import_assets_from_json_publish])
+                task = async_import_assets_from_json.apply_async(args=[json_cache_id, request.user.id, import_assets_from_json_publish])
                 messages.info(request, 'Starting assets import')
             elif not update_structure:
                 return HttpResponseBadRequest('json is acceptable only for Updating structure')
