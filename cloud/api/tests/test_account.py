2afd65883ecad4d9cef39a8db9b4ab60a28a0593
Roman Barsegian <rbarsegian@networkoptix.com>
2021-05-13 11:55:11 -0700

    CLOUD-6834: Introduce pytest, fixtures, and mocks

--- cloud/api/tests/test_account.py
+++ cloud/api/tests/test_account.py
@@ -7,74 +7,71 @@ from api.models import *
 from cms.models import Language
 from .utils import NxTestClient
 
+import pytest
+from pytest_django.asserts import assertContains
 
-class LoginHistoryTestCase(TestCase):
-    client_class = NxTestClient
 
-    @classmethod
-    def setUpTestData(cls) -> None:
-        cls.user = Account.objects.create(email='test@test.com')
+class TestLoginHistory:
+    test_user_email = 'test@test.com'
+
+    @pytest.fixture(autouse=True)
+    def setup(self, client, db) -> None:
+        self.user = Account.objects.get_or_create(email='test@test.com')[0]
+        self.client = client
 
     def test_login(self):
         # response = self.client.post('/api/account/login', data={'email': 'test@test.com', 'password': 'password'})
         self.client.force_login(self.user)
         last_record = AccountLoginHistory.objects.last()
-        self.assertEqual('user_logged_in', last_record.action)
-        self.assertEqual('test@test.com', last_record.email)
-        self.assertEqual('127.0.0.1', last_record.ip)
+        assert last_record.action == 'user_logged_in'
+        assert last_record.email == 'test@test.com'
+        assert last_record.ip == '127.0.0.1'
 
     def test_logout(self):
         self.client.force_login(self.user)
         self.client.logout(self.user)
         last_record = AccountLoginHistory.objects.order_by('id').last()
-        self.assertEqual('user_logged_out', last_record.action)
-        self.assertEqual('test@test.com', last_record.email)
-        self.assertEqual('127.0.0.1', last_record.ip)
+        assert last_record.action == 'user_logged_out'
+        assert last_record.email == 'test@test.com'
+        assert last_record.ip == '127.0.0.1'
 
     def test_failed_login(self):
-        self.client.post('/api/account/login', data={'email': 'test@test.com', 'password':'wrongPass'})
+        self.client.post('/api/account/login', data={'email': 'test@test.com', 'password': 'wrongPass'})
         last_record = AccountLoginHistory.objects.order_by('id').last()
-        self.assertEqual('user_login_failed', last_record.action)
-        self.assertEqual('test@test.com', last_record.email)
-        self.assertEqual('127.0.0.1', last_record.ip)
+        assert last_record.action == 'user_login_failed'
+        assert last_record.email == 'test@test.com'
+        assert last_record.ip == '127.0.0.1'
 
 
-class CloudInviteViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls) -> None:
-        cls.superuser = Account.objects.create(
-            email='test@test.com', first_name='super', last_name='user', is_superuser=True, is_staff=True
-        )
-        lang = Language.objects.create(code='en_US')
-        cls.customization = Customization.objects.create(name='default', default_language=lang)
-
-    def setUp(self) -> None:
-        self.client.force_login(self.superuser)
+class TestCloudInvite:
+    @pytest.fixture(autouse=True)
+    def setup(self, admin_client, default_customization):
+        self.client = admin_client
         self.invite_path = reverse('admin:invite')
-        self.client.follow = True
-        self.existing_user = Account.objects.create(email='exists@exists.com')
+        self.existing_user = Account.objects.get_or_create(email='exists@exists.com')[0]
+        self.customization = default_customization
 
     def test_get(self):
         response = self.client.get(self.invite_path)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<h1>Invite User</h1>', html=True)
+        assert response.status_code == 200
+        assertContains(response, '<h1>Invite User</h1>', html=True)
 
     def test_post(self):
         response = self.client.post(self.invite_path, data={
             'customization': 'default', 'email': 'invite@test.com', 'message': 'Welcome to cloud!'
         }, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'User has been invited to cloud.')
+        assert response.status_code == 200
+        assertContains(response, 'User has been invited to cloud.')
         user_created = Account.objects.filter(email='invite@test.com').exists()
-        self.assertTrue(user_created, 'Invited account was created')
-        self.assertTrue(mail.outbox)
+        assert user_created, 'Invited account was not created'
+        assert mail.outbox
 
     def test_post_fail(self):
         response = self.client.post(self.invite_path, data={
             'customization': 'not', 'email': 'invite@test.com', 'message': 'Welcome to cloud!'
         }, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Select a valid choice')
+        assert response.status_code == 200
+        assertContains(response, 'Select a valid choice')
 
     def test_user_exists(self):
         default_data = {'customization': self.customization.name, 'message': 'Welcome!'}
@@ -85,9 +82,9 @@ class CloudInviteViewTest(TestCase):
             'email': self.existing_user.email,
         }, follow=True)
         messages = list(get_messages(response.wsgi_request))
-        self.assertEqual('User already has a cloud account!', messages[0].message)
+        assert 'User already has a cloud account!' == messages[0].message
         # Make sure no email is sent
-        self.assertFalse(mail.outbox)
+        assert not mail.outbox
 
     def test_group(self):
         group = Group.objects.create(name='test_group')
@@ -98,29 +95,29 @@ class CloudInviteViewTest(TestCase):
             'email': self.existing_user.email,
         }, follow=True)
         messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(f'User successfully added to "{group.name}" group.', messages[0].message)
-        self.assertTrue(self.existing_user.groups.filter(id=group.id).exists())
+        assert f'User successfully added to "{group.name}" group.' == messages[0].message
+        assert self.existing_user.groups.filter(id=group.id).exists()
         # Make sure no email is sent
-        self.assertFalse(mail.outbox)
+        assert not mail.outbox
 
         # Invite user to group they are already a part of
         response = self.client.post(f'{self.invite_path}?group_id={group.id}', data={
             **default_data,
             'email': self.existing_user.email,
         }, follow=True)
-        self.assertTrue(self.existing_user.groups.filter(id=group.id).exists())
+        assert self.existing_user.groups.filter(id=group.id).exists()
         messages = list(get_messages(response.wsgi_request))
-        self.assertEqual(f'User already in "{group.name}" group.', messages[0].message)
+        assert f'User already in "{group.name}" group.' == messages[0].message
         # Make sure no email is sent
-        self.assertFalse(mail.outbox)
+        assert not mail.outbox
 
-    def test_message(self):
-        self.client.post(self.invite_path, data={
-            'customization': self.customization.name,
+    def test_message(self, admin_client):
+        admin_client.follow = True
+        response = admin_client.post(reverse('admin:invite'), data={
+            'customization': 'default',
             'email': 'new_account@newaccount.com',
             'message': 'Test message'
         }, follow=True)
-
-        self.assertTrue('Test message' in mail.outbox[0].body)
+        assert 'Test message' in mail.outbox[0].body
 
 
---
+++ cloud/conftest.py
@@ -0,0 +1,66 @@
+import pytest
+
+from api.tests.utils import NxTestClient
+from cms.controllers.structure import read_structure_json
+from cms.models import *
+
+
+@pytest.fixture(scope='session')
+def django_db_setup(django_db_setup, django_db_blocker, django_db_createdb, django_db_keepdb):
+    with django_db_blocker.unblock():
+        if django_db_createdb:
+            read_structure_json()
+        eng = Language.objects.get_or_create(name='English', code='en_US')[0]
+        Customization.objects.get_or_create(name='default', default_language=eng)
+        portal_type = AssetType.get_model_by_type(AssetType.ASSET_TYPES.documentation)
+        Asset.objects.get_or_create(name='Nx Cloud', asset_type=portal_type)
+
+
+@pytest.fixture(autouse=True)
+def set_settings(settings):
+    settings.TESTING = True
+
+
+@pytest.fixture
+def superuser(django_user_model, db):
+    return django_user_model.objects.get_or_create(email='auto_superuser@networkoptix.com', is_superuser=True, is_staff=True)[0]
+
+
+@pytest.fixture()
+def client():
+    return NxTestClient()
+
+
+@pytest.fixture
+def admin_client(db, superuser):
+    client = NxTestClient()
+    client.force_login(superuser)
+    return client
+
+
+@pytest.fixture
+def authenticated_client(django_user_model, client, db):
+    def _authenticated_client(email):
+        user = django_user_model.objects.get_or_create(email=email)[0]
+        client.force_login(user)
+    return _authenticated_client
+
+
+@pytest.fixture
+def english_language(db):
+    return Language.objects.get_or_create(name='English', code='en_US')[0]
+
+
+@pytest.fixture()
+def default_customization(english_language, db):
+    return Customization.objects.get_or_create(name='default', default_language=english_language)[0]
+
+
+@pytest.fixture
+def cloud_portal_type(db):
+    return AssetType.get_model_by_type(AssetType.ASSET_TYPES.documentation)
+
+
+@pytest.fixture
+def default_portal(default_customization, cloud_portal_type, db):
+    return Asset.objects.get_or_create(name='Nx Cloud', asset_type=cloud_portal_type)[0]
