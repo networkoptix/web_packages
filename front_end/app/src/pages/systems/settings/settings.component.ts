d20fad9a9476427428d63238c198727c7b30d6a9
Tsanko Tsolov <ttsolov@networkoptix.com>
2021-06-09 10:53:46 -0700

    CLOUD-7414: Detailed Info button in cameras settings takes user to 500

--- front_end/app/src/pages/systems/settings/settings.component.ts
+++ front_end/app/src/pages/systems/settings/settings.component.ts
@@ -1,34 +1,34 @@
 import {
     Component, Input,
     OnDestroy, OnInit
-}                                    from '@angular/core';
+}                                       from '@angular/core';
 import {
     ActivatedRoute, Router,
-    NavigationEnd
-}                                    from '@angular/router';
-import { UntilDestroy }              from '@ngneat/until-destroy';
-import { Subject, Subscription }     from 'rxjs';
-import { filter, takeUntil, tap }    from 'rxjs/operators';
-import { NxConfigService, IConfig }  from '../../../services/nx-config';
-import { NxLanguageProviderService } from '@services/nx-language-provider';
-import { NxProcessService, Process } from '@services/process.service';
-import { NxDialogsService }          from '@dialogs/dialogs.service';
-import { NxSettingsService }         from './settings.service';
-import { NxMenuService }             from '@src/menu';
+    NavigationEnd, NavigationStart
+}                                       from '@angular/router';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Subject, Subscription }        from 'rxjs';
+import { filter, takeUntil, tap }       from 'rxjs/operators';
+import { NxConfigService, IConfig }     from '../../../services/nx-config';
+import { NxLanguageProviderService }    from '@services/nx-language-provider';
+import { NxProcessService, Process }    from '@services/process.service';
+import { NxDialogsService }             from '@dialogs/dialogs.service';
+import { NxSettingsService }            from './settings.service';
+import { NxMenuService }                from '@src/menu';
 import {
     ICamera, NxSystem,
     NxSystemService
-}                                    from '@services/system.service';
-import { NxSystemsService }          from '@services/systems.service';
-import { Account, NxAccountService } from '@services/account.service';
-import { NxUtilsService }            from '@services/utils.service';
-import { NxUriService }              from '@services/uri.service';
-import { NxScrollMechanicsService }  from '@services/scroll-mechanics.service';
-import { NxApplyService }            from '@services/apply.service';
-import { NxPageService }             from '@services/page.service';
-import { NxRibbonService }           from '@components/ribbon';
-import { LanguageI18NStaticTypes }   from '@app/language_i18n_static_types';
-import { NxAppStateService }             from '@services/nx-app-state.service';
+}                                       from '@services/system.service';
+import { NxSystemsService }             from '@services/systems.service';
+import { Account, NxAccountService }    from '@services/account.service';
+import { NxUtilsService }               from '@services/utils.service';
+import { NxUriService }                 from '@services/uri.service';
+import { NxScrollMechanicsService }     from '@services/scroll-mechanics.service';
+import { NxApplyService }               from '@services/apply.service';
+import { NxPageService }                from '@services/page.service';
+import { NxRibbonService }              from '@components/ribbon';
+import { LanguageI18NStaticTypes }      from '@app/language_i18n_static_types';
+import { NxAppStateService }            from '@services/nx-app-state.service';
 
 @UntilDestroy({ checkProperties: true })
 @Component({
@@ -167,6 +167,12 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
         });
 
         this.router.events.subscribe(route => {
+            if (route instanceof NavigationStart) {
+                // remove unnecessary system update (ex. health monitor will trigger system update)
+                // and orphan metrics request in systemInfoSubscription
+                this.systemInfoSubscription?.unsubscribe();
+                this.systemSubscription?.unsubscribe();
+            }
             if (route instanceof NavigationEnd) {
                 const isSystemRoute = route.url.includes('/systems');
                 const isCameraRoute = route.url.includes('/cameras');
@@ -284,11 +290,15 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
                         }
 
                         if (this.systemSubscription) {
+                            this.systemInfoSubscription?.unsubscribe();
                             this.systemSubscription.unsubscribe();
                         }
-                        this.systemSubscription = this.systemsService
-                            .systemsSubject
+                        this.systemSubscription = this.systemsService.systemsSubject
+                            .pipe(
+                                untilDestroyed(this)
+                            )
                             .subscribe((systems) => {
+                                console.log('systemSubscription ->');
                                 if (!systems.filter(s => s.id === this.systemId).length) {
                                     this.systemNoAccess = true;
                                     return;
@@ -307,6 +317,7 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
                                 }
                                 this.systemInfoSubscription = this.system.infoSubject
                                     .pipe(
+                                        untilDestroyed(this),
                                         filter((system: any) => system !== undefined),
                                         tap(({ isOnline }) => {
                                             this.applyService.isOnline$.next(!!isOnline);
@@ -318,6 +329,7 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
                                         })
                                     )
                                     .subscribe(() => {
+                                        console.log('systemInfoSubscription ->');
                                         // if system is removed while on page, redirects to systems page
                                         if (
                                             this.system && this.systemsService.systems &&
