48be0eb9c204f2a22957625f5e07f6d40718e4dc
Cliff Kang <ckang@networkoptix.com>
2021-06-22 15:53:02 -0700

    CLOUD-7525: reintroduce code that was pulling camera archive data

--- front_end/app/src/pages/systems/view/pages/system-view-index/system-view-index.page.component.ts
+++ front_end/app/src/pages/systems/view/pages/system-view-index/system-view-index.page.component.ts
@@ -278,37 +278,38 @@ export class NxSystemViewIndexPageComponent implements OnInit, OnDestroy {
                         }
                     });
                     // TODO: If no issues with this section being commented out remove it in 21.1
-                    // const findCameraArchiveRanges = (cid) => {
-                    //     // (check archive presence mode)
-                    //     if (!this.system.userManager.permissions.viewArchives) {
-                    //         return Promise.resolve();
-                    //     }
-                    //     return this.system.getCameraRecords(cid, 0, now, now).then(response => {
-                    //         const hasArchive = parseInt(response.error) ? false : (response.reply && response.reply.length);
-                    //         // this._log('check archive presence', cid, result, response, '|', response.reply, '|', response.reply.length)
-                    //         const extractChunk = chunks => {
-                    //             let longestDuration = 0;
-                    //             let earliestStart = Number.POSITIVE_INFINITY;
-                    //             chunks.forEach((chunk) => {
-                    //                 // 4.3 api response changed
-                    //                 const start = parseInt(chunk?.periods.length ? chunk.periods[0].startTimeMs : chunk.startTimeMs);
-                    //                 const duration = parseInt(chunk?.periods.length ? chunk.periods[0].durationMs : chunk.durationMs);
-                    //                 if (start < earliestStart) {
-                    //                     earliestStart = start;
-                    //                 }
-                    //                 if (longestDuration !== -1 && (duration === -1 || duration > longestDuration)) {
-                    //                     longestDuration = duration;
-                    //                 }
-                    //             });
-                    //             const end = (longestDuration === -1) ? now : (earliestStart + longestDuration);
-                    //             return [earliestStart, end];
-                    //         };
-                    //         if (hasArchive) {
-                    //             const [start, end] = extractChunk(response.reply);
-                    //             archiveRanges[cid] = new SimpleTimeRange(start, end);
-                    //         }
-                    //     });
-                    // };
+                    // issue with camera archive data not being loaded --> may refactor in 21.1
+                    const findCameraArchiveRanges = (cid) => {
+                        // (check archive presence mode)
+                        if (!this.system.userManager.permissions.viewArchives) {
+                            return Promise.resolve();
+                        }
+                        return this.system.getCameraRecords(cid, 0, now, now).then(response => {
+                            const hasArchive = parseInt(response.error) ? false : (response.reply && response.reply.length);
+                            // this._log('check archive presence', cid, result, response, '|', response.reply, '|', response.reply.length)
+                            const extractChunk = chunks => {
+                                let longestDuration = 0;
+                                let earliestStart = Number.POSITIVE_INFINITY;
+                                chunks.forEach((chunk) => {
+                                    // 4.3 api response changed
+                                    const start = parseInt(chunk?.periods.length ? chunk.periods[0].startTimeMs : chunk.startTimeMs);
+                                    const duration = parseInt(chunk?.periods.length ? chunk.periods[0].durationMs : chunk.durationMs);
+                                    if (start < earliestStart) {
+                                        earliestStart = start;
+                                    }
+                                    if (longestDuration !== -1 && (duration === -1 || duration > longestDuration)) {
+                                        longestDuration = duration;
+                                    }
+                                });
+                                const end = (longestDuration === -1) ? now : (earliestStart + longestDuration);
+                                return [earliestStart, end];
+                            };
+                            if (hasArchive) {
+                                const [start, end] = extractChunk(response.reply);
+                                archiveRanges[cid] = new SimpleTimeRange(start, end);
+                            }
+                        });
+                    };
                     const processCameras = (c, ms) => {
                         this.hasCameras = true;
                         const result = new Camera(
@@ -320,7 +321,7 @@ export class NxSystemViewIndexPageComponent implements OnInit, OnDestroy {
                             ms.status === 'Offline' ? 'Offline' : (c.status === 'Online' ? 'Live' : c.status) as CAMERA_STATUS,
                             c.scheduleEnabled,
                             c.disableDualStreaming,
-                            new SimpleTimeRange(0, 0),
+                            archiveRanges[c.id] || new SimpleTimeRange(0, 0),
                             [],
                             this.system?.getCameraThumbnailUrl(c.id),
                             (transport: string, quality: string, t?: ms) => this.system?.getPlaybackUrl(c.id, transport, quality, t),
@@ -330,11 +331,10 @@ export class NxSystemViewIndexPageComponent implements OnInit, OnDestroy {
                         return result;
                     };
                     // TODO: If no issues with this section being commented out remove it in 21.1
-                    // const cameraIds = mediaServers.reduce((acc, ms) => acc.concat(ms.cameras.map(c => c.id)), []);
-                    // const archiveRanges = {};
-                    // const archives = {};
-                    // const now = Date.now();
-                    // await Promise.all(cameraIds.map(findCameraArchiveRanges));
+                    const cameraIds = mediaServers.reduce((acc, ms) => acc.concat(ms.cameras.map(c => c.id)), []);
+                    const archiveRanges = {};
+                    const now = Date.now();
+                    await Promise.all(cameraIds.map(findCameraArchiveRanges));
 
                     cachedMediaServers = mediaServers.map(ms => NxUtilsService.formatURL(({
                         id               : ms.id,
