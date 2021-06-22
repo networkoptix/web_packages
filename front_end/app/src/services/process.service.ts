119d00c96b6ef2b659b62eb01760227924499107
Chris Czach <czach@networkoptix.com>
2021-06-22 11:02:50 -0700

    CLOUD-7542: Fix process error messages

--- front_end/app/src/services/process.service.ts
+++ front_end/app/src/services/process.service.ts
@@ -263,5 +263,6 @@ export const formatError = (error, errorCodes, lang: LanguageI18NStaticTypes): s
             return errorCodes[errorCode];
         }
     }
-    return (lang.errorCodes[errorCode])(errorCode) || lang.errorCodes.unknownError();
+    const errorText = typeof lang.errorCodes[errorCode] === 'function' ? lang.errorCodes[errorCode]() : lang.errorCodes[errorCode];
+    return errorText || lang.errorCodes.unknownError();
 };
