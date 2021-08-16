92df88683ecc12930684231392b255c62a56cc3c
Tsanko Tsolov <ttsolov@networkoptix.com>
2021-08-16 13:51:20 -0700

    CLOUD-7648: Timeline - last minute playback for cording camera should be green stripy lines only

--- front_end/app/src/pages/systems/view/vms-client/submodules/timeline/services/canvas-renderer/stripy-bar/cfg.ts
+++ front_end/app/src/pages/systems/view/vms-client/submodules/timeline/services/canvas-renderer/stripy-bar/cfg.ts
@@ -1,10 +1,10 @@
 import { deg, px, pxPerSecond, color } from './types';
-import colors from '../drawingConfigs/colors'
+import colors from '../drawingConfigs/colors';
 
 export default {
     stripeWidth     : 8 as px,
     slope           : 45 as deg,
     speed           : 24 as pxPerSecond,
-    backgroundColor : `${colors.light4}C0` as color,
-    stripeColor     : `${colors.light6}C0` as color,
+    backgroundColor : `${colors.light6}C0` as color,
+    stripeColor     : `${colors.light1}C0` as color
 };
