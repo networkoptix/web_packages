db30418bc4ccf28e87994fef93be66eb8e4ce19a
Grigory Bezyuk <gbezyuk@networkoptix.com>
2021-09-01 10:41:12 +0000

    Resolve CLOUD-7790 "Fullscreen vs pause"

---
+++ front_end/app/src/pages/systems/view/vms-client/utils/generateClickDubleClickPair.ts
@@ -0,0 +1,26 @@
+export function generateClickDubleClickPair (onClick, onDblClick, dblClickDelayMs = 300) {
+    let scheduledHandler = null
+    let prevClickTime = null
+
+    return function (e) {
+        const now = Date.now()
+        if (scheduledHandler) {
+            const timePassed = now - prevClickTime
+            if (timePassed < dblClickDelayMs) {
+                console.log(timePassed)
+                clearTimeout(scheduledHandler)
+                scheduledHandler = null
+                onDblClick()
+            }
+        } else {
+            scheduledHandler = setTimeout(() => {
+                scheduledHandler = null
+                prevClickTime = null
+                onClick(e)
+            }, dblClickDelayMs)
+            prevClickTime = now
+        }
+    }
+}
+
+export default generateClickDubleClickPair
