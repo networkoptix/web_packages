d50d2db7a8a4243a39491dd7750fec1b973c99f2
Chris Czach <czach@networkoptix.com>
2021-09-10 13:05:11 -0700

    CLOUD-7830: Celery View Unit Tests

--- cloud/cms/views/celery.py
+++ cloud/cms/views/celery.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from celery.result import AsyncResult
+from celery import result
 
-from api.helpers.exceptions import APIException, api_success
+from api.helpers.exceptions import APINotFoundException, api_success, handle_exceptions
 
 from cms.models import PackagesCache
 from cms.permissions import IsSuperuser
@@ -11,16 +11,17 @@ from cms.permissions import IsSuperuser
 @api_view(["GET"])
 @permission_classes((IsAuthenticated,))
 def check_status(request, task_id):
-    task = AsyncResult(task_id)
+    task = result.AsyncResult(task_id)
     return api_success(task.result or task.state)
 
 
 @api_view(["GET"])
 @permission_classes((IsSuperuser,))
+@handle_exceptions
 def download_result(request, task_id):
     from cms.views.asset import response_attachment
     DOWNLOAD_CACHE = PackagesCache()
     result = DOWNLOAD_CACHE.get(task_id)
     if not result:
-        return APIException('File not available', 404)
+        raise APINotFoundException('File not available')
     return response_attachment(result['file'], result['file_name'], 'application/json', attachment=True)
