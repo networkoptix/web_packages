ab28775e855b1343b1e5e130b8de014e2155e4fc
Nick Hartleb <nhartleb@networkoptix.com>
2021-10-28 19:23:02 -0700

    CLOUD-7996: Authorize loading improvements
    - Moved setting the background into the index file. Waiting for angular to bootstrap took too long.
    - Added minimum height and width to svgs so that the page holds its shape.
    - Fixed routing issue were app never went to a page.

--- front_end/projects/authorization/src/app/app-routing.module.ts
+++ front_end/projects/authorization/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     },
     {
         path: '**',
-        redirectTo: '/'
+        redirectTo: '/authorize'
     }
 ];
 
--- front_end/projects/authorization/src/app/app.component.ts
+++ front_end/projects/authorization/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { debounceTime, filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -8,16 +6,4 @@ import { debounceTime, filter } from 'rxjs/operators';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'authorization';
-    viewType = 'web';
-
-    constructor(router: Router) {
-        router.events
-            .pipe(filter(ev => ev instanceof NavigationEnd), debounceTime(50))
-            .subscribe(({ url }: NavigationEnd) => {
-                this.viewType = url.includes('?') && new URLSearchParams(
-                    url.match(/.*(\?.*)/i)[1]
-                ).get('view_type') || 'web';
-            });
-    }
 }
