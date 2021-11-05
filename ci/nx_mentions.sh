8554545cdce699dcfae7a220fcab6e4572581b12
Roman Barsegian <rbarsegian@networkoptix.com>
2021-11-05 04:03:37 +0300

    CLOUD-8123: Make reusable docker image for django tests

--- ci/nx_mentions.sh
+++ ci/nx_mentions.sh
@@ -3,7 +3,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 BAN_LIST="[^[:alpha:]]nx\ |nxvms"
 echo "Checking files for mentions of nx with the following patterns: ${BAN_LIST}"
-branding=$(grep -Ei "$BAN_LIST" -rl --exclude-dir=fonts --exclude={\*.{mock.ts,spec.ts,swf,png,gif},{commonPasswordsList,downloads}.json} ../front_end/app) || true
+branding=$(grep -Ei "$BAN_LIST" -rl --exclude-dir=fonts --exclude={\*.{mock.ts,spec.ts,swf,png,gif},{commonPasswordsList,downloads}.json,test.ts} ../front_end/app) || true
 if [[ -z ${branding} ]]
 then
     echo "No mentions were found"
