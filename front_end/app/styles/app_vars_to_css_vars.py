6c18e0dc052a0b1608c5a7e1689cdf4ea26e4388
Andrew Wu <awu@networkoptix.com>
2021-11-20 05:38:15 +0300

    Reduce CSS variables to only light/dark variables

--- front_end/app/styles/app_vars_to_css_vars.py
+++ front_end/app/styles/app_vars_to_css_vars.py
@@ -1,41 +1,22 @@
 """A script to generate CSS variables for app Sass variables.
 
+Currently limited to light/dark colors from the theme palettes.
+
 Run in app/styles.
 """
 
-import itertools
 import re
 
 from pathlib import Path
-from typing import Dict, List
+from typing import List
 
 VAR_NAME_RE_STR = r"[\w\-]+"
-SASS_VAR_REGEX = re.compile(fr"^ *\${VAR_NAME_RE_STR}", re.M)
-SKIN_VAR_REGEX = re.compile(fr"^ *--({VAR_NAME_RE_STR}):", re.M)
 LINE_REGEX = re.compile(
-    fr"(?P<name>{VAR_NAME_RE_STR})\s*:\s*(?P<value>.+)\s*;"
+    fr"^ *(?P<name>\${VAR_NAME_RE_STR})\s*:\s*(?P<value>.+)\s*;", re.M
 )
-CSS_VAR_REGEX = re.compile(fr"var\(--.+\)")
-HEX_REGEX = re.compile(r"#[a-zA-Z0-9]+")
-
-def get_sass_vars(filepath: str) -> List[str]:
-    """Find all variables defined in a Sass file.
-
-    Includes leading $ in variable names.
-    """
-    return SASS_VAR_REGEX.findall(Path(filepath).read_text(encoding="utf-8"))
+LIGHT_DARK_VALUE_REGEX = re.compile(r"\$[\w\-]*?(light|dark)\d+")
 
 def main():
-    palette_files = [
-        "./native-theme/_theme_palette.scss",
-        "./_theme_palette.scss"
-    ]
-    palette_vars = set(
-        itertools.chain.from_iterable(
-            get_sass_vars(file) for file in palette_files
-        )
-    )
-
     source_files = [
         "./_theme_variables.scss",
         # "./_pages_variables.scss"
@@ -44,32 +25,23 @@ def main():
     for file in source_files:
         source_path = Path(file)
 
-        source_vars: Dict[str, str] = {}
-        for line in source_path.read_text().splitlines():
-            if not SASS_VAR_REGEX.match(line):
-                continue
+        source_vars: List[str] = []
+        line_matches = dict(
+            match.groups() for match in LINE_REGEX.finditer(
+                source_path.read_text(encoding="utf-8")
+            )
+        )
+        for name, value in line_matches.items():
+            if value in line_matches:
+                original = value
+                while original in line_matches:
+                    original = line_matches[original]
+                    # Follow up to original value
 
-            try:
-                name, value = LINE_REGEX.search(line).groups()
-            except AttributeError:
-                continue
-
-            # Only capture colors, ignore everything else
-            if (
-                value in palette_vars # palette variable
-                or value in source_vars # source variable
-                or CSS_VAR_REGEX.fullmatch(value) # CSS variable
-                or HEX_REGEX.match(value) # hex value
-                or value == "transparent"
-                or any(var in value for var in palette_vars)
-                # palette variable with modification
-                # e.g. $light16 !default
-                or any(var in value for var in source_vars)
-                # source variable with modification
-            ):
-                source_vars[name] = value
-
-        css_var_value_regex = re.compile(fr"--({VAR_NAME_RE_STR})")
+                if LIGHT_DARK_VALUE_REGEX.match(original):
+                    source_vars.append(name)
+            elif LIGHT_DARK_VALUE_REGEX.match(value):
+                source_vars.append(name)
 
         output_file = Path("./css-variables", source_path)
         with output_file.open("w", encoding="utf-8", newline="\n") as f:
@@ -77,23 +49,8 @@ def main():
             f.write(f"@use \"../{source_path.name}\" as *;\n\n")
             # Assuming variable files to be in app/styles
             f.write(":root {\n")
-            for name, value in source_vars.items():
-                if value.startswith("var"):
-                    value = css_var_value_regex.search(value).group(1)
-                    # Replace CSS variables with source Sass variables
-                elif value in source_vars:
-                    original = value
-                    while original in source_vars:
-                        original = source_vars[original]
-                        # Follow up to original value
-
-                    if original.startswith("var"):
-                        value = css_var_value_regex.search(value).group(1)
-                    else:
-                        value = name
-                else:
-                    value = name
-                f.write(f"\t@include generateVarAndRgb(--{name}, ${value});\n")
+            for name in source_vars:
+                f.write(f"\t@include generateVarAndRgb(--{name[1:]}, {name});\n")
             f.write("}\n")
 
 if __name__ == "__main__":
