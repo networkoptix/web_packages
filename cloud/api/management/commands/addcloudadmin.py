ddc149fea89fcd5a57935805f562d25cc3090327
Nick Hartleb <nhartleb@networkoptix.com>
2021-11-24 18:41:29 -0800

    CLOUD-8278: Updated addcloudadmin command to run

--- cloud/api/management/commands/addcloudadmin.py
+++ cloud/api/management/commands/addcloudadmin.py
@@ -1,13 +1,15 @@
-import os
 import time
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from api.controllers import cloud_api
+from api.helpers.exceptions import APINotFoundException, APINotAuthorisedException
 from api.models import Account
 from notifications.models import Message
 
 SLEEP_TIMER = 10
+CHECK_N_TIMES = 30
 
 
 class Command(BaseCommand):
@@ -18,9 +20,6 @@ class Command(BaseCommand):
         parser.add_argument("password", type=str)
 
     def handle(self, *args, **options):
-        if Account.objects.first() and not os.environ.get("LOCAL_ENV"):
-            raise RuntimeError(
-                "This command should only be run if no accounts exists")
         if not (email := options.get('email', '')):
             raise ValueError("Missing email!")
 
@@ -28,24 +27,36 @@ class Command(BaseCommand):
             raise ValueError("Missing password!")
 
         first, last, *_ = email
-        cloud_api.Account.register("", email, password, first, last)
-        account = Account.objects.create(email=email, first_name=first, last_name=last,
-                                         is_superuser=True, is_staff=True, is_active=True)
-        self.stdout.write(self.style.SUCCESS(
-            f"Successfully added user with {email} for email."))
-
-        self.stdout.write(self.style.NOTICE(
-            f"Waiting for {SLEEP_TIMER}s to activate the account."))
-        time.sleep(SLEEP_TIMER)
-
-        message = Message.objects.filter(user_email__iexact=email).last()
-        if message:
-            cloud_api.Account.activate(message.message.get("code"))
+        account, _ = Account.objects.get_or_create(email=email, first_name=first, last_name=last,
+                                                   is_superuser=True, is_staff=True, is_active=True)
+        try:
+            cloud_api.Account.register(email, password, first, last)
             self.stdout.write(self.style.SUCCESS(
-                f"Successfully activated {email}."))
+                f"Successfully created user with {email} for email."))
+        except APINotFoundException:
+            cloud_api.Account.reactivate(email)
+            self.stdout.write(self.style.WARNING(
+                f"User with {email} already exists."))
+
+        for _ in range(CHECK_N_TIMES):  # SLEEP_TIMER * CHECK_N_TIMES = time waiting for email (Currently 300s)
+            message = Message.objects.filter(user_email__iexact=email).last()
+            if message:
+                try:
+                    cloud_api.Account.activate(message.message.get("code").replace("%3D", "=", 3))
+                    account.activated_date = timezone.now()
+                    account.save()
+                    self.stdout.write(self.style.SUCCESS(
+                        f"Successfully activated {email}."))
+                except APINotAuthorisedException:
+                    self.stdout.write(self.style.WARNING(
+                        f"{email} was already activated ."))
+                break
+
+            self.stdout.write(self.style.NOTICE(
+                f"Waiting for {SLEEP_TIMER}s to activate the account."))
+            time.sleep(SLEEP_TIMER)
         else:
-            account.delete()
             self.stdout.write(
-                self.style.ERROR(f"Failed to activate {email}. Deleting the account."
-                                 f"Please try again. If it continues to fail please contact support.")
+                self.style.ERROR(f"Failed to activate {email}. Please check your inbox for {email} or try again. "
+                                 f"If it continues to fail please contact support.")
             )
