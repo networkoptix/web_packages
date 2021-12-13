168ddfb36b22382d59e2eab189062e7d7bf73601
Illya Artemchuk <iartemchuk@networkoptix.com>
2021-12-13 15:05:04 -0800

    CLOUD-8423: Clear old response before showing new one

--- front_end/app/src/pages/api-tool/swagger/swagger.component.ts
+++ front_end/app/src/pages/api-tool/swagger/swagger.component.ts
@@ -219,7 +219,11 @@ export class NxSwaggerComponent implements OnChanges {
         // Clicking on execute or try-it-out/cancel button triggers a rerender
         const buttons = this.document.querySelectorAll('.try-out__btn, .opblock-control__btn');
         for (const button of buttons as any) {
-            fromEvent(button, 'click').pipe(take(1), untilDestroyed(this)).subscribe(() => {
+            fromEvent(button, 'click').pipe(take(1), untilDestroyed(this)).subscribe((event: any) => {
+                if (event?.target?.classList.contains('execute')) {
+                    const clearBtn: HTMLButtonElement = this.document.querySelector('.btn-clear');
+                    clearBtn?.click(); // CLOUD-8423, clear the response if the previous one is showing, then generate a new one
+                }
                 if (!this.customComponentsRendering) {
                     this.addCustomChanges();
                 }
