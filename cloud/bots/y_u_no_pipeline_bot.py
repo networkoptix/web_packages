22bb7fba8c7b66c33d93684e0b5303833722807e
Chris Czach <czach@networkoptix.com>
2022-01-22 18:56:26 -0800

    move out depend

--- cloud/bots/y_u_no_pipeline_bot.py
+++ cloud/bots/y_u_no_pipeline_bot.py
@@ -1,4 +1,5 @@
 import os
+from discord_users import user_lookup
 
 import discord
 
@@ -7,21 +8,6 @@ GUILD = os.getenv('DISCORD_GUILD')
 GITLAB_USER_EMAIL = os.getenv('GITLAB_USER_EMAIL')
 CI_PROJECT_URL = os.getenv('CI_PROJECT_URL')
 CI_MERGE_REQUEST_IID = os.getenv('CI_MERGE_REQUEST_IID')
-
-user_lookup = {
-    'alvovsky': 651124929189904399,
-    'awu': 884875999987261501,
-    'ckang': 641051765328052274,
-    'czach': 243412893234757632,
-    'iartemchuk': 846488198104088676,
-    'gbezyuk': 587670385793695766,
-    'kamilb': 545036247366631425,
-    'kepperson': 152986701063520257,
-    'nhartleb': 167031417128222721,
-    'oazulay': 862000690188779520,
-    'rbarsegian': 554749942137094164,
-    'ttsolov': 407616388895866901
-}
 
 SAD_MR = f'{CI_PROJECT_URL}/-/merge_requests/{CI_MERGE_REQUEST_IID}'
 TARGET_USER = user_lookup.get(GITLAB_USER_EMAIL.split('@')[0])
