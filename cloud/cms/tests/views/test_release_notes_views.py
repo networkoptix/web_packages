a4c44cae01286ff7aa84827a1039b62bcc023a86
Chris Czach <czach@networkoptix.com>
2022-01-26 04:02:52 +0300

    CLOUD-8607: Release Notes Serializer

--- cloud/cms/tests/views/test_release_notes_views.py
+++ cloud/cms/tests/views/test_release_notes_views.py
@@ -5,14 +5,17 @@ from cms.models import AssetType
 
 from cms.views.release_notes import RELEASE_NOTES_DRAFT_FORBIDDEN, RELEASE_NOTES_FORBIDDEN, RELEASE_NOTES_NOT_FOUND, RELEASE_NOTES_REVIEW_FORBIDDEN, get_release_note, get_release_notes
 
+
 class TestReleaseNotes:
     @pytest.fixture(autouse=True)
     def setup(self, account_factory, asset_factory, db):
-        self.release_notes_quantity = randint(1,10)
+        self.release_notes_quantity = randint(1, 10)
 
         self.user = account_factory()
-        self.non_superuser = account_factory(email='non@super.com', is_superuser=False)
-        self.unrelated_user =  account_factory(email='unrelated@user.com', is_superuser=False)
+        self.non_superuser = account_factory(
+            email='non@super.com', is_superuser=False)
+        self.unrelated_user = account_factory(
+            email='unrelated@user.com', is_superuser=False)
         self.release_notes = list(asset_factory(
             qty=self.release_notes_quantity, account=self.user, asset_type=AssetType.ASSET_TYPES.release_notes))
         self.existing_release_note_id = self.release_notes[0].id
@@ -30,33 +33,39 @@ class TestReleaseNotes:
 
     def test_get_release_note_404(self, arf):
         non_existant_release_note_id = 0
-        response = self.get_release_note(arf=arf, asset_id=non_existant_release_note_id, user=self.user)
+        response = self.get_release_note(
+            arf=arf, asset_id=non_existant_release_note_id, user=self.user)
         assert response.status_code == status.HTTP_404_NOT_FOUND
         assert response.data == RELEASE_NOTES_NOT_FOUND
 
     def test_get_release_note_success(self, arf):
-        response = self.get_release_note(arf=arf, asset_id=self.existing_release_note_id, user=self.user)
+        response = self.get_release_note(
+            arf=arf, asset_id=self.existing_release_note_id, user=self.user)
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 1
         assert response.data[0]['id'] == self.existing_release_note_id
 
     def test_get_release_note_forbidden_anonymous_user_draft(self, arf):
-        response = self.get_release_note(arf=arf, asset_id=self.existing_release_note_id, draft=True) # No user
+        response = self.get_release_note(
+            arf=arf, asset_id=self.existing_release_note_id, draft=True)  # No user
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.data == RELEASE_NOTES_FORBIDDEN
 
     def test_get_release_note_forbidden_anonymous_user_pending(self, arf):
-        response = self.get_release_note(arf=arf, asset_id=self.existing_release_note_id, pending=True) # No user
+        response = self.get_release_note(
+            arf=arf, asset_id=self.existing_release_note_id, pending=True)  # No user
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.data == RELEASE_NOTES_FORBIDDEN
 
     def test_get_release_note_forbidden_authenticated_user_draft(self, arf):
-        response = self.get_release_note(arf=arf, asset_id=self.existing_release_note_id, draft=True, user=self.non_superuser)
+        response = self.get_release_note(
+            arf=arf, asset_id=self.existing_release_note_id, draft=True, user=self.non_superuser)
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.data == RELEASE_NOTES_DRAFT_FORBIDDEN
 
     def test_get_release_note_forbidden_authenticated_user_review(self, arf):
-        response = self.get_release_note(arf=arf, asset_id=self.existing_release_note_id, pending=True, user=self.non_superuser)
+        response = self.get_release_note(
+            arf=arf, asset_id=self.existing_release_note_id, pending=True, user=self.non_superuser)
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.data == RELEASE_NOTES_REVIEW_FORBIDDEN
 
@@ -76,4 +85,5 @@ class TestReleaseNotes:
     def test_get_release_notes_success_superuser(self, arf):
         response = self.get_release_notes(arf=arf, user=self.user)
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data['data']) == self.release_notes_quantity * 2 # double for drafts
+        # double for drafts
+        assert len(response.data['data']) == self.release_notes_quantity * 2
