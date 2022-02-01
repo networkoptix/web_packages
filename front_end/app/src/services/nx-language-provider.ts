02b9f68bcdec957eb4c9a7e2b6d26f78e224208e
Nick Hartleb <nhartleb@networkoptix.com>
2022-02-01 12:05:09 -0800

    CLOUD-8653: Added May to full month names

--- front_end/app/src/services/nx-language-provider.ts
+++ front_end/app/src/services/nx-language-provider.ts
@@ -99,7 +99,7 @@ export class NxLanguageProviderService {
         const timelineTranslations = this.translations.view.timeline;
         const times = {
             dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-            monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'January', 'February', 'March', 'April', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
             timeNames: ['a', 'p', 'am', 'pm', 'A', 'P', 'AM', 'PM']
         };
         const translations: any = {
