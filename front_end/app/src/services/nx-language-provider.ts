d9020373e7bbda25306679214f4615c8831e0378
Nick Hartleb <nhartleb@networkoptix.com>
2022-02-02 09:36:06 -0800

    CLOUD-8653: Removed extra May that was being inserted into translations

--- front_end/app/src/services/nx-language-provider.ts
+++ front_end/app/src/services/nx-language-provider.ts
@@ -107,7 +107,6 @@ export class NxLanguageProviderService {
             monthNames: reduceTranslations(timelineTranslations.monthNames, times.monthNames),
             timeNames: reduceTranslations(timelineTranslations.timeNames, times.timeNames)
         };
-        translations.monthNames.splice(4, 0, timelineTranslations.monthNames.May());
         return translations;
     }
 
