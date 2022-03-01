6f46cb43d5ca93de6e2ebb63e4307cd46de9cd41
Chris Czach <czach@networkoptix.com>
2022-03-01 08:24:36 +0300

    Updated translations (committed by Jenkins)

--- cloud/cms/controllers/documentation.py
+++ cloud/cms/controllers/documentation.py
@@ -65,7 +65,7 @@ class SearchableCache(BaseCache):
         self.current_settings = None
         super().__init__(*args, **kwargs)
         client = get_meilisearch_client()
-        self.search_index = client.index(self.cache_key)
+        self.search_index = client.get_or_create_index(self.cache_key)
         self.fields_from_doc = self.custom_settings.pop('fields')
 
     # @ignore_index_not_found
