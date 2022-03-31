a6d8bcf31b6f5145f844b060f48daa32f14d9b0d
Andrew Wu <awu@networkoptix.com>
2022-03-31 23:11:48 +0300

    CLOUD-8795: Fix 2FA redirect

--- front_end/app/src/routeGuards/twofaGuard.ts
+++ front_end/app/src/routeGuards/twofaGuard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import {
     CanActivate,
     ActivatedRouteSnapshot,
@@ -6,10 +6,12 @@ import {
     UrlTree,
     Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { NxAccountService, Account } from '@services/account.service';
 import { NxSystemsService, NxSystemWithUserInfo } from '@services/systems.service';
+import { WINDOW } from '@services/window-provider';
 
 @Injectable({
     providedIn: 'root'
@@ -19,25 +21,40 @@ export class TwofaGuard implements CanActivate {
         private router: Router,
         private accountService: NxAccountService,
         private systemsService: NxSystemsService,
-    ) { }
+        @Inject(WINDOW) private window: Window,
+    ) {}
 
     canActivate(
         route: ActivatedRouteSnapshot,
         _state: RouterStateSnapshot
     ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        return this.accountService.get(true).then((account: Account) => {
-            const systemSubscription = this.systemsService.systemsSubject
+        const canActivateSubject = new Subject<boolean>();
+        this.accountService.get(true).then((account: Account) => {
+            this.systemsService.systemsSubject
+                .pipe(take(1))
                 .subscribe((systems: NxSystemWithUserInfo[]) => {
-                    setTimeout(() => systemSubscription.unsubscribe());
                     const { systemId } = route.params;
                     const system = systems.find(system => system.id === systemId);
                     if (system?.system2faEnabled && !account.totpExistsForAccount) {
-                        this.router.navigate(['twofa-required'], {
-                            queryParams: { systemName: system.name }
-                        });
+                        const noRedirect = this.window.location.href.endsWith(
+                            `twofa-required?systemName=${system.name}`
+                        );
+                        if (!noRedirect) {
+                            canActivateSubject.complete();
+                            this.router.navigate(
+                                ['twofa-required'],
+                                { queryParams: { systemName: system.name } }
+                            );
+                        } else {
+                            canActivateSubject.next(false);
+                            canActivateSubject.complete();
+                        }
+                    } else {
+                        canActivateSubject.next(true);
+                        canActivateSubject.complete();
                     }
                 });
-            return true;
         });
+        return canActivateSubject;
     }
 }
