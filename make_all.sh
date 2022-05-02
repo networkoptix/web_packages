5d01e78e00a4750b563fdb1a2a1881272b3d390e
Nick Hartleb <nhartleb@networkoptix.com>
2022-05-02 23:41:36 +0300

    Added nginx for system groups.

--- make_all.sh
+++ make_all.sh
@@ -13,3 +13,7 @@ popd
 pushd "$SCRIPT_DIR/deploy/system_groups"
   ./make.sh publish || { echo 'Building system_groups failed' ; exit 1; }
 popd
+
+pushd "$SCRIPT_DIR/deploy/system_groups_nginx"
+  ./make.sh publish || { echo 'Building system_groups_nginx failed' ; exit 1; }
+popd
