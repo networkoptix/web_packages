d6b2bef5edbb7ac74568da3d7c159ae2c47c0453
Nick Hartleb <nhartleb@networkoptix.com>
2022-06-22 18:28:11 -0700

    post merge fixes

--- front_end/app/src/services/apply.service/apply.service.ts
+++ front_end/app/src/services/apply.service/apply.service.ts
@@ -289,8 +289,14 @@ export class NxApplyService extends DialogBase {
 
                         Object.keys(item.originalForm).forEach(key => {
                             item.form.form.controls[key].setValue(item.originalForm[key]);
+                            setTimeout(() => {
+                                item.form.form.controls[key].touched = false;
+                                item.form.form.controls[key].pristine = true;
+                            });
                         });
 
+                        item.form.form.touched = false;
+                        item.form.form.pristine = true;
                         item.discard && item.discard();
                     }
                 });
