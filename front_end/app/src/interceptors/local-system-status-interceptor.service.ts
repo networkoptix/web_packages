2d4a1dad45eada1c8e04afe935c160e41fbdb31e
Tsanko Tsolov <ttsolov@networkoptix.com>
2022-06-29 18:43:36 -0700

    CLOUD-9120: Cloud user isn't logged out from webadmin due to lost internet

--- front_end/app/src/interceptors/local-system-status-interceptor.service.ts
+++ front_end/app/src/interceptors/local-system-status-interceptor.service.ts
@@ -65,14 +65,29 @@ export class LocalSystemStatusInterceptor implements HttpInterceptor {
         const offlineStatus = [504, 502, 0].includes(status);
         const errorStatus = [504, 502, 0].includes(this.appState.lastErrorStatus$.value);
 
-        if (res instanceof HttpErrorResponse && offlineStatus && offlineStatus !== errorStatus) {
+        if (
+            res instanceof HttpErrorResponse &&
+            offlineStatus &&
+            offlineStatus !== errorStatus
+        ) {
+            // Don't show overlay if we're showing session dialog
+            if (this.isDialogActive) {
+                return;
+            }
+
             this.appState.lastErrorStatus$.next(status);
             this.appState.systemAvailable$.next(false);
-        } else if (res instanceof HttpErrorResponse && [401].includes(status)) {
+        } else if (
+            res instanceof HttpErrorResponse &&
+            (status === 401 || status === 422 && res.url.includes('rest/v1/login/sessions')) ||
+            (status === 0 && res.url?.includes('oauth/token'))
+        ) {
             // Session expired
             if (this.isDialogActive) {
                 return;
             }
+            // remove overlay if visible
+            this.appState.systemAvailable$.next(true);
             this.isDialogActive = true;
             return this.dialogService.expiredSession()
                 .then(() => this.window.location.reload());
