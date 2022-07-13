4e23d611071f29834f0a58d337fe98fe6592248b
Nick Hartleb <nhartleb@networkoptix.com>
2022-07-13 09:30:07 +0300

    CLOUD-9280: Added a BUILD arg to the nginx Dockerfile

--- deploy/common.sh
+++ deploy/common.sh
@@ -53,7 +53,7 @@ function copy_deps()
 function pack()
 {
     echo "Packing $MODULE:$VERSION to a container"
-    local COMMON_BUILD_ARGS=(--build-arg VERSION="$VERSION" --build-arg REVISION="$REVISION" --build-arg BUILD_DATE="$BUILD_DATE" --build-arg BUILD_HOST="$BUILD_HOST" --build-arg BUILD_USER="$BUILD_USER")
+    local COMMON_BUILD_ARGS=(--build-arg VERSION="$VERSION" --build-arg REVISION="$REVISION" --build-arg BUILD_DATE="$BUILD_DATE" --build-arg BUILD_HOST="$BUILD_HOST" --build-arg BUILD_USER="$BUILD_USER" --build-arg BUILD_NUMBER="$BUILD_NUMBER")
 
     grep 'ARG.*DOCKER_REGISTRY' Dockerfile >& /dev/null && COMMON_BUILD_ARGS=("${COMMON_BUILD_ARGS[@]}" --build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY")
 
