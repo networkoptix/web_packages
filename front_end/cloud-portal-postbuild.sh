1fca721e9af3658f80a5629d6de2df3c97f68a03
Nick Hartleb <nhartleb@networkoptix.com>
2022-07-21 11:35:06 -0700

    Fixes for svg icons and css.

--- front_end/cloud-portal-postbuild.sh
+++ front_end/cloud-portal-postbuild.sh
@@ -5,7 +5,7 @@ rm ./dist/styles/*.scss;
 [ -e ./dist/styles/native-theme ] && rm -rf ./dist/styles/native-theme
 # target only main style
 mv ./dist/*.css ./dist/styles;
-sed -i -e 's/href="static\//href="static\/styles\//g' dist/index.html;
+sed -i -e 's/href="static\/styles\./href="static\/styles\/styles\./g' dist/index.html;
 
 mv ./dist/*.js ./dist/scripts;
 sed -i -e 's/src="static\//type="text\/javascript" src="static\/scripts\//g' dist/index.html;
@@ -16,7 +16,7 @@ if [ $IS_WEBADMIN ]; then
     cp ./app/customization/webadmin_logo.png ./dist/images/logo.png
 else
     cp ./dist/index.html ./dist/index.mustache.html
-    sed -i -d 's/<title><\/title>/<title>{{title}}<\/title>/g'
+    sed -i -d 's/<title></title>\/<title>{{title}}</title>/'
     sed -i -e 's/<meta name="description" content="">/{% for property, value in meta %}<meta name="{{property}}" content="{{value}}" property="og:{{property}}">{% endfor %}/' ./dist/index.mustache.html
 fi
 
