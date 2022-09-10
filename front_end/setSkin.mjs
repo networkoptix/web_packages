c3f38dd8fd453799a2d90d605a690005cae21bb8
Nick Hartleb <nhartleb@networkoptix.com>
2022-09-09 19:20:33 -0700

    Fixed setSkin script app -> scr

--- front_end/setSkin.mjs
+++ front_end/setSkin.mjs
@@ -7,14 +7,14 @@ const __dirname = path.dirname(__filename);
 
 const copySkin = (color) => {
     var source = path.resolve(__dirname, '../skins', color, 'front_end/styles');
-    var dest = path.resolve(__dirname, 'app/styles/custom');
+    var dest = path.resolve(__dirname, 'src/styles/custom');
     fsmv.copy(source, dest, { mkdirp: true }, error => error ? console.log(error) : null);
 
     const inlineWizardDest = path.resolve(__dirname, 'inline-wizard/customization/custom');
     fsmv.copy(source, inlineWizardDest, { mkdirp: true }, error => error ? console.log(error) : null);
 
     source = path.resolve(__dirname, '../skins', color, 'front_end/images');
-    dest = path.resolve(__dirname, 'app/images');
+    dest = path.resolve(__dirname, 'src/images');
     fsmv.copy(source, dest, { mkdirp: true }, error => error ? console.log(error) : null);
 };
 
