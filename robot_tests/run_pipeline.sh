a280bc3d0c49822141b1effbf9e79308cab4827f
George Sovetov <gsovetov@networkoptix.com>
2022-10-05 18:33:46 +0300

    Create venv in script and use Python from venv

--- robot_tests/run_pipeline.sh
+++ robot_tests/run_pipeline.sh
@@ -6,15 +6,19 @@ FRONTEND_REVISION="${1:?First parameter must be a frontend revision}"
 
 : "${API_TOKEN:?Provide manually or from Jenkins credentials}"
 
+python3.8 -m venv venv
+PYTHON=venv/bin/python
+"$PYTHON" -m pip install -r requirements.txt --no-deps
+
 set +e
 
 BACKEND_REVISION='master'
-DEPLOYMENT_JOB_URL=$(python3 -m 'request_cloud_deployment' "$BACKEND_REVISION" "$FRONTEND_REVISION")
-CLOUD_HOST=$(python3 -m 'wait_for_cloud' "$DEPLOYMENT_JOB_URL")
+DEPLOYMENT_JOB_URL=$("$PYTHON" -m 'request_cloud_deployment' "$BACKEND_REVISION" "$FRONTEND_REVISION")
+CLOUD_HOST=$("$PYTHON" -m 'wait_for_cloud' "$DEPLOYMENT_JOB_URL")
 if [ ! "$CLOUD_HOST" ]; then
   echo >&2 "Failed to deploy cloud with backend revision '$BACKEND_REVISION'" \
     "and frontend revision '$FRONTEND_REVISION'"
-  CLOUD_HOST=$(python3 -m 'get_cloud_host' "$DEPLOYMENT_JOB_URL")
+  CLOUD_HOST=$("$PYTHON" -m 'get_cloud_host' "$DEPLOYMENT_JOB_URL")
   if [ ! "$CLOUD_HOST" ]; then
     echo >&2 "Cloud is not deployed. There is nothing to remove"
     exit 1
@@ -23,7 +27,7 @@ if [ ! "$CLOUD_HOST" ]; then
   set -e
 
   echo >&2 "Remove failed deployment '$CLOUD_HOST'"
-  python3 -m 'remove_cloud' "$CLOUD_HOST"
+  "$PYTHON" -m 'remove_cloud' "$CLOUD_HOST"
   exit 1
 fi
 
@@ -32,8 +36,8 @@ pabot --pabotlib --processes 4 -e integrations -L trace:info \
 TESTS_RESULT_CODE=$?
 
 # BUILD_URL is an environment variable provided by Jenkins.
-python3 -m 'post_run_status' "$FRONTEND_REVISION" "$TESTS_RESULT_CODE" "$BUILD_URL"
+"$PYTHON" -m 'post_run_status' "$FRONTEND_REVISION" "$TESTS_RESULT_CODE" "$BUILD_URL"
 
 set -e
 
-python3 -m 'remove_cloud' "$CLOUD_HOST"
+"$PYTHON" -m 'remove_cloud' "$CLOUD_HOST"
