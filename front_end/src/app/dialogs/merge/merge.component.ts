384533c64d84391ea3673a77745abf6162fcec31
Cliff Kang <ckang@networkoptix.com>
2022-10-17 21:58:46 +0300

    Resolve CLOUD-9226 "X/ merge post verify"

--- front_end/src/app/dialogs/merge/merge.component.ts
+++ front_end/src/app/dialogs/merge/merge.component.ts
@@ -400,6 +400,9 @@ export class MergeModalContent {
         this.loginService.updateSession('merge')
             .then(ready => {
                 this.updateSession = !ready;
+                if (ready) {
+                    process.run();
+                }
             });
     }
 
