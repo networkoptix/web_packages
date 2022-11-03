b1ed97fbad93251beeac4eec108d370ac238dce9
Nick Hartleb <nhartleb@networkoptix.com>
2022-11-03 10:36:31 +0300

    Created DataStructure for linking to Theme Feature flag.

--- front_end/src/app/services/theme.service.ts
+++ front_end/src/app/services/theme.service.ts
@@ -50,7 +50,7 @@ export class NxThemeService {
                     console.error('Feature not available', err);
                 });
         } else {
-            this.themeSelected = 'auto';
+            this.themeSelected = 'dark';
         }
 
         this.setTheme(this.themeSelected, loginState);
