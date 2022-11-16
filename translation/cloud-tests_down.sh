4ccb19229989344fc885b92c8ea5d37067110429
Sergei Ivanov <sivanov@networkoptix.com>
2022-11-16 13:30:10 +0300

    Update scripts for manual work with translations
    (cherry picked from commit e9c0a7457e3bffe0627300c0104d1720e1f2f398)

--- translation/cloud-tests_down.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-crowdin download -b cloud_20.1 --config crowdin-cloud-autotests.yaml --ignore-match $@
--- translation/cloud-tests_up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-crowdin upload sources -b cloud_20.1 --config crowdin-cloud-autotests.yaml
