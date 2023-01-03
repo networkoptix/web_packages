d60fcecd177bfc880a0f7085ce7e17110c20f5d4
Alex Lvovsky <alvovsky@networkoptix.com>
2023-01-03 12:04:37 -0600

    removing old smoke check folders

--- robot_tests/smoke_check/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ "$1" == *"nxvms.com"* ]]
-then
-    pabot --pabotlib --outputdir smoke_check/test_results -e merge_and_licenses -v ENV:$1 -v VMS:$2 smoke_check/tests
-else
-    pabot --pabotlib --outputdir smoke_check/test_results -e relays -v ENV:$1 -v VMS:$2 smoke_check/tests
-fi
