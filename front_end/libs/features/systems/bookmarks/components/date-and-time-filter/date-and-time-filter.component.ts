47fbc66475a59337e4ca63fa9f0dedefa145ea50
Andrew Wu <awu@networkoptix.com>
2023-01-13 03:51:55 +0300

    CLOUD-9778: Update bookmarks date filter

--- front_end/libs/features/systems/bookmarks/components/date-and-time-filter/date-and-time-filter.component.ts
+++ front_end/libs/features/systems/bookmarks/components/date-and-time-filter/date-and-time-filter.component.ts
@@ -1,7 +1,17 @@
-import { Component, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
-import { DateRange as DR } from '@angular/material/datepicker';
+import {
+    Component,
+    ViewEncapsulation,
+    Input,
+    Output,
+    EventEmitter,
+    HostListener,
+    ViewChild,
+    Inject,
+} from '@angular/core';
+import { DateRange as DR, MatCalendar } from '@angular/material/datepicker';
 
 import { icons } from '@lib/variables/static-variables';
+import { WINDOW } from '@services/window-provider';
 
 import type { TimeRange } from '../../bookmarks.types';
 
@@ -23,9 +33,37 @@ export class NxDateAndTimeFilterComponent {
     @Input() timeRange: TimeRange;
     @Output() timeRangeChange = new EventEmitter<TimeRange>();
 
+    @ViewChild('matCalendar') private matCalendar: MatCalendar<Date>;
+
     icons = icons;
 
-    preview: DateRange | null = null;
+    private hoveredDate: Date | null = null;
+    private hoverTimeout: number;
+    quickPreview: DateRange | null = null;
+
+    private get singleDayRange(): boolean {
+        return this.dateRange &&
+            this.dateRange.start.toString() === this.dateRange.end.toString();
+    }
+
+    // Comparison range start date must be before end
+    get comparisonStart(): Date | null {
+        if (!this.singleDayRange || !this.hoveredDate) {
+            return;
+        }
+        return this.dateRange.start.getTime() > this.hoveredDate.getTime()
+            ? this.hoveredDate
+            : this.dateRange.start;
+    }
+
+    get comparisonEnd(): Date | null {
+        if (!this.singleDayRange || !this.hoveredDate) {
+            return;
+        }
+        return this.dateRange.start.getTime() < this.hoveredDate.getTime()
+            ? this.hoveredDate
+            : this.dateRange.start;
+    }
 
     get lastDay(): DateRange {
         const now = new Date();
@@ -44,6 +82,46 @@ export class NxDateAndTimeFilterComponent {
         return new DR(thirtyDaysAgo, now);
     }
 
+    constructor(@Inject(WINDOW) private window: Window) {
+    }
+
+    /* <mat-calendar> doesn't include a way to detect when a cell is
+    hovered over, so we have to manually check with mouseover/mouseout */
+    @HostListener('mouseover', ['$event'])
+    setHoverPreview(e: MouseEvent): void {
+        if (
+            this.matCalendar.currentView === 'month' &&
+            (e.target as HTMLElement).classList.contains('mat-calendar-body-cell-content') &&
+            this.singleDayRange
+        ) {
+            clearTimeout(this.hoverTimeout);
+            const target = e.target as HTMLDivElement;
+            const day = Number(target.textContent.trim());
+            // .activeDate is always in displayed month
+            const year = this.matCalendar.activeDate.getFullYear();
+            const month = this.matCalendar.activeDate.getMonth();
+            this.hoveredDate = new Date(year, month, day);
+        }
+    }
+
+    /* Because the calendar days aren't completely flush, moving from
+    day to day results in an unpleasant flickering as the hover date is
+    repeatedly cleared and set. Adding a timeout makes so that as long
+    as the mouse is moving quickly enough, the hover date will only be
+    cleared after moving the mouse away from the calendar */
+    @HostListener('mouseout', ['$event'])
+    removeHoverPreview(e: MouseEvent): void {
+        if (
+            this.matCalendar.currentView === 'month' &&
+            this.hoveredDate &&
+            (e.target as HTMLElement).classList.contains('mat-calendar-body-cell-content')
+        ) {
+            this.hoverTimeout = this.window.setTimeout(() => {
+                this.hoveredDate = null;
+            }, 200);
+        }
+    }
+
     quickSelectMatch(range: DateRange): boolean {
         if (!this.dateRange) {
             return false;
@@ -59,11 +137,25 @@ export class NxDateAndTimeFilterComponent {
     }
 
     selectedChange(selected: Date): void {
-        this.dateRangeChange.emit(new DR(selected, selected));
+        if (!this.dateRange) {
+            this.dateRangeChange.emit(new DR(selected, selected));
+        } else if (
+            this.singleDayRange &&
+            selected.toString() !== this.dateRange.start.toString()
+        ) {
+            const newRange = selected.getTime() > this.dateRange.start.getTime()
+                ? new DR(this.dateRange.start, selected)
+                : new DR(selected, this.dateRange.start);
+            this.dateRangeChange.emit(newRange);
+        } else {
+            this.dateRangeChange.emit(new DR(selected, selected));
+        }
+        this.hoveredDate = null;
     }
 
     clear(): void {
         this.dateRangeChange.emit(null);
+        this.hoveredDate = null;
     }
 
     setStartTime(time: number | null): void {
