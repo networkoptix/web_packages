67ee0b0b62380bcf78ec874151e3119b255b9418
Kyle Epperson <kepperson@networkoptix.com>
2023-01-18 07:59:02 -0800

    added ssl verifcation to getting and setting flags, and addressed some linting

--- robot_tests/NoptixLibrary/FeatureFlagListener.py
+++ robot_tests/NoptixLibrary/FeatureFlagListener.py
@@ -11,15 +11,14 @@ class FeatureFlagListener:
         expected_settings = cloud.set_feature_flags()
         expected_settings_converted = {}
         for setting in expected_settings.keys():
-            new_key = setting.replace(' ', '')
-            new_key = new_key[0].lower() + new_key[1:]
+            new_key = setting[0].lower() + setting[1:].replace(' ', '')
             expected_settings_converted[new_key] = expected_settings[setting]
 
-        startTime = time.monotonic()
+        start_time = time.monotonic()
         while True:
             cloud_settings = cloud.get_cloud_settings()
             if cloud_settings["featureFlags"] == expected_settings_converted:
                 break
-            if time.monotonic() - startTime > 120:
+            if time.monotonic() - start_time > 120:
                 raise TimeoutError("Feature flags did not update in time.")
             time.sleep(5)
