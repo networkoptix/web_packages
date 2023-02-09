d3cb9fbf81d8669d5f6642f9d07e9d4a1cba4220
Nick Hartleb <nhartleb@networkoptix.com>
2023-02-08 19:01:30 -0800

    More postmerge fixes
    - Fixes bookmarks-card
    - Fixes lang issue in merge dialog

--- front_end/libs/features/systems/bookmarks/components/card/bookmarks-card.module.ts
+++ front_end/libs/features/systems/bookmarks/components/card/bookmarks-card.module.ts
@@ -1,3 +1,4 @@
+import { AsyncPipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
@@ -8,6 +9,7 @@ import { NxBookmarksCardComponent } from './bookmarks-card.component';
     imports: [
         AngularSvgIconModule.forRoot(),
         LazyLoadImageModule,
+        AsyncPipe
     ],
     declarations: [
         NxBookmarksCardComponent,
