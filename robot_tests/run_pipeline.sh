579e45f09f72f03aba916b5555c949f11b9e03ab
Vladimir Isimetov <visimetov@networkoptix.com>
2023-02-09 17:05:15 +0100

    Fix checking of env variables from MR event

--- robot_tests/run_pipeline.sh
+++ robot_tests/run_pipeline.sh
@@ -6,7 +6,7 @@ FRONTEND_REVISION="${1:?First parameter must be a frontend revision}"
 
 : "${API_TOKEN:?Provide manually or from Jenkins credentials}"
 
-if [ "$MERGE_REQUEST_ACTION" ]; then
+if [ -n "${MERGE_REQUEST_ACTION+x}" ]; then
   case "$MERGE_REQUEST_ACTION" in
     open|reopen|update) ;;
     close|approved|unapproved|approval|unapproval|merge)
@@ -20,7 +20,7 @@ if [ "$MERGE_REQUEST_ACTION" ]; then
   esac
 fi
 
-if [ "$MERGE_REQUEST_SOURCE_WEB_URL" ]; then
+if [ -n "${$MERGE_REQUEST_SOURCE_WEB_URL+x}" ]; then
   echo "Testing changes to merge from the repository $MERGE_REQUEST_SOURCE_WEB_URL"
 fi
 
