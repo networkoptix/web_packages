eb0480a6a7216061102500bb21d1db7d7c21232c
Tsanko Tsolov <ttsolov@networkoptix.com>
2023-02-09 15:23:38 -0800

    CLOUD-10135: New System setup password check doesn't work

--- front_end/apps/setup-wizard/src/app/components/local-login/local-login.component.ts
+++ front_end/apps/setup-wizard/src/app/components/local-login/local-login.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, HostListener, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 
@@ -53,6 +53,15 @@ export class LocalLoginComponent implements AfterViewInit {
     checkPasswords(): void {
         if (this.confirmedPassword !== this.password) {
             this.setAdminPasswordForm.controls.confirmPassword.setErrors({ dontMatch: true });
+        }
+    }
+
+    @HostListener('document:keypress', ['$event'])
+    handleKeyboardEvent(event: KeyboardEvent): void {
+        if (event.key === 'Enter') {
+            if (this.setAdminPasswordForm.form.valid) {
+                this.wizardService.finish();
+            }
         }
     }
 }
