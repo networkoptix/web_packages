a7138746c62b473a8c430a95cc52b6f160f7a3f8
Nick Hartleb <nhartleb@networkoptix.com>
2023-03-08 20:37:21 -0800

    CLOUD-10333: Stripped /web from rest requests

--- front_end/libs/services/system-rest-api.service.ts
+++ front_end/libs/services/system-rest-api.service.ts
@@ -312,7 +312,7 @@ export class NxSystemRestAPI extends NxSystemAPI {
     // Checks if the url does not have swagger-ui in it.
     private requiresWeb(url) {
         // Leaving this method incase we remember what it was used for.
-        return true;
+        return environment.isLocal;
     }
 
     // Legacy api requires runtime in the header of the request.
