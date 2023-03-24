d0431e1fe05c8bad19995781f345b3ac90957b12
Nick Hartleb <nhartleb@networkoptix.com>
2023-03-24 15:43:36 -0700

    CLOUD-10431: Fixed condition when appType was falsy

--- front_end/libs/features/download-history/release/release.component.ts
+++ front_end/libs/features/download-history/release/release.component.ts
@@ -23,7 +23,7 @@ export class ReleaseComponent implements OnInit {
 
     ngOnInit(): void {
         this.release.platforms.forEach(({ files }, i) => {
-            this.release.platforms[i].files = files.filter(({ appType }) => !['_benchmark', '_debug', '_refs', '_update'].some(partialAppType => appType.includes(partialAppType)));
+            this.release.platforms[i].files = files.filter(({ appType }) => !['_benchmark', '_debug', '_refs', '_update'].some(partialAppType => appType?.includes(partialAppType)));
         });
         this.cardExpanded = Object.fromEntries(
             this.release.platforms.map(p => [p.name, false])
