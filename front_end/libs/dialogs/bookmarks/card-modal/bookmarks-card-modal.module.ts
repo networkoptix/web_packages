534e5c56201254a5e7c15de0375bf359301942d0
Vincent Lee <vlee@networkoptix.com>
2023-03-30 22:15:37 +0300

    CLOUD-10314: Replace bookmarks video error text with Placeholder

--- front_end/libs/dialogs/bookmarks/card-modal/bookmarks-card-modal.module.ts
+++ front_end/libs/dialogs/bookmarks/card-modal/bookmarks-card-modal.module.ts
@@ -4,6 +4,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { ClipModule } from '@components/clip/clip.module';
+import { PlayerPlaceholderModule } from '@components/placeholders/player/player-placeholder.module';
 
 import { ProcessButtonModule } from '../../../components/process-button/process-button.module';
 
@@ -23,6 +24,7 @@ import { NxBookmarksCardModalComponent } from './bookmarks-card-modal.component'
         ClipModule,
         ProcessButtonModule,
         TranslateModule,
+        PlayerPlaceholderModule,
     ]
 })
 export class NxBookmarksCardModalModule {}
