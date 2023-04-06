051df9ae345a488b25cd228abe8a026dfa006157
Nick Hartleb <nhartleb@networkoptix.com>
2023-04-06 13:37:19 -0700

    Adding account caching back for reducing db load

--- deploy/cloud_portal/docker/entrypoint.sh
+++ deploy/cloud_portal/docker/entrypoint.sh
@@ -112,7 +112,7 @@ do
             fi
             python manage.py update_host
             python manage.py filldata
-            python manage.py filldata --preview=True &
+#            python manage.py filldata --preview=True &  # Removing for now
 
             find /app/app/static | xargs touch
             exec gunicorn cloud.asgi:application --capture-output --workers ${PORTAL_WORKERS} --bind :5000 --log-level=debug --timeout 300 -k uvicorn.workers.UvicornWorker
