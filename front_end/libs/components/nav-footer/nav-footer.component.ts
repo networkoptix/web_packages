c46fc5ea1161d0f233ba287f1081e58d318fdcc3
Ilya Artemchuk <iartemchuk@networkoptix.com>
2023-05-01 20:49:06 +0300

    CLOUD-10447,CLOUD-10062: Fix footer translations

--- front_end/libs/components/nav-footer/nav-footer.component.ts
+++ front_end/libs/components/nav-footer/nav-footer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { BehaviorSubject, filter } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject, filter, take } from 'rxjs';
 
 import { environment } from '@environments/environment';
 import { icons } from '@lib/variables/static-variables';
@@ -30,7 +31,14 @@ export class NxNavFooterComponent implements OnInit {
         private menusService: NxMenusService,
         private router: Router,
         public scrollMechanicsService: NxScrollMechanicsService,
+        translateService: TranslateService,
     ) {
+        translateService.onTranslationChange.pipe(untilDestroyed(this)).subscribe(() => {
+            setTimeout(() => {
+                this.getFooterMenu();
+            });
+        });
+
         this.inAuthorization = this.router.url.includes('/authorize');
         this.CONFIG = config.getConfig();
 
@@ -45,20 +53,26 @@ export class NxNavFooterComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.menusService.getMenu('footer').subscribe(footer => {
-            this.footerItems = this.menusService.cleanEmptyNodes(footer.nodes);
-            if (environment.isLocal) {
-                this.footerItems.forEach(footerItem => {
-                    footerItem.new_window = true;
-                    footerItem.url = footerItem.url.replace(
-                        '{{CLOUD_HOST}}',
-                        this.CONFIG.cloudHost,
-                    );
-                });
-            }
-        });
-
+        this.getFooterMenu();
         this.checkVisible(this.router.url);
+    }
+
+    getFooterMenu(): void {
+        this.menusService
+            .getMenu('footer')
+            .pipe(take(1))
+            .subscribe(footer => {
+                this.footerItems = this.menusService.cleanEmptyNodes(footer.nodes);
+                if (environment.isLocal) {
+                    this.footerItems.forEach(footerItem => {
+                        footerItem.new_window = true;
+                        footerItem.url = footerItem.url.replace(
+                            '{{CLOUD_HOST}}',
+                            this.CONFIG.cloudHost,
+                        );
+                    });
+                }
+            });
     }
 
     scrollToTop(): void {
