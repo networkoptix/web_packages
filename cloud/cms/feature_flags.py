b2ec38fd1ef87b1cbb67831bad1727161b945164
Chris Czach <czach@networkoptix.com>
2023-05-31 13:56:28 -0700

    Fix Feature Flag Typo

--- cloud/cms/feature_flags.py
+++ cloud/cms/feature_flags.py
@@ -81,7 +81,7 @@ class FLAGS(metaclass=_FlagType):
     layouts_ptz_control = ('Layouts PTZ Control', 'layoutsPtz', '%LAYOUTS_PTZ_ENABLED%')
     layouts_demo = ('Layouts Demo', 'layoutsDemo', '%LAYOUTS_DEMO_ENABLED%')
     layouts_io_devices = ('Layouts IO Devices', 'layoutsIoDevices', '%LAYOUTS_IO_DEVICES_ENABLED%')
-    layouts_non_chrome = ('Layouts Non-Chromium Browsers', 'layoutsNonchrome', '%LAYOUTS_NON_CHROME%')
+    layouts_non_chrome = ('Layouts Non-Chromium Browsers', 'layoutsNonChrome', '%LAYOUTS_NON_CHROME%')
 
     use_json_rpc = ('Use Json Rpc', 'useJsonRpc', '%USE_JSON_RPC%')
 
