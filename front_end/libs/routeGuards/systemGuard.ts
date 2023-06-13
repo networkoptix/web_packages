59d922a3f240cb9dd33836eeb1c14909e0b9c8da
Andrew Wu <awu@networkoptix.com>
2023-06-13 21:12:05 +0300

    CLOUD-10818: Fix offline system access for non-admins

--- front_end/libs/routeGuards/systemGuard.ts
+++ front_end/libs/routeGuards/systemGuard.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import {
     ActivatedRouteSnapshot,
@@ -50,7 +51,7 @@ export class SystemGuard implements CanActivate {
             return true;
         }
 
-        const routesChecked = [
+        const restrictedRoutes = [
             'users',
             'cloud-storage',
             'health',
@@ -61,7 +62,7 @@ export class SystemGuard implements CanActivate {
             'layouts',
             'bookmarks',
         ];
-        const currentRoute = routesChecked.find(route => state.url.includes(route));
+        const currentRoute = restrictedRoutes.find(route => state.url.includes(route));
         const systemId =
             environment.isLocal ||
             route.pathFromRoot.find(snapshot => snapshot.params.systemId).params.systemId;
@@ -148,6 +149,16 @@ export class SystemGuard implements CanActivate {
                 } catch (e) {
                     if (e === 'Media server cloud not be reached.') {
                         const cloudUsers = await currSystem.getUsersCachedInCloud();
+                        if (cloudUsers instanceof HttpErrorResponse && cloudUsers.status === 403) {
+                            // User doesn't have permission to view cloud users
+                            if (currentRoute) {
+                                return this.router.navigate([
+                                    environment.isLocal ? '/settings/' : `/systems/${systemId}`,
+                                ]);
+                            } else {
+                                return true;
+                            }
+                        }
                         currSystem.userManager.processUsers(cloudUsers);
                     } else {
                         throw e;
