947198c4af1e11f6f37621279d956f675f8c2506
Kirill Pisarchik <kpisarchik@networkoptix.com>
2023-06-23 01:44:33 +0300

    CLOUD-10860: Safari - incorrect up/down button styling

--- front_end/libs/components-buildable/src/lib/numeric-input/numeric.component.spec.ts
+++ front_end/libs/components-buildable/src/lib/numeric-input/numeric.component.spec.ts
@@ -1,129 +1,350 @@
+import {
+    ComponentFixture,
+} from '@angular/core/testing';
+
 import { setupComponent } from '../../setup';
 
 import {
     NxNumericComponent
 } from './numeric.component';
 
-function keyEvent(el: HTMLInputElement, key: string, eventType: string): void {
-    const event: KeyboardEvent = new KeyboardEvent(eventType, {
+function dispatchKeyEvent(el: HTMLInputElement, eventType: string, key: string): KeyboardEvent {
+    const event = new KeyboardEvent(eventType, {
         key
     });
     el.dispatchEvent(event);
+
+    return event;
+}
+
+function dispatchInputEvent(el: HTMLInputElement, value = '0'): Event {
+    const event = new Event('input');
+    el.dispatchEvent(event);
+    return event;
+}
+
+function dispatchMouseEvent(el: HTMLElement, eventType: string): MouseEvent {
+    const event = new MouseEvent(eventType);
+    el.dispatchEvent(event);
+    return event;
+}
+
+function createPasteEvent(data: string): ClipboardEvent {
+    const dt = new DataTransfer();
+    const event = new ClipboardEvent('paste', { clipboardData: dt });
+    event.clipboardData.setData('text/plain', data);
+
+    return event;
+}
+
+function dispatchPasteEvent(el: HTMLInputElement): Event {
+    // JSDOM does not support `ClipboardEvent` in version <= 22
+    const event:Event = new Event('paste');
+
+    el.dispatchEvent(event);
+
+    return event;
 }
 
 describe('NumericComponent', () => {
-    it('should create NumericComponent', async () => {
-        const { component } = await setupComponent(NxNumericComponent);
+    let component: NxNumericComponent;
+    let fixture: ComponentFixture<NxNumericComponent>;
+    let el: HTMLInputElement;
+    let up: HTMLInputElement;
+    let down: HTMLInputElement;
+    const baseValue = 7011;
+    const minValue = 1;
+    const maxValue = 65535;
+
+    beforeEach(async () => {
+        ({ component, fixture } = await setupComponent(NxNumericComponent));
+
+        el = fixture.debugElement.nativeElement.querySelector('input');
+        up = fixture.debugElement.nativeElement.querySelector('.spinner-button.spinner-button--increment');
+        down = fixture.debugElement.nativeElement.querySelector('.spinner-button.spinner-button--decrement');
+        el.value = `${baseValue}`;
+        component._value = baseValue;
+        component.min = minValue;
+        component.max = maxValue;
+        fixture.detectChanges();
+    });
+
+    it('should create NumericComponent', () => {
         expect(component).toBeTruthy();
+        expect(el).toBeDefined();
+        expect(up).toBeDefined();
+        expect(down).toBeDefined();
     });
 
-    it('should call checkValue on keydown', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        const el = fixture.debugElement.nativeElement.querySelector('input');
-        const checkValueSpy = jest.spyOn(component, 'checkValue');
+    describe('Events', () => {
+        it('should call valueChanged on keyup', () => {
+            const spy = jest.spyOn(component, 'valueChanged');
 
-        keyEvent(el, '5', 'keydown');
-        fixture.detectChanges();
-        expect(checkValueSpy.mock.calls.length).toBe(1);
+            dispatchKeyEvent(el, 'keyup', '5');
+            fixture.detectChanges();
+            expect(spy).toBeCalledTimes(1);
+            expect(spy).toBeCalledWith();
+        });
+
+        it('should call valueChanged on input', () => {
+            const spy = jest.spyOn(component, 'valueChanged');
+
+            const event = dispatchInputEvent(el);
+            fixture.detectChanges();
+            expect(spy).toBeCalledTimes(1);
+            expect(spy).toBeCalledWith(event);
+        });
+
+        it('should call onPaste on paste', () => {
+            const spy = jest.spyOn(component, 'onPaste');
+            spy.mockImplementation();
+
+            const event = dispatchPasteEvent(el);
+            fixture.detectChanges();
+            expect(spy).toBeCalledTimes(1);
+            expect(spy).toBeCalledWith(event);
+        });
+
+        it('should call onKeyDown on keydown', () => {
+            const spy = jest.spyOn(component, 'onKeyDown');
+
+            const event = dispatchKeyEvent(el, 'keydown', '5');
+            fixture.detectChanges();
+            expect(spy).toBeCalledTimes(1);
+            expect(spy).toBeCalledWith(event);
+        });
+
+        it('should call increment on mousedown', () => {
+            const increment = jest.spyOn(component, 'increment');
+            const decrement = jest.spyOn(component, 'decrement');
+
+            const mouseEvent = dispatchMouseEvent(up, 'mousedown');
+            fixture.detectChanges();
+
+            expect(increment).toBeCalledTimes(1);
+            expect(increment).toBeCalledWith(mouseEvent);
+            expect(decrement).not.toHaveBeenCalled();
+        });
+        it('should call decrement on mousedown', () => {
+            const increment = jest.spyOn(component, 'increment');
+            const decrement = jest.spyOn(component, 'decrement');
+
+            const mouseEvent = dispatchMouseEvent(down, 'mousedown');
+            fixture.detectChanges();
+
+            expect(decrement).toBeCalledTimes(1);
+            expect(decrement).toBeCalledWith(mouseEvent);
+            expect(increment).not.toHaveBeenCalled();
+        });
     });
 
-    it('should call setValue on keyup', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        const el = fixture.debugElement.nativeElement.querySelector('input');
-        const setValueSpy = jest.spyOn(component, 'setValue');
+    describe('getNativeValue', () => {
+        it('returns value of an input as number', () => {
+            el.value = '700';
 
-        keyEvent(el, '5', 'keyup');
-        fixture.detectChanges();
-        expect(setValueSpy.mock.calls.length).toBe(1);
+            expect(component.getNativeValue()).toBe(700);
+        });
+        it('returns null of an input as empty', () => {
+            el.value = '';
+
+            expect(component.getNativeValue()).toBe(null);
+        });
     });
 
-    it('should check and accept valid value', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        const el = fixture.debugElement.nativeElement;
-        // key event from the test doesn't not change the value so here we'll mimic it
-        component._value = 701;
-        keyEvent(el, '5', 'keyup');
-        fixture.detectChanges();
+    describe('onKeyDown', () => {
+        const preventDefault = jest.fn();
+        const getEvent = (char: string): KeyboardEvent => ({
+            key: char,
+            preventDefault
+        } as unknown as KeyboardEvent);
 
-        expect(component._value).toBe(701);
+        Array.from('0123456789').map(char =>
+            it(`should prevent event if a digit ${char} is received`, () => {
+                component.onKeyDown(getEvent(char));
+                expect(preventDefault).not.toHaveBeenCalled();
+            })
+        );
+        Array.from('abe-+/]."').map(char =>
+            it(`should prevent event if a digit ${char} is received`, () => {
+                component.onKeyDown(getEvent(char));
+                expect(preventDefault).toHaveBeenCalled();
+            })
+        );
     });
 
-    it('should check and correct a value greater than max', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        component.min = 69;
-        component.max = 420;
-        const el = fixture.debugElement.nativeElement.querySelector('input');
-        component._value = component.max + 1;
-        keyEvent(el, '80085', 'keyup');
-        fixture.detectChanges();
+    describe('valueChanged', () => {
+        let setValue: jest.SpyInstance;
+        let getNativeValue: jest.SpyInstance;
+        let checkUpdateNativeValue: jest.SpyInstance;
 
-        expect(component._value).toBe(component.max);
+        beforeEach(async () => {
+            checkUpdateNativeValue = jest.spyOn(component, 'checkUpdateNativeValue').mockImplementation();
+            setValue = jest.spyOn(component, 'setValue').mockImplementation();
+            getNativeValue = jest.spyOn(component, 'getNativeValue');
+            getNativeValue.mockReturnValue(baseValue);
+        });
+
+        it('calls setValue with nativeValue when called internally', () => {
+            component.valueChanged();
+
+            expect(getNativeValue).toBeCalledTimes(1);
+            expect(getNativeValue).toBeCalledWith();
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(baseValue);
+            expect(checkUpdateNativeValue).toBeCalledTimes(0);
+        });
+
+        it('calls setValue with nativeValue when called as a listener', () => {
+            const mockEvent = new Event('input');
+            component.valueChanged(mockEvent);
+
+            expect(getNativeValue).toBeCalledTimes(1);
+            expect(getNativeValue).toBeCalledWith();
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(baseValue);
+            expect(checkUpdateNativeValue).toBeCalledTimes(1);
+            expect(checkUpdateNativeValue).toBeCalledWith(baseValue, mockEvent.target);
+        });
     });
 
-    it('should check and correct a value less than min', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        component.min = 69;
-        component.max = 420;
-        const el = fixture.debugElement.nativeElement.querySelector('input');
-        component._value = component.min - 1;
-        keyEvent(el, '42', 'keyup');
-        fixture.detectChanges();
+    describe('checkUpdateNativeValue', () => {
+        let input : { value: string };
+        let getNativeValue: jest.SpyInstance;
 
-        expect(component._value).toBe(component.min);
+        beforeEach(() => {
+            input = {
+                value: `${baseValue}`
+            };
+            getNativeValue = jest.spyOn(component, 'getNativeValue');
+            getNativeValue.mockReturnValue(baseValue);
+        });
+
+        it('does nothing if native and private values match', () => {
+            component.checkUpdateNativeValue(baseValue, input as HTMLInputElement);
+
+            expect(input.value).toEqual(`${baseValue}`);
+        });
+        it('sets new value for event target if native and private values do not match', () => {
+            component.checkUpdateNativeValue(baseValue - 1, input as HTMLInputElement);
+
+            expect(input.value).toEqual(`${baseValue - 1}`);
+        });
     });
 
-    it('should check and reject invalid value onPaste', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        const dt = new DataTransfer();
-        const event = new ClipboardEvent('paste', { clipboardData: dt });
-        event.clipboardData?.setData('text/plain', 'test');
+    describe('increment and decrement', () => {
+        let event: MouseEvent;
+        let preventDefault: jest.SpyInstance;
+        let setValue: jest.SpyInstance;
 
-        component.onPaste(event);
-        fixture.detectChanges(); // NaN here
-        fixture.detectChanges();
+        beforeEach(() => {
+            preventDefault = jest.fn();
+            event = { preventDefault } as unknown as MouseEvent;
+            setValue = jest.spyOn(component, 'setValue');
+        });
 
-        expect(component._value).toBe(component._previousValue);
+        it('increments native value and updates internal value', () => {
+            component.increment(event);
+            fixture.detectChanges();
+
+            expect(preventDefault).toBeCalledTimes(1);
+            expect(preventDefault).toBeCalledWith();
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(baseValue + 1);
+        });
+        it('decrement native value and updates internal value', () => {
+            component.decrement(event);
+            fixture.detectChanges();
+
+            expect(preventDefault).toBeCalledTimes(1);
+            expect(preventDefault).toBeCalledWith();
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(baseValue - 1);
+        });
     });
 
-    it('should check and sanitize invalid value onPaste', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        const dt = new DataTransfer();
-        const event = new ClipboardEvent('paste', { clipboardData: dt });
-        event.clipboardData?.setData('text/plain', '70-e3');
+    describe('onPaste', () => {
+        let setValue: jest.SpyInstance;
 
-        component.onPaste(event);
-        fixture.detectChanges();
+        beforeEach(() => {
+            setValue = jest.spyOn(component, 'setValue');
+        });
 
-        expect(component._value).toBe(703);
+        it('should check and set partially valid value', () => {
+            component.onPaste(createPasteEvent('70-e3'));
+
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(703);
+        });
+
+        it('should check and set invalid value', () => {
+            component.onPaste(createPasteEvent('test'));
+
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(NaN);
+        });
+
+        it('should check and set valid value', () => {
+            component.onPaste(createPasteEvent(`${baseValue}`));
+
+            expect(setValue).toBeCalledTimes(1);
+            expect(setValue).toBeCalledWith(baseValue);
+        });
     });
 
-    it('should check and reject invalid value on valueChange', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        component._previousValue = 420;
-        component.min = 69;
-        component.max = 80085;
-        const event = { target: { value: 1 } };
-        component.valueChanged(event);
-        fixture.detectChanges();
+    describe('setValue', () => {
+        let onTouchedCallback: jest.SpyInstance;
+        let onChangeCallback: jest.SpyInstance;
+        let onChangeEmit: jest.SpyInstance;
 
-        expect(component._value).toBe(component._previousValue);
+        beforeEach(() => {
+            onTouchedCallback = jest.spyOn(component, <never>'onTouchedCallback');
+            onChangeCallback = jest.spyOn(component, <never>'onChangeCallback');
+            onChangeEmit = jest.spyOn(component.onChange, 'emit');
+        });
 
-        event.target.value = component.max + 1;
-        component.valueChanged(event);
-        fixture.detectChanges();
+        const expectSetValue = (value: number | null): void => {
+            expect(onTouchedCallback).toBeCalledTimes(1);
+            expect(onTouchedCallback).toBeCalledWith();
+            expect(onChangeCallback).toBeCalledTimes(1);
+            expect(onChangeCallback).toBeCalledWith(value);
+            expect(onChangeEmit).toBeCalledTimes(1);
+            expect(onChangeEmit).toBeCalledWith(value);
+        };
 
-        expect(component._value).toBe(component._previousValue);
-    });
+        it('should update value', () => {
+            const newValue = baseValue + 1;
+            component.setValue(newValue);
 
-    it('should check and accept valid value on valueChange', async () => {
-        const { component, fixture } = await setupComponent(NxNumericComponent);
-        component.min = 69;
-        component.max = 80085;
-        const value = 420;
-        const event = { target: { value } };
-        component.valueChanged(event);
-        fixture.detectChanges();
+            expectSetValue(newValue);
+        });
 
-        expect(component._value).toBe(value);
+        it('should update null value', () => {
+            const newValue: null = null;
+            component.setValue(newValue);
+
+            expectSetValue(newValue);
+        });
+
+        it('should check and correct a value less than min', () => {
+            const newValue = minValue - 1;
+            component.setValue(newValue);
+
+            expectSetValue(minValue);
+        });
+
+        it('should check and correct a value greater than max', () => {
+            const newValue = maxValue + 1;
+            component.setValue(newValue);
+
+            expectSetValue(maxValue);
+        });
+
+        it('should check and reject NaN', () => {
+            component.setValue(NaN);
+
+            expect(onTouchedCallback).not.toHaveBeenCalled();
+            expect(onChangeCallback).not.toHaveBeenCalled();
+            expect(onChangeEmit).not.toHaveBeenCalled();
+        });
     });
 });
