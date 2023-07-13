8b87adfb35f8576f27e3cb578dc3f737a591356c
Kyle Epperson <kepperson@networkoptix.com>
2023-07-13 20:08:25 +0300

    X/update changepass cases

--- PythonRobot/change-pass.py
+++ PythonRobot/change-pass.py
@@ -21,14 +21,16 @@ rb = RobotVariables("en_US")
 
 def can_be_accessed_via_dropdown():
     driver = get_headless_chrome()
+    email = resource.get_random_email()
+    register_and_activate_account(driver, "Mark", "Hamill", email, "qweasd 123")
     robot_keywords.go_to_url(driver, ENV)
     robot_keywords.wait_until_element_is_visible(driver, rb.LOG_IN_NAV_BAR)
     robot_keywords.click_element(driver, rb.LOG_IN_NAV_BAR)
 
-    LoginDialog(driver).basic_cloud_login("noptixautoqa+viewer@gmail.com", "qweasd 123")
+    LoginDialog(driver).basic_cloud_login(email, "qweasd 123")
     header = HeaderNav(driver)
     header.account_dropdown().click()
-    header.change_password().click()
+    header.change_password_option().click()
 
     ChangePassForm(driver).verify_form_is_visible()
     robot_keywords.close_browser(driver)
@@ -37,8 +39,10 @@ def can_be_accessed_via_dropdown():
 
 def can_be_accessed_via_direct_url():
     driver = get_headless_chrome()
+    email = resource.get_random_email()
+    register_and_activate_account(driver, "Mark", "Hamill", email, "qweasd 123")
     robot_keywords.go_to_url(driver, f"{ENV}/account/password")
-    LoginDialog(driver).basic_cloud_login("noptixautoqa+viewer@gmail.com", "qweasd 123")
+    LoginDialog(driver).basic_cloud_login(email, "qweasd 123")
     ChangePassForm(driver).verify_form_is_visible()
     robot_keywords.close_browser(driver)
     print("pass")
@@ -74,23 +78,29 @@ def password_is_actually_changed_and_login_works_with_new_password():
 
 def password_with_symbols_is_valid():
     driver = get_headless_chrome()
+    email = resource.get_random_email()
+    register_and_activate_account(driver, "Mark", "Hamill", email, "qweasd 123")
     robot_keywords.go_to_url(driver, f"{ENV}/account/password")
+    LoginDialog(driver).basic_cloud_login(email, "qweasd 123")
+
+    change_pass_form = ChangePassForm(driver)
+    change_pass_form.current_password_input().input_text("qweasd 123")
+    change_pass_form.new_password_input().input_text('''pass!@#$%^&*()_-+=;:'"`~,./\|?[]{}''')
+    change_pass_form.save_button().click()
+
+    time.sleep(1)
+    HeaderNav(driver).log_out()
+    LandingPage(driver)
+
+    header = HeaderNav(driver)
+    header.log_in_button().click()
     login_dialog = LoginDialog(driver)
-    login_dialog.basic_cloud_login("noptixautoqa+viewer@gmail.com", "qweasd 123")
-    robot_keywords.wait_until_elements_are_visible(driver, [CURRENT_PASSWORD_INPUT, NEW_PASSWORD_INPUT])
-    robot_keywords.input_text(driver, CURRENT_PASSWORD_INPUT, password)
-    robot_keywords.input_text(driver, NEW_PASSWORD_INPUT, SYMBOL_PASSWORD)
-    robot_keywords.wait_until_elements_are_visible(driver, [CHANGE_PASSWORD_BUTTON, CANCEL_PASSWORD_CHANGES_BUTTON])
-    robot_keywords.click_button(driver, CHANGE_PASSWORD_BUTTON)
-    robot_keywords.elements_should_not_be_visible(driver, [CHANGE_PASSWORD_BUTTON, CANCEL_PASSWORD_CHANGES_BUTTON])
-    robot_keywords.wait_until_element_is_visible(driver, CHANGE_PASS_NO_CHANGES)
-    resource.log_out_cloud(driver)
-    robot_keywords.go_to_url(driver, f"{ENV}/account/password")
-    cloud_login(driver, "noptixautoqa+advviewer@gmail.com", SYMBOL_PASSWORD, button=None)
-    resource.log_out_cloud(driver)
-    robot_keywords.go_to_url(driver, f"{ENV}/account/password")
-    cloud_login(driver, "noptixautoqa+viewer@gmail.com", "qweasd 123", button=None, validate=False)
-    robot_keywords.wait_until_element_is_visible(driver, rb.WRONG_PASSWORD_MESSAGE)
+    login_dialog.email_input().input_text(email)
+    login_dialog.next_button().click()
+    login_dialog.password_input().input_text("qweasd 123")
+    login_dialog.login_button().click()
+    login_dialog.password_input_error_message()
+
     robot_keywords.close_browser(driver)
     print("pass")
 
@@ -98,3 +108,4 @@ if __name__ == "__main__":
     can_be_accessed_via_dropdown()
     can_be_accessed_via_direct_url()
     password_is_actually_changed_and_login_works_with_new_password()
+    password_with_symbols_is_valid()
