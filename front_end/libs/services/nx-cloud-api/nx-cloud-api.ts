0660f219a49cca519fb08f4c1b865c1adb1b2aea
Nick Hartleb <nhartleb@networkoptix.com>
2023-07-26 09:07:08 -0700

    CLOUD-11097, CLOUD-11098: Shortened memoization from 60s - 5s
    # Problem
    The memoization was longer than the system tick so the UI would lag behind a few update tickets

--- front_end/libs/services/nx-cloud-api/nx-cloud-api.ts
+++ front_end/libs/services/nx-cloud-api/nx-cloud-api.ts
@@ -874,7 +874,7 @@ export class NxCloudApiService {
     }
 
     /* Ownership transfer */
-    @memoizeAsyncLong
+    @memoizeAsyncShort
     getTransfers(): Observable<t.SystemTransferInfo[]> {
         return this.http.get<t.SystemTransferInfo[]>(`${apiBase}/transfer/`);
     }
