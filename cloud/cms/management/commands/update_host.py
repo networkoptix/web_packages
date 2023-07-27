5dbcccf1136d7b266e1319a1cd2d00e32a63ff94
Roman Barsegian <rbarsegian@networkoptix.com>
2023-07-28 02:56:17 +0300

    Fixes for private cloud deployment

--- cloud/cms/management/commands/update_host.py
+++ cloud/cms/management/commands/update_host.py
@@ -3,7 +3,8 @@ import logging
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from cms.models import Customization
+from cms.controllers import structure
+from cms.models import Customization, Language
 from util import config
 
 
@@ -13,7 +14,37 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
-            '--customization', nargs='?', default='default', type=str)
+            '--customization', nargs='?', type=str)
+        parser.add_argument(
+            '--host', nargs='?', type=str)
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS(f"Not needed anymore"))
+        customization = options['customization']
+        conf = config.get_config()
+        host = conf["cloud_portal"]["url"]
+        host = options.get('host', host)
+        current_customization = Customization.objects.filter(
+            name=customization).first()
+
+        if not current_customization:
+            en_us = Language.objects.get(code="en_US")
+            current_customization = Customization(
+                name=customization, default_language=en_us)
+            current_customization.save()
+            current_customization.languages.add(en_us)
+            current_customization.save()
+
+            structure.find_or_add_asset_with_single_customization(
+                'Cloud Portal', current_customization, 'cloud_portal', '')
+
+        if host.startswith('http://'):
+            host = host[7:]
+        elif host.startswith('https://'):
+            host = host[8:]
+
+        if current_customization.host != host:
+            current_customization.host = host
+            current_customization.save()
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Host for {customization} updated to {host}'))
---
+++ cloud/cms/tests/management/commands/test_update_host.py
@@ -0,0 +1,41 @@
+import pytest
+from unittest.mock import call
+from random import randint
+from uuid import uuid4
+
+from cms.management.commands.update_host import *
+
+
+class TestUpdateHost:
+    def test_handle(self, mocker, db):
+        options = {'customization': settings.TEST_CUSTOMIZATION}
+        instance = Command()
+        current_customization = Customization.objects.filter(
+            name=settings.TEST_CUSTOMIZATION).first()
+        mock_write_stdout = mocker.patch.object(instance.stdout, 'write')
+        expected_host = str(uuid4())
+
+        customization = settings.TEST_CUSTOMIZATION
+
+        # Test host updated
+        instance.handle(**options, host=expected_host)
+        mock_write_stdout.assert_called_with(
+            instance.style.SUCCESS(
+                f'Host for {customization} updated to {expected_host}'))
+        assert Customization.objects.get(
+            id=current_customization.id).host == expected_host
+
+        # Test already correct host
+        instance.handle(**options, host=expected_host)
+        mock_write_stdout.assert_called_with(
+            instance.style.SUCCESS(
+                f'Host for {customization} updated to {expected_host}'))
+        assert Customization.objects.get(
+            id=current_customization.id).host == expected_host
+
+        # Test customization not found
+        customization = str(uuid4())
+        instance.handle(customization=customization, host=expected_host)
+        mock_write_stdout.assert_called_with(
+            instance.style.ERROR(
+                f'Host for {customization} updated to {expected_host}'))
