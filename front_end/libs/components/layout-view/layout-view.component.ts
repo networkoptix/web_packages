be590bc61bdf444cf7a2acdeabd1edbe230ad2ff
Chris Czach <czach@networkoptix.com>
2023-08-02 00:19:37 +0300

    CLOUD-11138: Update layouts sort to  match thick client

--- front_end/libs/components/layout-view/layout-view.component.ts
+++ front_end/libs/components/layout-view/layout-view.component.ts
@@ -53,7 +53,7 @@ import { NxSystem } from '@services/system.service/system';
 import { NxSystemServer } from '@services/system.service/system-types';
 import { NxSystemService } from '@services/system.service/system.service';
 import { NxSystemsService } from '@services/systems.service';
-import { alphabeticalSort, cleanId, pickFrom } from '@utils/general';
+import { alphaNumericSort, alphabeticalSort, cleanId, pickFrom } from '@utils/general';
 import { generateTour, translateStep } from '@utils/nx';
 
 interface Resource {
@@ -299,10 +299,11 @@ export class NxLayoutViewComponent {
                     }),
                     {} as ResourceLookup<typeof webPages[0]>,
                 );
-                const byName = alphabeticalSort<Pick<Resource, 'name'>>(
+                const byName = alphaNumericSort<Pick<Resource, 'name'>>(
                     this.locale,
                     r => r.name || '',
                 );
+
                 const layoutsForTree = layouts
                     .filter(layout => layout.id && layout.id !== 'new')
                     .filter(layout =>
@@ -316,7 +317,8 @@ export class NxLayoutViewComponent {
                         shared: details.parentId === '{00000000-0000-0000-0000-000000000000}',
                         type: ResourceType.LAYOUT,
                         details,
-                    }));
+                    }))
+                    .sort((a, b) => (a.shared === b.shared ? byName(a, b) : a.shared ? -1 : 1));
                 const parsedResources = {
                     ...parsedServers,
                     ...parsedCameras,
