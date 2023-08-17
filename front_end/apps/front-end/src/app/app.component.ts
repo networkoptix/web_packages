576a53ff6a99432bf1f2555b6be16186b1ed83a4
Nick Hartleb <nhartleb@networkoptix.com>
2023-08-16 19:33:02 -0700

    CLOUD-11204: Fixing issue where initTheme fails on initial load

--- front_end/apps/front-end/src/app/app.component.ts
+++ front_end/apps/front-end/src/app/app.component.ts
@@ -331,14 +331,13 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.themeService.initTheme().then(() => {
+        this.themeService.initTheme().finally(() => {
             this.themeSet = true;
             setTimeout(() => {
                 this.initComponents();
                 this.initScroll();
             });
-        },
-        () => { });
+        });
     }
 
     headerResize(size: { width: number; height: number }): void {
