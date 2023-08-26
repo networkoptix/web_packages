503c2dfa70a7e6b0f0db7dd42315966ae49351ef
Alex Lvovsky <alvovsky@networkoptix.com>
2023-08-25 22:43:26 -0500

    QR code test

--- PythonRobot/security_form.py
+++ PythonRobot/security_form.py
@@ -6,6 +6,7 @@ from checkbox import Checkbox
 from RobotVariables import RobotVariables
 from variables import ENV
 from NoptixLibrary.Cloud2fa import Cloud2fa
+from generic_element import Element
 import random
 
 class SecurityForm:
@@ -54,13 +55,17 @@ class SecurityForm:
     def twofa_ok_button(self):
         return Button(self.driver, f"{self.twofa_modal}//button[@id='wizardDone']")
     
-    def turn_on_2fa(self, password):
+    def turn_on_2fa(self, password, qr_code=False):
         self.twofa_enable_button().click()
         self.twofa_password_modal_input().input_text(password)
         self.twofa_password_modal_next_button().click()
-        self.twofa_code_button().click()
-        key = self.twofa_key().text.strip()
-        self.twofa_key_modal_next_button().click()
+        if qr_code:
+            key = self._get_key_from_qr_code()
+            self.twofa_key_modal_next_button().click()
+        else:    
+            self.twofa_code_button().click()
+            key = self.twofa_key().text.strip()
+            self.twofa_key_modal_next_button().click()
         robot_keywords.sleep(5)
         totp = Cloud2fa().get_2fa_verification_code(key)
         self.twofa_totp_input().input_text(totp)
@@ -73,6 +78,10 @@ class SecurityForm:
         twofa_codes = {"totp" : totp, "backup": randomOneTimeBackupCode}
         return twofa_codes
     
+    def _get_key_from_qr_code(self):
+        Element(self.driver, "//nx-two-fa-modal-content//qr-code").get_selenium_element().screenshot('qr_code.png')
+        return Cloud2fa().decode_qr('qr_code.png')
+        
     def _wait_until_form_is_visible(self):
         self.twofa_enable_button()
         self.twofa_disabled_badge()
--- PythonRobot/test_2fa.py
+++ PythonRobot/test_2fa.py
@@ -66,7 +66,27 @@ def login_with_backup_code():
     CloudPortalAPI().toggle_2fa_off_api(SERVERS[0]['cloudOwner'], password, verification_code=twofa_codes['totp'])
     driver.close()
 
+def login_with_qr_code():
+    """3. Enable and perform login with 2fa using QR"""
+    driver = get_headless_chrome()
+    robot_keywords.go_to_url(driver, ENV)
+    header = HeaderNav(driver)
+    header.log_in_button().click()
+    LoginDialog(driver).basic_cloud_login(SERVERS[0]['cloudOwner'], password)
+    header.account_dropdown().click()
+    header.security_option().click()
+    security_form = SecurityForm(driver)    
+    twofa_codes = security_form.turn_on_2fa(password, qr_code=True)
+    security_form.twofa_enabled_badge()
+    header.log_out()
+    header.log_in_button().click()
+    LoginDialog(driver).twofa_cloud_login(SERVERS[0]['cloudOwner'], password, twofa_codes['totp'])
+    header.account_dropdown().click()
+    CloudPortalAPI().toggle_2fa_off_api(SERVERS[0]['cloudOwner'], password, verification_code=twofa_codes['totp'])
+    driver.close()
+
 if __name__ == "__main__":
     enable_and_login_with_2fa()
     login_with_backup_code()
+    login_with_qr_code()
     keywords.teardown_servers(SERVERS)
