1d09bb080a0fc9718a90adaf33386b6b7646abaf
Kimberly Vandiver <kvandiver@networkoptix.com>
2023-09-08 22:17:10 +0300

    CLOUD-11220 - added showList boolean to prevent system list page from showing...
    # Problem
    When a user with only one system logs in, the list component is shown before redirecting to the system details page.
    
    # Solution
    -Added conditional logic to the template using the pre-existing hasOneSystem variable. If hasOneSystem is true, then the div won't render and the user will be immediately redirected to the systems page.
    
    # Notes
    - this MR is a duplicate of the original MR due to rebasing issues.
    - link to original MR: https://gitlab.nxvms.dev/dev/cloud_portal/-/merge_requests/4471

--- front_end/libs/components/systems-list/list.component.ts
+++ front_end/libs/components/systems-list/list.component.ts
@@ -45,6 +45,7 @@ export class NxSystemsListComponent implements OnInit {
     endpoint: Endpoint = {};
     hasOneSystem: boolean;
     searchChanged = new Subject<void>();
+    showList: boolean = false;
 
     static SYSTEMS_BASE = '/systems';
 
@@ -107,6 +108,8 @@ export class NxSystemsListComponent implements OnInit {
                         if (!system.system2faEnabled || account.sessionVerified) {
                             this.openSystem(system);
                         }
+                    } else {
+                        this.showList = true;
                     }
 
                     this.showSearch = this.systems.length >= search.minSystems;
