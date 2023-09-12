4fe9d5402c9896cf09094ec3c2e8539e80ab5375
Valentin Shebanov <vshebanov@networkoptix.com>
2023-09-12 12:22:21 +0200

    Remove trailing whitespsaces from footer.py

--- PythonRobot/footer.py
+++ PythonRobot/footer.py
@@ -13,39 +13,38 @@ class Footer:
 
     def api_documentation_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.API_DOCUMENTATION_TEXT}')]")
-    
+
     def api_download_sdk_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.DOWNLOAD_SDK_TEXT}')]")
-    
+
     def support_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.SUPPORT}')]")
-    
+
     def privacy_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.PRIVACY}')]")
-    
+
     def copyright_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.COPYRIGHT_SYMBOL}')]/span[contains(text(),'{self.rb.COMPANY}')]/parent::a")
-    
+
     def integrations_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.INTEGRATIONS_TITLE_TEXT}')]")
-    
+
     def known_limits_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.KNOWN_LIMITATIONS}')]")
-    
+
     def about_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.ABOUT}')]")
-    
+
     def supported_devices_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.SUPPORTED_DEVICES}')]")
-    
+
     def terms_link(self):
         return Button(self.driver, f"//nx-nav-footer//a[contains(text(),'{self.rb.TERMS}')]")
-
 
     def _wait_until_footer_is_visible_webadmin(self):
         self.api_documentation_link()
         self.api_download_sdk_link()
-        self.support_link()       
+        self.support_link()
         self.copyright_link()
 
     def _wait_until_footer_is_visible_cloud(self):
