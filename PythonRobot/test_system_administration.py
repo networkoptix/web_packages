67566dbb8abf573395c31baf6902ad42fc774cc2
Anna Klimova <aklimova@networkoptix.com>
2023-09-12 17:41:07 +0200

    Use Suite() in system administration tests

--- PythonRobot/test_system_administration.py
+++ PythonRobot/test_system_administration.py
@@ -1,35 +1,29 @@
-import sys
 import time
-import os
-from selenium import webdriver
 
-from resource_import import get_headless_chrome, register_and_activate_account, get_random_email
-from NoptixLibrary.CloudPortalAPI import CloudPortalAPI
-from variables import ENV
 import robot_keywords
-from RobotVariables import RobotVariables
-from login import LoginDialog
+from NoptixLibrary.CloudPortalAPI import CloudPortalAPI
+from NoptixLibrary.suite import CloudServer
+from NoptixLibrary.suite import Suite
 from header import HeaderNav
-from systems_page import SystemsPage
-
+from login import LoginDialog
+from resource_import import get_headless_chrome
+from resource_import import get_random_email
+from resource_import import register_and_activate_account
 from system_admin import SystemAdmin
 from system_left_menu import SystemLeftMenu
-
-from NoptixLibrary.generic_keywords import GenericKeywords
-from RobotVariables import RobotVariables
+from systems_page import SystemsPage
+from variables import ENV
 
 password = "qweasd 123"
 
-keywords = GenericKeywords()
-SERVERS = keywords.create_systems(os.path.basename(__file__))
 CLOUD_API = CloudPortalAPI()
 viewer_permissions = 'GlobalViewArchivePermission|GlobalExportPermission|GlobalViewBookmarksPermission|GlobalAccessAllMediaPermission'
 
 
-def can_log_in_to_system_from_direct_link():
+def can_log_in_to_system_from_direct_link(server: CloudServer):
     driver = get_headless_chrome()
-    robot_keywords.go_to_url(driver, ENV + f"/systems/{SERVERS[0]['id']}")
-    LoginDialog(driver).basic_cloud_login(SERVERS[0]['cloudOwner'], password)
+    robot_keywords.go_to_url(driver, ENV + f"/systems/{server.id}")
+    LoginDialog(driver).basic_cloud_login(server.cloud_owner.email, password)
     HeaderNav(driver).account_dropdown()
     SystemAdmin(driver)
 
@@ -37,28 +31,30 @@ def can_log_in_to_system_from_direct_link():
     print("pass")
 
 
-def owner_can_disconnect_system_from_cloud():
+def owner_can_disconnect_system_from_cloud(server: CloudServer):
     driver = get_headless_chrome()
-    robot_keywords.go_to_url(driver, ENV + f"/systems/{SERVERS[1]['id']}")
-    LoginDialog(driver).basic_cloud_login(SERVERS[1]['cloudOwner'], password)
+    robot_keywords.go_to_url(driver, ENV + f"/systems/{server.id}")
+    LoginDialog(driver).basic_cloud_login(server.cloud_owner.email, password)
     HeaderNav(driver).account_dropdown()
 
     sys_admin = SystemAdmin(driver)
     sys_admin.disconnect_from_cloud_button().click()
     sys_admin.disconnect_modal_disconnect_button().click()
     assert sys_admin.disconnect_from_cloud_toast_notification().message().in_dom
-    assert (len(CLOUD_API.get_account_systems(SERVERS[1]['cloudOwner'], password))) == 1, "Number of systems owned " \
+    assert (len(CLOUD_API.get_account_systems(server.cloud_owner.email, password))) == 1, "Number of systems owned " \
                                                                                           "was not 1"
 
     robot_keywords.close_browser(driver)
     print("pass")
 
-def non_owner_can_disconnect_account_from_system():
+
+def non_owner_can_disconnect_account_from_system(server: CloudServer):
     driver = get_headless_chrome()
     email = get_random_email()
     register_and_activate_account(driver, "Mark", "Hamill", email, password)
-    CLOUD_API.share(SERVERS[0]['cloudAuth'], SERVERS[0]['id'], "viewer", email, viewer_permissions)
-    robot_keywords.go_to_url(driver, ENV + f"/systems/{SERVERS[0]['id']}")
+    cloud_auth = (server.cloud_owner.email, server.cloud_owner.password)
+    CLOUD_API.share(cloud_auth, server.id, "viewer", email, viewer_permissions)
+    robot_keywords.go_to_url(driver, ENV + f"/systems/{server.id}")
     LoginDialog(driver).basic_cloud_login(email, password)
     sys_admin = SystemAdmin(driver)
     time.sleep(2)
@@ -67,12 +63,12 @@ def non_owner_can_disconnect_account_from_system():
     sys_admin.disconnect_from_account_cancel_button().click()
     sys_admin.disconnect_from_account_button().click()
     sys_admin.disconnect_from_account_confirm_button().click()
-    assert sys_admin.disconnect_from_account_toast_notification(SERVERS[0]["name"]).message().in_dom
+    assert sys_admin.disconnect_from_account_toast_notification(server.name).message().in_dom
     SystemsPage(driver).no_systems()
     header = HeaderNav(driver)
     header.log_out()
-    robot_keywords.go_to_url(driver, ENV + f"/systems/{SERVERS[0]['id']}")
-    LoginDialog(driver).basic_cloud_login(SERVERS[0]["cloudOwner"], password)
+    robot_keywords.go_to_url(driver, ENV + f"/systems/{server.id}")
+    LoginDialog(driver).basic_cloud_login(server.cloud_owner.email, password)
     left_menu = SystemLeftMenu(driver)
     left_menu.users_button().click()
     left_menu.update_users_list()
@@ -84,10 +80,12 @@ def non_owner_can_disconnect_account_from_system():
     print("pass")
 
 
-
 if __name__ == "__main__":
-    # can_log_in_to_system_from_direct_link()
-    # owner_can_disconnect_system_from_cloud()
-    non_owner_can_disconnect_account_from_system()
-
-    keywords.teardown_servers(SERVERS)
+    with Suite() as suite:
+        cloud_owner_first = suite.create_cloud_account()
+        cloud_server_first = suite.create_cloud_server(cloud_owner_first)
+        # cloud_owner_second = suite.create_cloud_account()
+        # cloud_server_second = suite.create_cloud_server(cloud_owner_second)
+        # can_log_in_to_system_from_direct_link(cloud_server_first)
+        # owner_can_disconnect_system_from_cloud(cloud_server_second)
+        non_owner_can_disconnect_account_from_system(cloud_server_first)
