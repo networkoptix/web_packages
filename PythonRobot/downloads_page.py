5976237b21abeea8e0bdfae0eb8b35d13cdb798d
Valentin Shebanov <vshebanov@networkoptix.com>
2023-09-12 14:20:21 +0200

    Check download tabs for differnet OS

--- PythonRobot/downloads_page.py
+++ PythonRobot/downloads_page.py
@@ -1,4 +1,9 @@
+import time
+
+from selenium.webdriver.common.by import By
+
 import robot_keywords
+from button import Button
 
 
 class DownloadsPage:
@@ -9,3 +14,25 @@ class DownloadsPage:
 
     def _wait_until_loaded(self):
         robot_keywords.wait_until_page_contains_element(self._driver, "//nx-download-component")
+
+    def click_windows_client_installer_tab(self):
+        self._click_tab('//nx-download-component//*[@id="windows"]')
+
+    def click_linux_client_installer_tab(self):
+        self._click_tab('//nx-download-component//*[@id="linux"]')
+
+    def click_mac_client_installer_tab(self):
+        self._click_tab('//nx-download-component//*[@id="macos"]')
+
+    def _click_tab(self, locator):
+        Button(self._driver, locator).click()
+        started_at = time.monotonic()
+        timeout_sec = 5
+        download_button = self._driver.find_element(
+            By.XPATH, '//nx-download-component//a[contains(@class, "download-button")]')
+        while True:
+            if download_button.is_displayed():
+                break
+            if time.monotonic() - started_at > timeout_sec:
+                raise RuntimeError(f"{locator!r} is not visible after {timeout_sec} seconds")
+            time.sleep(0.5)
--- PythonRobot/test_download.py
+++ PythonRobot/test_download.py
@@ -1,3 +1,4 @@
+import robot_keywords
 import variables
 from downloads_page import DownloadsPage
 from footer import Footer
@@ -20,6 +21,22 @@ def check_download_link():
     driver.close()
 
 
+# Make sure each tab changes the text to show the corresponding OS and url
+def check_download_tabs():
+    driver = get_headless_chrome()
+    driver.get(variables.ENV)
+    Footer(driver, 'cloud').get_downloads_link().click()
+    download_page = DownloadsPage(driver)
+    download_page.click_windows_client_installer_tab()
+    robot_keywords.location_should_be(driver, variables.ENV + '/download/windows')
+    download_page.click_linux_client_installer_tab()
+    robot_keywords.location_should_be(driver, variables.ENV + '/download/linux')
+    download_page.click_mac_client_installer_tab()
+    robot_keywords.location_should_be(driver, variables.ENV + '/download/macos')
+    driver.close()
+
+
 if __name__ == '__main__':
     download_link_is_in_the_footer()
     check_download_link()
+    check_download_tabs()
