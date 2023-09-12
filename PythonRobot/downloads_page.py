2ad1c130f9896faadd8cb50837955b5a02e30c1b
Valentin Shebanov <vshebanov@networkoptix.com>
2023-09-12 17:18:18 +0200

    Test default OS download tab

--- PythonRobot/downloads_page.py
+++ PythonRobot/downloads_page.py
@@ -3,7 +3,6 @@ import time
 from selenium.webdriver.common.by import By
 
 import robot_keywords
-from button import Button
 
 
 class DownloadsPage:
@@ -15,24 +14,40 @@ class DownloadsPage:
     def _wait_until_loaded(self):
         robot_keywords.wait_until_page_contains_element(self._driver, "//nx-download-component")
 
-    def click_windows_client_installer_tab(self):
-        self._click_tab('//nx-download-component//*[@id="windows"]')
+    def get_windows_client_installer_tab(self) -> '_Tab':
+        return self._get_tab('//nx-download-component//*[@id="windows"]')
 
-    def click_linux_client_installer_tab(self):
-        self._click_tab('//nx-download-component//*[@id="linux"]')
+    def get_linux_client_installer_tab(self) -> '_Tab':
+        return self._get_tab('//nx-download-component//*[@id="linux"]')
 
-    def click_mac_client_installer_tab(self):
-        self._click_tab('//nx-download-component//*[@id="macos"]')
+    def get_mac_client_installer_tab(self) -> '_Tab':
+        return self._get_tab('//nx-download-component//*[@id="macos"]')
 
-    def _click_tab(self, locator):
-        Button(self._driver, locator).click()
+    def _get_tab(self, locator):
+        robot_keywords.wait_until_page_contains_element(self._driver, locator, timeout=5)
+        return _Tab(self._driver, locator)
+
+
+class _Tab:
+
+    def __init__(self, driver, locator):
+        self._locator = locator
+        self._element = driver.find_element(By.XPATH, locator)
+
+    def click(self):
+        self._element.click()
         started_at = time.monotonic()
         timeout_sec = 5
-        download_button = self._driver.find_element(
-            By.XPATH, '//nx-download-component//a[contains(@class, "download-button")]')
         while True:
-            if download_button.is_displayed():
+            if self.is_active():
                 break
             if time.monotonic() - started_at > timeout_sec:
-                raise RuntimeError(f"{locator!r} is not visible after {timeout_sec} seconds")
+                raise RuntimeError(f"{self._locator!r} is not visible after {timeout_sec} seconds")
             time.sleep(0.5)
+
+    def is_active(self):
+        return self._get_download_button().is_displayed()
+
+    def _get_download_button(self):
+        return self._driver.find_element(
+            By.XPATH, '//nx-download-component//a[contains(@class, "download-button")]')
--- PythonRobot/test_download.py
+++ PythonRobot/test_download.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 import robot_keywords
 import variables
 from downloads_page import DownloadsPage
@@ -27,16 +31,59 @@ def check_download_tabs():
     driver.get(variables.ENV)
     Footer(driver, 'cloud').get_downloads_link().click()
     download_page = DownloadsPage(driver)
-    download_page.click_windows_client_installer_tab()
+    download_page.get_windows_client_installer_tab().click()
     robot_keywords.location_should_be(driver, variables.ENV + '/download/windows')
-    download_page.click_linux_client_installer_tab()
+    download_page.get_linux_client_installer_tab().click()
     robot_keywords.location_should_be(driver, variables.ENV + '/download/linux')
-    download_page.click_mac_client_installer_tab()
+    download_page.get_mac_client_installer_tab().click()
     robot_keywords.location_should_be(driver, variables.ENV + '/download/macos')
     driver.close()
+
+
+# Going to the downloads page should show you the tab according to your OS
+def check_default_download_tab():
+    windows_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    windows_user_agent_driver = _get_chromedriver(windows_user_agent)
+    windows_user_agent_driver.get(variables.ENV)
+    Footer(windows_user_agent_driver, 'cloud').get_downloads_link().click()
+    download_page = DownloadsPage(windows_user_agent_driver)
+    windows_tab = download_page.get_windows_client_installer_tab()
+    if not windows_tab.is_active():
+        raise RuntimeError(f"Linux tab is not visible with User-Agent {windows_user_agent!r}")
+    windows_user_agent_driver.close()
+    linux_user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    linux_user_agent_driver = _get_chromedriver(linux_user_agent)
+    linux_user_agent_driver.get(variables.ENV)
+    Footer(linux_user_agent_driver, 'cloud').get_downloads_link().click()
+    download_page = DownloadsPage(linux_user_agent_driver)
+    linux_tab = download_page.get_linux_client_installer_tab()
+    if not linux_tab.is_active():
+        raise RuntimeError(f"Linux tab is not visible with User-Agent {linux_user_agent!r}")
+    linux_user_agent_driver.close()
+    mac_user_agent = "Mozilla/5.0  (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+    mac_user_agent_driver = _get_chromedriver(mac_user_agent)
+    mac_user_agent_driver.get(variables.ENV)
+    Footer(mac_user_agent_driver, 'cloud').get_downloads_link().click()
+    download_page = DownloadsPage(mac_user_agent_driver)
+    mac_tab = download_page.get_mac_client_installer_tab()
+    if not mac_tab.is_active():
+        raise RuntimeError(f"Linux tab is not visible with User-Agent {mac_user_agent!r}")
+    mac_user_agent_driver.close()
+
+
+def _get_chromedriver(user_agent):
+    chrome_options = Options()
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument(f"--user-agent={user_agent}")
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_argument("--headless")
+    capabilities = DesiredCapabilities.CHROME
+    capabilities['goog:loggingPrefs'] = {'browser': 'ALL'}
+    return webdriver.Chrome(options=chrome_options, desired_capabilities=capabilities)
 
 
 if __name__ == '__main__':
     download_link_is_in_the_footer()
     check_download_link()
     check_download_tabs()
+    check_default_download_tab()
