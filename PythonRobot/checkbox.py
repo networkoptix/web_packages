a623d413e4d8c29b652b0de771f200c4cf380c02
Ilia Malyshev <imalyshev@networkoptix.com>
2023-09-18 14:44:30 +0200

    Checkbox wrapper: Make fields protected

--- PythonRobot/checkbox.py
+++ PythonRobot/checkbox.py
@@ -7,29 +7,29 @@ from generic_element import Element
 class Checkbox:
 
     def __init__(self, driver: webdriver, visible_locator, actual_locator):
-        self.driver = driver
-        Element(self.driver, visible_locator).wait_until_visible()
-        self.selenium_element = self.driver.find_element(By.XPATH, f"{visible_locator}{actual_locator}")
-        self.clickable_element = self.driver.find_element(By.XPATH, visible_locator)
-        self.checked_xpath = f'{visible_locator}//span[@class="tick checked"]'
-        self.unchecked_xpath = f'{visible_locator}//span[contains(@class,"unchecked")]'
+        self._driver = driver
+        Element(self._driver, visible_locator).wait_until_visible()
+        self._selenium_element = self._driver.find_element(By.XPATH, f"{visible_locator}{actual_locator}")
+        self._clickable_element = self._driver.find_element(By.XPATH, visible_locator)
+        self._checked_xpath = f'{visible_locator}//span[@class="tick checked"]'
+        self._unchecked_xpath = f'{visible_locator}//span[contains(@class,"unchecked")]'
 
     def click(self):
-        self.clickable_element.click()
+        self._clickable_element.click()
 
     def select(self):
         if self.unchecked():
-            self.clickable_element.click()
+            self._clickable_element.click()
 
     def unselect(self):
         if self.checked():
-            self.clickable_element.click()
+            self._clickable_element.click()
 
     def checked(self):
-        return self.driver.find_element(By.XPATH, self.checked_xpath)
+        return self._driver.find_element(By.XPATH, self._checked_xpath)
 
     def unchecked(self):
-        return self.driver.find_element(By.XPATH, self.unchecked_xpath)
+        return self._driver.find_element(By.XPATH, self._unchecked_xpath)
 
     def is_focused(self):
-        return self.selenium_element == self.driver.switch_to.active_element()
+        return self._selenium_element == self._driver.switch_to.active_element()
