01e34c01555acb85cc2d3f97d555acfcb1c58962
Ilia Malyshev <imalyshev@networkoptix.com>
2023-09-18 13:05:37 +0200

    Auto reformat checkbox.py

--- PythonRobot/checkbox.py
+++ PythonRobot/checkbox.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-import robot_keywords
 from generic_element import Element
 
 
 class Checkbox:
+
     def __init__(self, driver: webdriver, visible_locator, actual_locator):
         self.driver = driver
         Element(self.driver, visible_locator).wait_until_visible()
@@ -20,16 +20,16 @@ class Checkbox:
     def select(self):
         if self.unchecked():
             self.clickable_element.click()
-    
+
     def unselect(self):
         if self.checked():
             self.clickable_element.click()
 
     def checked(self):
         return self.driver.find_element(By.XPATH, self.checked_xpath)
-    
+
     def unchecked(self):
-         return self.driver.find_element(By.XPATH, self.unchecked_xpath)
-    
+        return self.driver.find_element(By.XPATH, self.unchecked_xpath)
+
     def is_focused(self):
         return self.selenium_element == self.driver.switch_to.active_element()
