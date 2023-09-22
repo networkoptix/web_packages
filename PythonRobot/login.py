535d4587fb7714f79c46a837158d4373921ba4a0
Andrei Zosimov <azosimov@networkoptix.com>
2023-09-22 18:14:14 +0200

    Small refactor of login methods

--- PythonRobot/login.py
+++ PythonRobot/login.py
@@ -16,7 +16,7 @@ class LoginDialog:
 
     def password_input(self) -> TextField:
         return TextField(self.driver, "//nx-authorize-component//input[@id='authorizePassword']")
-    
+
     def password_input_error_message(self) -> TextField:
         translated_xpath = self.rb.replace_nested_variables(
             "//nx-authorize-component//p[contains(text(),'{WRONG_PASSWORD}')]")
@@ -32,19 +32,19 @@ class LoginDialog:
     def activation_success_login_button(self):
         translated_xpath = self.rb.replace_nested_variables("//button[contains(text(), '{LOG_IN_BUTTON_TEXT}')]")
         return Button(self.driver, translated_xpath)
-    
+
     def twofa_auth_code_input(self):
         return TextField(self.driver, "//nx-authorize-component//nx-authorize-auth-code-component//input[@id='authCode']")
 
     def twofa_login_button(self):
         return Button(self.driver, f"//nx-authorize-component//nx-process-button//button[@type='submit']")
-    
+
     def twofa_backup_code_button(self):
         return Button(self.driver, f"//nx-authorize-auth-code-component//span[text()='{self.rb.TWOFA_BACKUP_CODE_BTN_TEXT}']")
-    
+
     def twofa_backup_code_input(self):
         return TextField(self.driver, "//nx-authorize-backup-code-component//input[@id='backupCode']")
-    
+
     def twofa_error_login_code(self):
         return PageText(self.driver, f'//nx-authorize-component//nx-authorize-auth-code-component//p[contains(text(),"{self.rb.TWOFA_INVALID_CODE_TEXT}")]')
 
@@ -66,18 +66,12 @@ class LoginDialog:
         self.login_button().click()
 
     def twofa_cloud_login(self, email, password, twofa):
-        self.email_input().input_text(email)
-        self.next_button().click()
-        self.password_input().input_text(password)
-        self.login_button().click()
+        self.basic_cloud_login(email, password)
         self.twofa_auth_code_input().input_text(twofa)
         self.twofa_login_button().click()
 
     def twofa_backup_cloud_login(self, email, password, backup_code):
-        self.email_input().input_text(email)
-        self.next_button().click()
-        self.password_input().input_text(password)
-        self.login_button().click()
+        self.basic_cloud_login(email, password)
         self.twofa_backup_code_button().click()
         self.twofa_backup_code_input().input_text(backup_code)
         self.twofa_login_button().click()
