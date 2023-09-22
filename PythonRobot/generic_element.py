51b59534d8212f5c6e0da6faa4c40f831c3eb78e
Ilia Malyshev <imalyshev@networkoptix.com>
2023-09-22 17:24:55 +0200

    RobotTests: Auto reformat generic_element.py

--- PythonRobot/generic_element.py
+++ PythonRobot/generic_element.py
@@ -2,13 +2,13 @@ import logging
 import platform
 import time
 
-from selenium.webdriver import ActionChains
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 _DEFAULT_TIMEOUT = 10
 _logger = logging.getLogger(__name__)
@@ -168,7 +168,6 @@ class Element:
 
     def find_element(self, locator: str, position: int = 1) -> 'Element':
         return Element(self._driver, f'({self._locator}{locator})[{position}]')
-
 
 class ElementNotInDOM(Exception):
     pass
