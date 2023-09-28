d872f1ca81becac20d46dcda6d771c6bd9eb2065
Alex Lvovsky <alvovsky@networkoptix.com>
2023-09-28 18:46:05 -0500

    fixed if trying to check element not visible

--- PythonRobot/wrappers.py
+++ PythonRobot/wrappers.py
@@ -72,7 +72,6 @@ class PageText:
     def __init__(self, driver: WebDriver, locator):
         self._driver = driver
         self._element = Element(self._driver, locator)
-        self._element.wait_until_visible()
 
     def get_text(self) -> str:
         return self._element.text()
