d3a9c7885c36e734f8dea4d6b02e6f64bf82a029
Kimberly Vandiver <kvandiver@networkoptix.com>
2023-10-09 20:20:13 +0300

    CLOUD-11408: Updated format of displayed permission groups
    # Problem
    Long group names (more than 50 characters) aren't truncated, so they overflow.
    
    # Solution
    Per the spec, group names and descriptions should now be displayed one below the other instead of in table format.

--- front_end/libs/features/systems/settings/users/users.module.ts
+++ front_end/libs/features/systems/settings/users/users.module.ts
@@ -9,6 +9,7 @@ import { NxContentBlockComponent } from '@components/content-block/content-block
 import { NxContentBlockSectionComponent } from '@components/content-block/section/section.component';
 import { NxMultiSelectDropdown } from '@components/dropdowns/multi-select/multi-select.component';
 import { NxPermissionsDropdown } from '@components/dropdowns/permissions/permissions.component';
+import { NxMultiLineEllipsisComponent } from '@components/multi-line-ellipsis/mle.component';
 import { NxPreLoaderComponent } from '@components/placeholders/pre-loader/pre-loader.component';
 import { NxSwitchComponent } from '@components/switch/switch.component';
 import { NxAddSvgSrcDirective } from '@directives/add-data.directive';
@@ -31,6 +32,7 @@ import { NxSystemUsersComponent } from './users.component';
         NxPermissionsDropdown,
         NxSwitchComponent,
         NxAddSvgSrcDirective,
+        NxMultiLineEllipsisComponent,
         NxApplyComponent,
     ],
     providers: [],
