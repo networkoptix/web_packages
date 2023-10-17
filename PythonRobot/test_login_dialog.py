e5308ea75f6d286e4e76604cac7fa022cb7f0b23
Anna Klimova <aklimova@networkoptix.com>
2023-10-17 18:56:13 +0200

    Use context manager to work with Chrome in log in dialog test

--- PythonRobot/test_login_dialog.py
+++ PythonRobot/test_login_dialog.py
@@ -2,7 +2,7 @@ import resource_import
 from header import HeaderNav
 from landing_page import LandingPage
 from login import LoginDialog
-from resource_import import get_headless_chrome
+from resource_import import get_chrome
 from resource_import import register_and_activate_account
 from systems_page import SystemsPage
 from variables import ENV
@@ -10,24 +10,23 @@ from variables import ENV
 
 def allows_login_with_correct_credentials_and_log_out():
     """C24212    C24213    smoke    ci    C94717    C94719"""
-    driver = get_headless_chrome()
-    email = resource_import.get_random_email()
-    password = "qweasd 123"
-    register_and_activate_account(driver, "Mark", "Hamill", email, password)
-    driver.get(ENV)
-    header = HeaderNav(driver)
-    header.log_in_button().click()
-    login = LoginDialog(driver)
-    login.email_input().input_text(email)
-    login.next_button().click()
-    login.password_input().input_text(password)
-    login.login_button().click()
-    SystemsPage(driver).no_systems()
-    header.account_dropdown().click()
-    header.log_out_option().click()
-    LandingPage(driver)
-    driver.quit()
-    print("pass")
+    with get_chrome() as driver:
+        email = resource_import.get_random_email()
+        password = "qweasd 123"
+        register_and_activate_account(driver, "Mark", "Hamill", email, password)
+        driver.get(ENV)
+        header = HeaderNav(driver)
+        header.log_in_button().click()
+        login = LoginDialog(driver)
+        login.email_input().input_text(email)
+        login.next_button().click()
+        login.password_input().input_text(password)
+        login.login_button().click()
+        SystemsPage(driver).no_systems()
+        header.account_dropdown().click()
+        header.log_out_option().click()
+        LandingPage(driver)
+        print("pass")
 
 
 if __name__ == "__main__":
