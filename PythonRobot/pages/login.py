397c2e04b3dc18bfc7c7ffadd1cbc04b461cdee9
Kyle Epperson <kepperson@networkoptix.com>
2023-11-08 01:14:43 +0300

    X/convert login form validation
    Converts login form validation to python.
    Deletes old form validation robot file.

--- PythonRobot/pages/login.py
+++ PythonRobot/pages/login.py
@@ -13,74 +13,87 @@ from generic_elements import ToastNotification
 
 class LoginDialog:
     def __init__(self, driver, lang="en_US", twofa=""):
-        self.driver = driver
+        self._driver = driver
         self.rb = RobotVariables(lang)
         self._wait_until_modal_is_visible()
 
     def email_input(self) -> TextField:
-        return TextField(self.driver, "//nx-authorize-component//input[@id='authorizeEmail']")
+        return TextField(self._driver, "//nx-authorize-component//input[@id='authorizeEmail']")
+
+    def email_does_not_exist_message(self) -> PageText:
+        translated_xpath = self.rb.replace_nested_variables(
+            f"//p[contains(text(),'{self.rb.ACCOUNT_DOES_NOT_EXIST_TEXT}')]"
+        )
+        return PageText(self._driver, translated_xpath)
+
+    def you_can_create_account_message(self) -> PageText:
+        translated_xpath = self.rb.replace_nested_variables(
+            f"//p[contains(text(),'{self.rb.YOU_CAN_CREATE_ACCOUNT_TEXT}')]"
+        )
+        return PageText(self._driver, translated_xpath)
 
     def password_input(self) -> TextField:
-        return TextField(self.driver, "//nx-authorize-component//input[@id='authorizePassword']")
+        return TextField(self._driver, "//nx-authorize-component//input[@id='authorizePassword']")
 
     def password_input_error_message(self) -> TextField:
         translated_xpath = self.rb.replace_nested_variables(
             "//nx-authorize-component//p[contains(text(),'{WRONG_PASSWORD}')]")
-        return TextField(self.driver, translated_xpath)
+        return TextField(self._driver, translated_xpath)
+
 
     def login_input_error_text(self):
-        return TextField(self.driver, '//nx-authorize-component//p[contains(@class,"error-label")]').get_text()
+        return TextField(self._driver, '//nx-authorize-component//p[contains(@class,"error-label")]').get_text()
 
     def next_button(self):
         translated_xpath = self.rb.replace_nested_variables("//button[contains(text(), '{NEXT_TEXT}')]")
-        return Button(self.driver, translated_xpath)
+        return Button(self._driver, translated_xpath)
 
     def russian_next_button(self):
         return Button(self.driver, "//button[contains(text(), 'Далее')]")
 
     def login_button(self):
-        return Button(self.driver, "//nx-authorize-component//nx-process-button[@data-testid='btnLogin']")
+        return Button(self._driver, "//nx-authorize-component//nx-process-button[@data-testid='btnLogin']")
 
     def restore_password_login_button(self) -> Button:
-        return Button(self.driver, "//nx-authorize-reset-password-component//button[@type='submit']")
+        return Button(self._driver, "//nx-authorize-reset-password-component//button[@type='submit']")
 
     def activation_success_login_button(self):
         translated_xpath = self.rb.replace_nested_variables("//button[contains(text(), '{LOG_IN_BUTTON_TEXT}')]")
-        return Button(self.driver, translated_xpath)
+        return Button(self._driver, translated_xpath)
 
     def twofa_auth_code_input(self):
-        return TextField(self.driver, "//nx-authorize-component//nx-authorize-auth-code-component//input[@id='authCode']")
+        return TextField(self._driver, "//nx-authorize-component//nx-authorize-auth-code-component//input[@id='authCode']")
 
     def twofa_login_button(self):
-        return Button(self.driver, f"//nx-authorize-component//nx-process-button//button[@type='submit']")
+        return Button(self._driver, f"//nx-authorize-component//nx-process-button//button[@type='submit']")
 
     def twofa_backup_code_button(self):
-        return Button(self.driver, f"//nx-authorize-auth-code-component//span[text()='{self.rb.TWOFA_BACKUP_CODE_BTN_TEXT}']")
+        return Button(self._driver, f"//nx-authorize-auth-code-component//span[text()='{self.rb.TWOFA_BACKUP_CODE_BTN_TEXT}']")
 
     def twofa_backup_code_input(self):
-        return TextField(self.driver, "//nx-authorize-backup-code-component//input[@id='backupCode']")
+        return TextField(self._driver, "//nx-authorize-backup-code-component//input[@id='backupCode']")
 
     def twofa_error_login_code(self):
-        return PageText(self.driver, f'//nx-authorize-component//nx-authorize-auth-code-component//p[contains(text(),"{self.rb.TWOFA_INVALID_CODE_TEXT}")]')
+        return PageText(self._driver, f'//nx-authorize-component//nx-authorize-auth-code-component//p[contains(text(),"{self.rb.TWOFA_INVALID_CODE_TEXT}")]')
 
     def forgot_password_button(self):
         translated_xpath = self.rb.replace_nested_variables("//button//span[contains(text(),'{FORGOT_PASSWORD_TEXT}')]")
-        return Button(self.driver, translated_xpath)
+        return Button(self._driver, translated_xpath)
 
     def russian_forgot_password_button(self):
         return Button(self.driver, "//button//span[contains(text(),'Забыли пароль?')]")
 
     def reset_password_email_input(self):
-        return TextField(self.driver, "//input[@id='resetPasswordEmail']")
+        return TextField(self._driver, "//input[@id='resetPasswordEmail']")
 
     def reset_password_button(self):
         translated_xpath = self.rb.replace_nested_variables("//button[contains(text(), '{RESET_PASSWORD_BUTTON_TEXT}')]")
-        return Button(self.driver, translated_xpath)
+        return Button(self._driver, translated_xpath)
 
     def get_reset_password_email_sent_text(self) -> Tuple[str, str]:
         base = '//div[@class="email-sent"]'
-        header_text = PageText(self.driver, f'{base}/h3').get_text()
-        description_text = PageText(self.driver, f'{base}/p').get_text()
+        header_text = PageText(self._driver, f'{base}/h3').get_text()
+        description_text = PageText(self._driver, f'{base}/p').get_text()
         return header_text, description_text
 
     def basic_cloud_login(self, email, password):
@@ -100,33 +113,45 @@ class LoginDialog:
         self.twofa_backup_code_input().input_text(backup_code)
         self.twofa_login_button().click()
 
+    def wait_until_error(self) -> str:
+        actual_error = self._get_error_message()
+        actual_error.wait_until_visible(3)
+        return actual_error.get_text().strip()
+
+    def _get_error_message(self) -> PageText:
+        return PageText(
+            self._driver,
+            '''//nx-authorize-email-component//p[contains(@class, "error-label")] | 
+            //nx-authorize-password-component//p[contains(@class, "error-label")]''',
+            )
+
     def _wait_until_modal_is_visible(self):
-        modal = Pane(self.driver, "//nx-authorize-component/div[@class='authorize-main main-w']")
+        modal = Pane(self._driver, "//nx-authorize-component/div[@class='authorize-main main-w']")
         modal.wait_until_visible()
 
     def create_account_button(self):
         translated_xpath = self.rb.replace_nested_variables(
             "//nx-authorize-email-component//button[contains(text(), "
             f"'{self.rb.CREATE_ACCOUNT_BUTTON_TEXT}')]")
-        return Button(self.driver, translated_xpath)
+        return Button(self._driver, translated_xpath)
 
     def wait_until_has_too_many_attempts_error(self):
         translated_xpath = self.rb.replace_nested_variables(
             "//nx-authorize-component//p[contains(text(), "
             f"'{self.rb.TOO_MANY_ATTEMPTS_TEXT}')]")
-        label = PageText(self.driver, translated_xpath)
+        label = PageText(self._driver, translated_xpath)
         label.wait_until_visible()
 
 
 class AccountActivatedPane:
 
     def __init__(self, driver):
-        self.driver = driver
+        self._driver = driver
         self.rb = RobotVariables("en_US")
 
     def wait_until_visible(self):
         pane = Pane(
-            self.driver,
+            self._driver,
             "//nx-authorize-activate-account-component//form",
             )
         pane.wait_until_visible()
@@ -134,7 +159,7 @@ class AccountActivatedPane:
     def get_log_in_button(self):
         self.wait_until_visible()
         return Button(
-            self.driver,
+            self._driver,
             "//nx-authorize-activate-account-component//button[contains(text(), "
             f"'{self.rb.LOG_IN_BUTTON_TEXT}')]",
             )
---
+++ PythonRobot/test_login_form_validation.py
@@ -0,0 +1,66 @@
+from NoptixLibrary.suite import Suite, CloudAccount
+from RobotVariables import RobotVariables
+from pages.header import HeaderNav
+from pages.login import LoginDialog
+from resource_import import get_chrome
+from variables import ENV
+rb = RobotVariables("en_US")
+
+
+def test_email_validation():
+    with get_chrome() as driver:
+        driver.get(ENV)
+        header = HeaderNav(driver)
+        header.log_in_button().click()
+        login = LoginDialog(driver)
+        login.next_button().click()
+        assert login.wait_until_error() == rb.ENTER_EMAIL_TEXT
+        login.email_input().input_text("noptixqagmail.com")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("@gmail.com")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixqa@gmail..com")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixqa@192.168.1.1.0")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixqa.@gmail.com")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixq..a@gmail.c")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixqa@-gmail.com")
+        login.next_button().click()
+        assert login.wait_until_error() == rb.EMAIL_INVALID_TEXT
+        login.email_input().input_text("noptixqa+does.not.exist@gmail.com")
+        login.next_button().click()
+        login.email_does_not_exist_message().wait_until_visible()
+        login.you_can_create_account_message().wait_until_visible()
+
+    print("pass")
+def test_password_validation(user):
+    with get_chrome() as driver:
+        driver.get(ENV)
+        header = HeaderNav(driver)
+        header.log_in_button().click()
+        login = LoginDialog(driver)
+        login.email_input().input_text(user.email)
+        login.next_button().click()
+        login.password_input().wait_until_visible()
+        login.login_button().click()
+        assert login.wait_until_error() == rb.ENTER_PASSWORD_ERROR_TEXT
+        login.password_input().input_text("incorrect password")
+        login.login_button().click()
+        assert login.wait_until_error() == rb.WRONG_PASSWORD
+
+    print("pass")
+
+if __name__ == "__main__":
+    with Suite() as suite:
+        test_email_validation()
+        with CloudAccount() as user:
+            test_password_validation(user)
