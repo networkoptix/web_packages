b81c4b7c8469bafa85d0815789c1811b1b038cdc
Kane Stapler <kstapler@networkoptix.com>
2023-11-11 04:28:53 +0300

    CLOUD-11670 - Backup Settings Label
    ## Problem
    
    Design wanted the `BackupNewCameras` label to be changed to `Backup new cameras by default`
    
    ## Solution
    
    We already have a pattern for this in the `NxSystemAdvancedAdminComponent`. I am bringing the same pattern over to `NxSystemDetailedSettingComponent` where the `BackupNewCameras` exists. Then also adding the new label to the translation list
    
    | BEFORE | AFTER |
    | ------ | ------ |
    |    ![image](/uploads/05cea0654dd1cad840ddfe788b5e0504/image.png)    |    ![image](/uploads/aaff8444fc59d00f1ecfe3fe447c1a1d/image.png)    |

--- front_end/libs/features/systems/settings/admin/detailedSetting/detailedSetting.component.ts
+++ front_end/libs/features/systems/settings/admin/detailedSetting/detailedSetting.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import staticLang from '@language_static';
+
 type SettingValue = string | boolean | number;
 
 interface SystemSetting {
@@ -29,4 +31,6 @@ export class NxSystemDetailedSettingComponent {
     getType(value: SettingValue): string {
         return typeof value;
     }
+
+    LANG = staticLang;
 }
