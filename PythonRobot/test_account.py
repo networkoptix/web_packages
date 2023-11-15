dc5de66a8104bb911559086d1db16765183e3d48
Anna Klimova <aklimova@networkoptix.com>
2023-11-15 17:43:35 +0100

    Inline URL in account tests and use one variable for same URL

--- PythonRobot/test_account.py
+++ PythonRobot/test_account.py
@@ -44,8 +44,7 @@ def test_can_access_account_page_from_direct_link():
         header.log_in_button().click()
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         header.account_dropdown().wait_until_visible()
-        url = rb.env + "/account"
-        driver.get(url)
+        driver.get(rb.env + "/account")
         verify_in_account_page(driver)
 
 
@@ -56,16 +55,14 @@ def test_cannot_access_account_page_from_direct_link_on_valid_login():
         driver.get(url)
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
-        url1 = rb.env + "/account"
-        driver.get(url1)
+        driver.get(url)
         verify_in_account_page(driver)
 
 
 def test_changing_first_name_and_saving_maintains_that_setting(cloud_user: CloudAccount):
     """5 changing first name and saving maintains that setting"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login(cloud_user.email, cloud_user.password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -101,8 +98,7 @@ def test_changing_last_name_and_saving_maintains_that_setting(cloud_user: CloudA
     """6 changing last name and saving maintains that setting"""
     #todo:
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login(cloud_user.email, cloud_user.password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -132,8 +128,7 @@ def test_changing_last_name_and_saving_maintains_that_setting(cloud_user: CloudA
 def test_first_name_is_required():
     """7 first name is required"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -152,8 +147,7 @@ def test_first_name_is_required():
 def test_last_name_is_required():
     """8 last name is required"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -172,8 +166,7 @@ def test_last_name_is_required():
 def test_space_for_first_name_is_not_valid():
     """9 space for first name is not valid"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -188,8 +181,7 @@ def test_space_for_first_name_is_not_valid():
 def test_space_for_last_name_is_not_valid():
     """10 space for last name is not valid"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         verify_in_account_page(driver)
@@ -205,8 +197,7 @@ def test_space_for_last_name_is_not_valid():
 def test_language_is_changeable_on_the_account_page():
     """13 language is changeable on the account page"""
     with get_chrome() as driver:
-        url = rb.ENV + "/account"
-        driver.get(url)
+        driver.get(rb.ENV + "/account")
         LoginDialog(driver).basic_cloud_login("noptixautoqa+owner@gmail.com", password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         driver.refresh()
@@ -234,8 +225,7 @@ def test_language_change_affects_emails():
     with get_chrome() as driver:
         with CloudAccount(get_random_email(sendemail=True)) as cloud_user:
             cloud_user.activate()
-            url = rb.ENV + "/account"
-            driver.get(url)
+            driver.get(rb.ENV + "/account")
             if rb.LANGUAGE != "ru_Ru":
                 LoginDialog(driver).basic_cloud_login(cloud_user.email, cloud_user.password)
                 HeaderNav(driver).account_dropdown().wait_until_visible()
@@ -293,8 +283,7 @@ def test_language_change_is_new_default(cloud_user: CloudAccount):
         header.account_dropdown().click()
         header.japanese_log_out_option().click()
         LandingPage(driver).wait_until_loaded()
-        url1 = rb.ENV + "/account"
-        driver.get(url1)
+        driver.get(url)
         LoginDialog(driver).basic_cloud_login(cloud_user.email, cloud_user.password)
         HeaderNav(driver).account_dropdown().wait_until_visible()
         api = CloudPortalAPI()
