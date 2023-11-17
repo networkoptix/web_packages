b2ea3407867999bc8808037dba266d3698df08d0
Kyrylo Apanovich <kapanovich@networkoptix.com>
2023-11-17 21:06:18 +0300

    Changing gunicorn worker class to gevent
    # Changes
    - gunicorn worker class change to gevent
    # Reason for the change
    - gevent shows speeding response time almost in 2 times

--- channel_partners/build/channel_partners_prod/entrypoint.sh
+++ channel_partners/build/channel_partners_prod/entrypoint.sh
@@ -13,7 +13,7 @@ do
             python manage.py migrate
         ;;
         web)
-            exec gunicorn channel_partners.asgi:application --capture-output --workers ${WEB_WORKERS} --bind :8000 --log-level=debug --timeout 300 -k uvicorn.workers.UvicornWorker
+            exec gunicorn channel_partners.asgi:application --capture-output --workers ${WEB_WORKERS} --bind :8000 --log-level=debug --timeout 300 -k gevent
         ;;
     esac
 done
