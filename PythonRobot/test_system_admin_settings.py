f41a769aad750f86f56b5f18ca1e85f2940d4a81
Ilia Malyshev <imalyshev@networkoptix.com>
2023-11-20 19:39:16 +0100

    RobotTests: Reset general settings before tests
    Reset general settings in the main body of the file
    instead of the body of a test

--- PythonRobot/test_system_admin_settings.py
+++ PythonRobot/test_system_admin_settings.py
@@ -17,7 +17,6 @@ def system_settings_and_security_settings_should_match_settings_on_server(server
     [tags]    system    cloud    webadmin    system settings    C69736    C65697
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -86,7 +85,6 @@ def test_changing_settings_changes_it_on_server(server: Mediaserver):
     [tags]    system    cloud    webadmin    system settings    C65722    C65724    C69740
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -159,7 +157,6 @@ def changing_several_random_checkboxes_works(server: Mediaserver):
     [tags]    system    cloud    webadmin    system settings
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -215,7 +212,6 @@ def system_and_security_settings_block_is_not_available_for_other_users(server: 
     [tags]    system    cloud    webadmin    system settings    C69737    C65698
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         viewer = server.get_cloud_viewer()
@@ -266,7 +262,6 @@ def changing_page_without_saving_changes(server: Mediaserver):
     [tags]    system    cloud    webadmin    system settings    C69739
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -323,7 +318,6 @@ def changes_made_in_the_thick_client_are_displayed_in_system_settings(server: Me
     [tags]    system    cloud    webadmin    system settings    C69741    C65723
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -438,7 +432,6 @@ def checking_the_dependency_of_system_settings_checkboxes(server: Mediaserver):
     [tags]    system    cloud    webadmin    system settings    C69742
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -473,7 +466,6 @@ def system_settings_block_is_not_available_when_the_system_is_offline(server: Me
     [tags]    system    cloud    webadmin    system settings    C69744
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -493,7 +485,6 @@ def check_limit_session_duration(server: Mediaserver):
     [tags]    C65703
     """
     with get_chrome() as driver:
-        server.reset_settings()
         url = ENV + f'/systems/{server.id}'
         driver.get(url)
         owner = server.get_cloud_owner()
@@ -563,14 +554,22 @@ if __name__ == '__main__':
             cloud_owner,
             cloud_users=users,
             )
+        cloud_server.reset_settings()
         system_settings_and_security_settings_should_match_settings_on_server(cloud_server)
+        cloud_server.reset_settings()
         test_changing_settings_changes_it_on_server(cloud_server)
+        cloud_server.reset_settings()
         changing_several_random_checkboxes_works(cloud_server)
+        cloud_server.reset_settings()
         system_and_security_settings_block_is_not_available_for_other_users(cloud_server)
+        cloud_server.reset_settings()
         changing_page_without_saving_changes(cloud_server)
+        cloud_server.reset_settings()
         changes_made_in_the_thick_client_are_displayed_in_system_settings(cloud_server)
+        cloud_server.reset_settings()
         checking_the_dependency_of_system_settings_checkboxes(cloud_server)
         # cloud_owner_2 = suite.create_cloud_account()
         # cloud_server_2 = suite.create_cloud_server(cloud_owner_2, f"{suite_name}_1_",)
         # system_settings_block_is_not_available_when_the_system_is_offline(cloud_server_2)
+        cloud_server.reset_settings()
         check_limit_session_duration(cloud_server)
