1dce3e6729a9057c9bb9d6c8d009d5b96cf1ac89
Chris Czach <czach@networkoptix.com>
2023-11-24 18:54:41 +0300

    Revert showing systems from all customizations in development
    # Changes
    Revert fetching systems from all customizations when running cloud portal locally
    # Reason for the change
    Not sure if there was a change on cdb that broke this or something but it gets stuck in a reload loop now when accessing systems from other customizations.

--- front_end/libs/services/nx-cloud-api/cloud-services/cloud-db/cloud-db-api.ts
+++ front_end/libs/services/nx-cloud-api/cloud-services/cloud-db/cloud-db-api.ts
@@ -4,7 +4,6 @@ import md5 from 'md5';
 import { iif, Observable, zip } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
-import { environment } from '@environments/environment';
 import { WINDOW } from '@services/window-provider';
 import { memoizeAsyncMedium, memoizeAsyncPersistent, memoizeAsyncShort } from '@utils/memoize';
 
@@ -79,11 +78,9 @@ export class CloudDbAPI extends BaseCloudServiceAPI {
     }
 
     public systems(systemId = ''): Observable<System[]> {
-        const params: Record<string, string> = environment.production
-            ? {
-                  customization: this.hostOrCustomization(),
-              }
-            : {};
+        const params: Record<string, string> = {
+            customization: this.hostOrCustomization(),
+        };
         const fetchSystems = this.get(this.systemEndpoint(systemId), { params });
         // If we get a singular system cdb returns a system object.
         // Otherwise, cdb returns an object { systems: System[] }.
