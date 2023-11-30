d1cc6c6b6651f03714d123845acdb8c58ab36033
Kyle Epperson <kepperson@networkoptix.com>
2023-11-30 22:14:11 +0300

    X/cloud merge dropdown conversions
    Add a couple more merge dialog dropdown tests
    Remove old robot tests

--- PythonRobot/test_cloud_merge_dropdown.py
+++ PythonRobot/test_cloud_merge_dropdown.py
@@ -20,7 +20,69 @@ def test_dropdown_has_three_sections(servers: Sequence[Mediaserver], rb: RobotVa
     2. Merge Dialog - Dropdown has three sections
     [Tags]    C70979    merge_dialog    should
     """
-    servers[1].stop()
+    [first_server, second_server, last_server] = servers
+    second_server.stop()
+    with get_chrome() as driver:
+        driver.get(rb.ENV)
+        owner = first_server.get_cloud_owner()
+        HeaderNav(driver).log_in_button().click()
+        LoginDialog(driver).basic_cloud_login(owner.email, owner.password)
+        LoginDialog(driver).wait_until_login_finished()
+        url = rb.ENV + f"/systems/{first_server.id}"
+        driver.get(url)
+        system_admin_page = SystemAdmin(driver, rb.language)
+        # The 'Merge...' button is disabled by default and only becomes active after switching to another tab and then back
+        system_admin_page.get_information_tab().click()
+        system_admin_page.get_tab_settings().click()
+        merge_button = system_admin_page.merge_with_another_system_button()
+        merge_button.wait_until_clickable(10)
+        merge_button.click()
+        merge_dialog = MergeDialog(driver)
+        merge_dialog.verify()
+        merge_dialog.wait_until_system_is_accessible(last_server.name)
+        select_button = merge_dialog.get_system_select_button()
+        select_button.wait_until_clickable()
+        select_button.click()
+        available_systems = merge_dialog.get_available_systems()
+        assert available_systems[0].state == ''
+        assert available_systems[1].state == 'incompatible'
+        assert available_systems[2].state == 'offline'
+
+def test_dropdown_has_one_section(servers: Sequence[Mediaserver], rb: RobotVariables):
+    """
+    3. Merge Dialog - Dropdown has two sections(no cloud systems)
+    [Tags]    C70980    merge_dialog    should
+    """
+    [first_server, second_server] = servers
+    with get_chrome() as driver:
+        driver.get(rb.ENV)
+        owner = first_server[0].get_cloud_owner()
+        HeaderNav(driver).log_in_button().click()
+        LoginDialog(driver).basic_cloud_login(owner.email, owner.password)
+        LoginDialog(driver).wait_until_login_finished()
+        url = rb.ENV + f"/systems/{first_server.id}"
+        driver.get(url)
+        system_admin_page = SystemAdmin(driver, rb.language)
+        # The 'Merge...' button is disabled by default and only becomes active after switching to another tab and then back
+        system_admin_page.get_information_tab().click()
+        system_admin_page.get_tab_settings().click()
+        merge_button = system_admin_page.merge_with_another_system_button()
+        merge_button.wait_until_clickable(10)
+        merge_button.click()
+        merge_dialog = MergeDialog(driver)
+        merge_dialog.verify()
+        merge_dialog.wait_until_system_is_accessible(second_server.name)
+        select_button = merge_dialog.get_system_select_button()
+        select_button.wait_until_clickable()
+        select_button.click()
+        available_systems = merge_dialog.get_available_systems()
+        assert available_systems[0].state == ''
+
+def test_dropdown_has_no_valid_systems(servers: Sequence[Mediaserver], rb: RobotVariables):
+    """
+    5. Merge Dialog - Dropdown has no valid systems
+    [Tags]    C76420    merge_dialog
+    """
     with get_chrome() as driver:
         driver.get(rb.ENV)
         owner = servers[0].get_cloud_owner()
@@ -37,16 +99,13 @@ def test_dropdown_has_three_sections(servers: Sequence[Mediaserver], rb: RobotVa
         merge_button.wait_until_clickable(10)
         merge_button.click()
         merge_dialog = MergeDialog(driver)
-        merge_dialog.verify()
+        # merge_dialog.verify()
         merge_dialog.wait_until_system_is_accessible(servers[-1].name)
         select_button = merge_dialog.get_system_select_button()
         select_button.wait_until_clickable()
         select_button.click()
         available_systems = merge_dialog.get_available_systems()
-        assert available_systems[0].state == ''
-        assert available_systems[1].state == 'incompatible'
-        assert available_systems[2].state == 'offline'
-    servers[1].start()
+        assert available_systems[0].state == 'incompatible'
 
 
 if __name__ == '__main__':
@@ -56,13 +115,28 @@ if __name__ == '__main__':
         suite_name = suite_name.replace('test_', '', 1)
     variables = RobotVariables("en_US")
     with Suite() as suite:
-        cloud_owner = suite.create_cloud_account()
-        servers = [
-            suite.create_cloud_server(cloud_owner, f"{suite_name}", vms_version='5.0'),
-            suite.create_cloud_server(cloud_owner, f"{suite_name}", vms_version='5.0'),
-            suite.create_cloud_server(cloud_owner, f"{suite_name}", vms_version='5.1'),
-            suite.create_cloud_server(cloud_owner, f"{suite_name}", vms_version='5.0'),
+        cloud_owner_1 = suite.create_cloud_account()
+        servers_1 = [
+            suite.create_cloud_server(cloud_owner_1, f"{suite_name}", vms_version='5.0'),
+            suite.create_cloud_server(cloud_owner_1, f"{suite_name}", vms_version='5.0'),
+            suite.create_cloud_server(cloud_owner_1, f"{suite_name}", vms_version='5.1'),
+            suite.create_cloud_server(cloud_owner_1, f"{suite_name}", vms_version='5.0'),
         ]
-        test_dropdown_has_three_sections(servers, variables)
+        test_dropdown_has_three_sections(servers_1, variables)
+
+        cloud_owner_2 = suite.create_cloud_account()
+        servers_2 = [
+            suite.create_cloud_server(cloud_owner_2, f"{suite_name}", vms_version='5.1'),
+            suite.create_cloud_server(cloud_owner_2, f"{suite_name}", vms_version='5.1'),
+        ]
+        test_dropdown_has_one_section(servers_2, variables)
+
+        cloud_owner_3 = suite.create_cloud_account()
+        servers_3 = [
+            suite.create_cloud_server(cloud_owner_3, f"{suite_name}", vms_version='5.1'),
+            suite.create_cloud_server(cloud_owner_3, f"{suite_name}", vms_version='5.0')
+            ]
+        test_dropdown_has_no_valid_systems(servers_3, variables)
+
         print(f'{Fore.WHITE}{test_dropdown_has_three_sections.__doc__.strip()}\t\t\t{Fore.GREEN}| PASS |')
 
