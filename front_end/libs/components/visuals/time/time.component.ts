ab853a44e04a3f4eef5dd61a0fab1b65f581f18d
Tsanko Tsolov <ttsolov@networkoptix.com>
2023-12-13 03:06:39 +0300

    CLOUD-11807: Create time component
    # Changes
    
    Added simple component to show local time.
    
    Related to CLOUD-11807

---
+++ front_end/libs/components/visuals/time/time.component.ts
@@ -0,0 +1,20 @@
+import { AsyncPipe } from '@angular/common';
+import { Input, Component } from '@angular/core';
+import dateFormat from 'dateformat';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { FULL_TIME_FORMAT } from '@vms-client/submodules/timeline/components/nx-webgl-canvas/webgl-canvas.types';
+
+@Component({
+    selector: 'nx-time',
+    template: ` <div>{{ time$ | async }}</div>`,
+    styleUrls: ['./time.component.scss'],
+    standalone: true,
+    imports: [AsyncPipe],
+})
+export class NxTimeComponent {
+    @Input() format: string = FULL_TIME_FORMAT;
+
+    time$ = timer(0, 1000).pipe(map(() => dateFormat(Date.now(), this.format)));
+}
