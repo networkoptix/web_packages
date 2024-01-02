65a94d33b1279b067578f9c5f606bd4bb7789ba5
Tsanko Tsolov <ttsolov@networkoptix.com>
2024-01-02 18:43:25 +0000

    CLOUD-12100: Cameras section is displayed below Servers section
    # Changes
    
    * Initialize Cameras Node prior fetching data (if permissions allow) to avoid inconsistent menu visuals.
    
    Related to CLOUD-12100

--- front_end/libs/features/systems/settings/settings.component.ts
+++ front_end/libs/features/systems/settings/settings.component.ts
@@ -297,6 +297,17 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
             ],
         };
 
+        // avoid delay in Camera node creation if we know we have some cameras.
+        if (this.system.cameraManager.cameras?.some(({ canEdit }) => canEdit)) {
+            this.content.level1.push({
+                id: menus.systemSettings.cameras.id,
+                svg: menus.systemSettings.cameras.icon,
+                label: this.LANG.menu.titles.cameras,
+                path: menus.systemSettings.cameras.path,
+                level3: [],
+            });
+        }
+
         // TODO: add processes back
         // Retrieve users list
         this.gettingSystemUsers = this.processService
@@ -816,8 +827,7 @@ export class NxSystemSettingsComponent implements OnInit, OnDestroy {
                     path: menus.systemSettings.cameras.path,
                     level3: [],
                 };
-                // Sometimes cameras node creation may be delayed
-                // this will just ensure the node will appear in correct place
+                // camerasNode should be already created in Init() ... leaving this just in case --TT
                 this.content.level1.splice(1, 0, camerasNode);
             }
             if (this.system.cameraManager.cameras) {
