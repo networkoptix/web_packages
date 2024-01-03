efa635ed5a42b1f8f8ca4c270f8e0c765712e4f3
Vincent Lee <vlee@networkoptix.com>
2024-01-03 23:09:07 +0300

    CLOUD-12192: Add check in Merge for "failedServers"
    # Problem
    
    When we try merging 2 systems in `cloud`, there is no indication on the Front End that the merge is happening
    
    # Changes
    
    It looks like the response for `merge` has changed and is now returning an empty array called `failedServers`, so we'll add this check with the rest of the response checks for Merge
    
    Related to CLOUD-12192
    
    (cherry picked from commit c3bba4309a1f05b711516f2090612eee6f0e558f)
    
    # Conflicts:
    #	front_end/libs/dialogs/merge/merge.component.ts

--- front_end/libs/dialogs/merge/merge.component.ts
+++ front_end/libs/dialogs/merge/merge.component.ts
@@ -647,7 +647,12 @@ export class MergeModalContent {
                 ignoreError: true
             })
             .then(res => {
-                if (res.mergeInProgress || res.error === '0' || res.resultCode === this.LANG.errorCodes.ok) {
+                if (
+                    res.mergeInProgress ||
+                    res.error === '0' ||
+                    res.resultCode === this.LANG.errorCodes.ok ||
+                    !res.failedServers.length
+                ) {
                     // handles telling the app which systems are getting merged and the proper messaging
                     if (this.environment.isLocal) {
                         const template =
