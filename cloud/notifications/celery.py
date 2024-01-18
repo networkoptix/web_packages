61a0b9b3c2acbf31e9978a80dc17b59aa16b76fa
Roman Barsegian <rbarsegian@networkoptix.com>
2024-01-17 16:26:00 -0800

    CLOUD-12357: Remove ipvd task and add to safety whitelist

--- cloud/notifications/celery.py
+++ cloud/notifications/celery.py
@@ -28,8 +28,6 @@ def setup_periodic_tasks(sender, **kwargs):
                              clean_push_logs.s(), name="clean push logs", queue='celery')
     sender.add_periodic_task(crontab(hour=0, minute=0, day_of_week='wed'),
                              clean_old_portal_notifications.s(), name="clean old portal notifications", queue='celery')
-    sender.add_periodic_task(crontab(hour='*/6', minute=0),
-                             update_ipvd.s(kwargs={'force': False}), name="update ipvd", queue='celery')
 
 
 @app.task(bind=True)
