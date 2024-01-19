d23c4e2d11b6a40a3e62c0b362e304f2c1aed754
Nick Hartleb <nhartleb@networkoptix.com>
2024-01-18 16:51:03 -0800

    CLOUD-12422: Added CommonModule to External Video Comp

--- front_end/libs/components/external-video/external-video.module.ts
+++ front_end/libs/components/external-video/external-video.module.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { NxExternalVideoComponent } from './external-video.component';
 
 @NgModule({
     imports: [
+        CommonModule
     ],
     declarations: [
         NxExternalVideoComponent
