490c930a11b7be9b52ec8f907a369f642d37a4aa
Nick Hartleb <nhartleb@networkoptix.com>
2024-01-25 17:53:47 -0800

    CLOUD-12477: Updated boolean check for playerError status
    # Problem
    If the camera was offline it was considered an error, but because we can play archive that's not always true.
    
    # Solution
    Update the check for if the player has an error and only consider offline an error while trying to play the live video feed.
    
    (cherry picked from commit 3d8fc119e2b74048525ed6b80ff771a4929164ac)

--- front_end/libs/features/systems/view/pages/system-view-camera/system-view-camera.page.component.ts
+++ front_end/libs/features/systems/view/pages/system-view-camera/system-view-camera.page.component.ts
@@ -265,7 +265,7 @@ export class NxSystemViewCameraPageComponent implements OnInit, OnDestroy, After
             error,
             errorState,
             noAccess,
-            playerError: !!error || noAccess || !isOnline || !isAuthorized,
+            playerError: !!error || noAccess || (!isOnline && isLive) || !isAuthorized,
         };
     });
 
