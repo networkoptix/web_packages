90b1d1879bf5d2dad8447e5e2580afa00997116f
Kyrylo Apanovich <kapanovich@networkoptix.com>
2024-02-01 20:35:32 +0000

    CLOUD-12553: Added missed static files
    # Changes
    - add `/static/apple-app-site-association` to serving by django app

--- cloud/cloud/urls.py
+++ cloud/cloud/urls.py
@@ -116,6 +116,7 @@ urlpatterns = [
     # Serving DB static for local development
     re_path(r'^static/styles/skin.css', static_serve.skin_styles),
     re_path(r'^static/503.html', static_serve.customizable_files),
+    re_path(r'^static/apple-app-site-association', static_serve.customizable_files),
     re_path(r'^static(/\d+/|/)languages.json', static_serve.languages_json),
     re_path(r'^static/images/(promo/.*|dark_logo\.png|logo\.png|favicon\.ico|placeholders/page/Maintenance\.svg)$',
             static_serve.customizable_files, name="customizable_static"),
