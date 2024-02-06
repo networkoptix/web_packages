5d3968c6f01f95602448bf369c7cce0dbafe02d7
Jestyn Apuya <japuya@networkoptix.com>
2024-02-06 13:41:03 +0300

    CLOUD-12602: Adds check for response from dialog before creating card
    **Problem**
    - Canceling out of new org dialog adds empty org to store
    
    **Solution**
    - Check for response from dialog before adding to store

--- front_end/libs/features/home/channel-partners/channel-partners.component.ts
+++ front_end/libs/features/home/channel-partners/channel-partners.component.ts
@@ -186,12 +186,14 @@ export class NxChannelPartnersComponent implements OnInit {
 
     newOrgDialog(): void {
         this.dialogsService.createOrganization(this.currentPartnerId).then((org: Organization) => {
-            this.store.dispatch(
-                CPActions.setCurrentPartner({
-                    currentPartnerId: this.currentPartnerId,
-                    currentPartnerOrganizations: [...this.currentPartnerOrgs, org],
-                }),
-            );
+            if (org) {
+                this.store.dispatch(
+                    CPActions.setCurrentPartner({
+                        currentPartnerId: this.currentPartnerId,
+                        currentPartnerOrganizations: [...this.currentPartnerOrgs, org],
+                    }),
+                );
+            }
         });
     }
 
