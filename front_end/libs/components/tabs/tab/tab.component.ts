a70e6e22b3ad1747a63f78c28d557e812f16211b
Kimberly Vandiver <kvandiver@networkoptix.com>
2024-02-21 06:16:24 +0300

    CLOUD-12717: Hide Settings Tab When Folder Selected
    # Problem
    Settings tab shouldn't show when a user has selected a folder.
    
    # Solution
    Added logic to hide Settings tab when a folder is selected

--- front_end/libs/components/tabs/tab/tab.component.ts
+++ front_end/libs/components/tabs/tab/tab.component.ts
@@ -4,6 +4,7 @@ import { Component, EventEmitter, Input, Output, booleanAttribute } from '@angul
 export class NxBaseTabComponent {
     @Input({ required: true }) displayName: string;
     @Input({ transform: booleanAttribute }) disabled: boolean;
+    @Input({ transform: booleanAttribute }) hidden: boolean;
     @Output() tabClick = new EventEmitter<number>();
     public selected: boolean = false;
 }
