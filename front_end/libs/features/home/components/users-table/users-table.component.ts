a2ffce61263cd81308647715dd9a278440486395
Nick Hartleb <nhartleb@networkoptix.com>
2024-02-27 04:37:50 +0300

    CLOUD-12736, CLOUD-12796: Fixed when a user is editable in orgs
    # Problem
    Users were not always editable in the correct places.
    
    # Solution
    Updated how the check for show role is done so that users can only be edited at their level.
    
    (cherry picked from commit df847301c22beebbd915079efbf21afaf3cd87e2)

--- front_end/libs/features/home/components/users-table/users-table.component.ts
+++ front_end/libs/features/home/components/users-table/users-table.component.ts
@@ -236,7 +236,12 @@ export class NxUsersTableComponent implements OnInit, OnChanges {
     }
 
     showRole(row: UserRecord): boolean {
-        return row?.isOrgUser || this.hasMultipleRoles(row) || !this.canManageUsers$$();
+        const currentGroupId = this.currentGroupId$$();
+        return (
+            (currentGroupId && row?.accessLevel?.id !== currentGroupId) ||
+            this.hasMultipleRoles(row) ||
+            !this.canManageUsers$$()
+        );
     }
 
     get tableType(): string {
