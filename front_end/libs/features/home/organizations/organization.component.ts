f8c29957bc2cc07ef9209b7c1dd5cfcf92754f6c
Vincent Lee <vlee@networkoptix.com>
2024-02-28 16:00:23 +0000

    CLOUD-12767: Display Sidebar/"New Folder" when applicable, remove placeholder
    # Problem
    
    The spec for the Org sidebar has been updated
    
    1. If there are no systems, do not display "New Folder" and the side bar
    2. When there is at least 1 system, display "New Folder"
    3. When there is a folder, display the side bar
    
    We can also remove the placeholder in the sidebar since it's no longer needed
    
    # Changes
    
    1. Get `currentOrganization$$` and `currentSystems$$` to change the Sidebar/"New Folder" logic above
    2. Placeholder in the sidebar has been removed
    3. Update Sidebar button from `<div>` to `<button>`
    
    No system, no "New Folder" button
    
    ![Screen Shot 2024-02-23 at 1.26.57 PM.png](/uploads/303c5f225facb2107c90ebdce37bbc45/Screen_Shot_2024-02-23_at_1.26.57_PM.png)
    
    When there's a folder, then we can display the sidebar
    
    ![Screen Shot 2024-02-23 at 2.08.13 PM.png](/uploads/bf3802649921dfef958f19c5c7e442ef/Screen_Shot_2024-02-23_at_2.08.13_PM.png)
    
    Related to CLOUD-12767
    
    (cherry picked from commit cfbf17ffe11c02520dc2d8554d98f66ce0aa780e)

--- front_end/libs/features/home/organizations/organization.component.ts
+++ front_end/libs/features/home/organizations/organization.component.ts
@@ -44,6 +44,8 @@ import {
     selectCurrentGroupId,
     selectHasGroups,
     selectOpenGroups,
+    selectCurrentGroups,
+    selectCurrentSystems,
 } from '../store/groups/groups.selectors';
 
 import { NxOrganizationCardContainerComponent } from './cards-container/org-cards-container.component';
@@ -99,6 +101,8 @@ export class NxOrganizationsComponent implements OnInit {
     hasGroups$ = this.store.select<boolean>(selectHasGroups);
     currentGroupId$$ = this.store.selectSignal<string>(selectCurrentGroupId);
     currentOrganization$$ = this.store.selectSignal(selectCurrentOrganization);
+    currentSystems$$ = this.store.selectSignal(selectCurrentSystems);
+    currentGroups$$ = this.store.selectSignal<GroupItem[]>(selectCurrentGroups);
 
     constructor(
         private store: Store,
