27cb1606637b66cfbc81faa9ec7b675986465cae
Nick Hartleb <nhartleb@networkoptix.com>
2024-02-28 16:22:20 -0800

    CLOUD-12767: Fixed left menu disappearing
    # Problems
    - If you went into a folder without a system or folder the menu disappeared.
    - If there are no system you cant make folders for the org.
    
    # Solutions
    - Updated the left menu to show if the rootGroup had folders or the org has systems.
    - Changed the logic for creating folders so that it's just the permission check again.

--- front_end/libs/features/home/organizations/organization.component.ts
+++ front_end/libs/features/home/organizations/organization.component.ts
@@ -54,8 +54,7 @@ import {
     selectCurrentGroupId,
     selectHasGroups,
     selectOpenGroups,
-    selectCurrentGroups,
-    selectCurrentSystems,
+    selectRootGroups,
 } from '../store/groups/groups.selectors';
 
 import { NxOrganizationCardContainerComponent } from './cards-container/org-cards-container.component';
@@ -111,8 +110,7 @@ export class NxOrganizationsComponent implements OnInit {
     hasGroups$ = this.store.select<boolean>(selectHasGroups);
     currentGroupId$$ = this.store.selectSignal<string>(selectCurrentGroupId);
     currentOrganization$$ = this.store.selectSignal(selectCurrentOrganization);
-    currentSystems$$ = this.store.selectSignal(selectCurrentSystems);
-    currentGroups$$ = this.store.selectSignal<GroupItem[]>(selectCurrentGroups);
+    rootGroups$$ = this.store.selectSignal<GroupItem[]>(selectRootGroups);
 
     constructor(
         private store: Store,
