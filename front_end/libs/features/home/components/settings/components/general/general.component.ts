fad8377283264b447f052148acf4750fd7037b11
Nick Hartleb <nhartleb@networkoptix.com>
2024-03-01 16:50:04 -0800

    CLOUD-12803: Refactored Settings to be cancelable
    # Problem
    User not able to cancel changes on the settings tab.
    
    # Solution
    Updated how the component tracks current values with the initial value. Most of this is a temporary measure and we will need to go back and use forms to do the same work.
    
    # Changes
    - Switched comp inputs to signal inputs
    - Fixed memory leak when user opened setting for sub channels.
    - Misc change updated lang.
    
    (cherry picked from commit e5a9b23d48b6e37ba864b53483d8346f35071a2a)

--- front_end/libs/features/home/components/settings/components/general/general.component.ts
+++ front_end/libs/features/home/components/settings/components/general/general.component.ts
@@ -1,5 +1,6 @@
+/* eslint nx/signal-naming-convention: 0 */
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { NxCheckboxComponent } from '@components/checkbox/checkbox.component';
@@ -8,11 +9,7 @@ import { NxContentBlockSectionComponent } from '@components/content-block/sectio
 import { DropdownItem } from '@components/dropdowns/generic/dropdown.component.types';
 import { NxGenericDropdownModule } from '@components/dropdowns/generic/dropdown.module';
 import { settingsViews } from '@pages/home/home.types';
-import {
-    ChannelPartner,
-    OrgRoleIds,
-    Organization,
-} from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
+import { OrgRoleIds } from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
 
 const partnerAccess: DropdownItem<string>[] = [
     {
@@ -49,26 +46,22 @@ const accessMap: { [key: string]: DropdownItem<string> } = {
         NxContentBlockSectionComponent,
     ],
 })
-export class NxSettingsGeneralComponent implements OnInit {
-    partnerAccess = partnerAccess;
-    settingsViews = settingsViews;
-    @Input() canAccess: string;
-    @Input() view: string;
-    @Input() item: Organization | ChannelPartner | undefined;
-    @Input() disableNameInput: boolean;
+export class NxSettingsGeneralComponent {
+    readonly partnerAccess = partnerAccess;
+    readonly settingsViews = settingsViews;
+    view = input.required<string>();
+    name = input.required<string>();
+    channelPartnerAccessLevel = input<string>('');
+    disableNameInput = input.required<boolean>();
+
     @Output() updateName = new EventEmitter<string>();
     @Output() updateAccess = new EventEmitter<string>();
-    extId: string;
-    name: string;
-    initialName: string;
-    currAccess: DropdownItem<string>;
-    changeService: boolean;
 
-    ngOnInit(): void {
-        if ('channelPartnerAccessLevel' in this.item) {
-            this.currAccess = accessMap[this.item.channelPartnerAccessLevel];
-        }
-        this.name = this.item?.name;
-        this.initialName = this.item?.name;
+    currAccess$$ = computed<DropdownItem<string> | null>(
+        () => accessMap?.[this.channelPartnerAccessLevel()] || null,
+    );
+
+    onNameChange(name: string): void {
+        this.updateName.emit(name);
     }
 }
