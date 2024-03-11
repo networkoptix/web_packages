80284dfaec48024b0197176b759e3b8183143f8b
Chris Czach <czach@networkoptix.com>
2024-03-11 11:02:28 +0300

    CLOUD-13008: Add approvals check to pipeline and remove code owners
    # Changes
    Remove code owners and replace with approval check script with code owner rules within pipeline.
    # Reason for the change
    Hardcoded code owners file was requiring approvals on cherry picks. It's also not that flexible.

---
+++ approval-check.py
@@ -0,0 +1,62 @@
+import fnmatch
+import sys
+import os
+import requests
+
+nhartleb = 'nhartleb'
+rbarsegian = 'rbarsegian'
+ttsolov = 'ttsolov'
+
+default_code_owners = [nhartleb, rbarsegian]
+
+def check_glob_pattern(pattern, filenames):
+    return fnmatch.filter(filenames, pattern)
+
+def create_code_owner_rule(pattern, code_owners=None, required_approvals=1):
+    code_owners = code_owners or default_code_owners
+    def rule(changed_files, approvals_usernames):
+        code_owners_set = set(code_owners)
+        approval_users_set = set(approvals_usernames)
+        owners_not_approved = code_owners_set.difference(approval_users_set)
+        approvals = len(code_owners_set.intersection(approval_users_set))
+        additional_approvals_required = max(required_approvals - approvals, 0)
+        approvals_required = additional_approvals_required and check_glob_pattern(pattern, changed_files)
+        return approvals_required and f'Need {additional_approvals_required} approval{"s" if additional_approvals_required > 1 else ""} from {owners_not_approved} for "{pattern}"'
+
+    return rule
+
+# Define the rules to check. Currently we only have rules created by the create_code_owner_rule factory.
+# We'll probably add more sophisticated rules in the future.
+rules = [
+    create_code_owner_rule('cloud/**/*'),
+    create_code_owner_rule('front-end/**/*', [ttsolov]),
+    create_code_owner_rule('**/*')
+]
+
+def check_approval_rules(approval_endpoint, access_token, target_commit):
+    print('Checking approval rules...')
+    headers = {
+        'Accept': 'application/json',
+        'PRIVATE-TOKEN': access_token,
+    }
+    approvals = requests.get(approval_endpoint, headers=headers).json()['approved_by']
+    approvals_usernames = [approval['user']['username'] for approval in approvals]
+    changed_files = [f'./{file}' for file in os.popen(f'git diff --name-only {target_commit}..@').read().splitlines()]
+
+    if not changed_files:
+        print('Error getting diff files. Exiting...')
+        exit(69)
+
+    print(f'Changed files: {len(changed_files)}')
+    print(f'Current Approvals: {", ".join(approvals_usernames)}')
+
+    return [error for rule in rules if (error := rule(changed_files, approvals_usernames))]
+
+if __name__ == '__main__':
+    if errors := check_approval_rules(*sys.argv[1:4]):
+        print('Approval rules failed')
+        for error in errors:
+            print(error)
+        exit(69)
+    else:
+        print('Approval rules passed')
