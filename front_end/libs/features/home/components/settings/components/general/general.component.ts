0a122f5bd180bee386100a9a68b6572b19f99945
Roman Barsegian <rbarsegian@networkoptix.com>
2024-03-12 21:36:06 +0300

    CLOUD-12983: Fix selecting service management in access level
    # Problem
    - Service management only option did not show in dropdown when it was the current value
    # Solution
    - Use null to represent service management only
    
    (cherry picked from commit e1d4372cf55d6aea74e63eabd1fde5653559a887)

--- front_end/libs/features/home/components/settings/components/general/general.component.ts
+++ front_end/libs/features/home/components/settings/components/general/general.component.ts
@@ -11,7 +11,7 @@ import { NxGenericDropdownModule } from '@components/dropdowns/generic/dropdown.
 import { settingsViews } from '@pages/home/home.types';
 import { OrgRoleIds } from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
 
-const partnerAccess: DropdownItem<string>[] = [
+const partnerAccess: DropdownItem<string | null>[] = [
     {
         name: 'Organization Administrator',
         value: OrgRoleIds.OrgAdmin,
@@ -22,14 +22,14 @@ const partnerAccess: DropdownItem<string>[] = [
     },
     {
         name: 'Service Management Only',
-        value: 'serviceManagementOnly',
+        value: null,
     },
 ];
 
-const accessMap: { [key: string]: DropdownItem<string> } = {
+const accessMap: { [key: string]: DropdownItem<string | null> } = {
     [OrgRoleIds.OrgAdmin]: partnerAccess[0],
     [OrgRoleIds.SysHealthViewer]: partnerAccess[1],
-    serviceManagementOnly: partnerAccess[2],
+    null: partnerAccess[2],
 };
 
 @Component({
@@ -57,7 +57,7 @@ export class NxSettingsGeneralComponent {
     @Output() updateName = new EventEmitter<string>();
     @Output() updateAccess = new EventEmitter<string>();
 
-    currAccess$$ = computed<DropdownItem<string> | null>(
+    currAccess$$ = computed<DropdownItem<string | null>>(
         () => accessMap?.[this.channelPartnerAccessLevel()] || null,
     );
 
