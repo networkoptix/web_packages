98d890d8862398fa60d90b7046c0fe04f8cd9707
Alex Lvovsky <alvovsky@networkoptix.com>
2024-03-20 09:49:10 -0500

    Increase sleep for owner_can_remove_user test
    Removes any chances of false negative result.

--- PythonRobot/test_system_users.py
+++ PythonRobot/test_system_users.py
@@ -61,7 +61,7 @@ def owner_can_remove_user(driver, server: Mediaserver):
         users_page = SystemUsers(driver)
         users_page.remove_user_button().click()
         users_page.remove_user_modal_button().click()
-        time.sleep(2)
+        time.sleep(5)
         header.log_out()
         LandingPage(driver).location_is_correct(url=f"{ENV}/")
         header.log_in_button().click()
