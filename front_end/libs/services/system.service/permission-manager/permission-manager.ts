networkoptix/web_packages

commit 9cc4a2c3bcee6e60957efc1ac575ff160c5d9224

CLOUD-13024: Fixed issue where offline 6.0 systems do not load
\# Problem\
6.0 requires the jsonrpc connection to start before saying it is ready to use. When the system is offline we cant establish a connection so the app gets caught in the system guard. Also, non Power Users can see the transfer buttons when the system is offline\
\
# Solution\
Refactor the useRpcOverPolling method so that when the system is offline its set as offline and marked as ready. Next updated the permission manager to use the system info from cdb for initializing non-power users.

front_end/libs/services/system.service/permission-manager/permission-manager.ts
@@ -1,4 +1,6 @@
 import { computed, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { environment } from '@environments/environment';
 import staticLang from '@language/language_i18n_static.json';
@@ -177,7 +179,7 @@ export class PermissionManager {
         const accessRights = user && 'resourceAccessRights' in user && user?.resourceAccessRights;
 
         let accessRole = '';
-        if (this.mediaserver instanceof NxSystemRestAPI3 && (user as RestV3User).groupIds) {
+        if (this.mediaserver instanceof NxSystemRestAPI3 && (user as RestV3User).groupIds.length) {
             accessRole = (user as RestV3User).groupIds
                 .map(groupId => groups.find(({ id }) => groupId === id)?.name)
                 .filter(role => !!role)
@@ -293,8 +295,18 @@ export class PermissionManager {
     }
 
     async getCurrentUserFromCloud(): Promise<void> {
-        this.cloudApi.users(this.systemId).subscribe(users => {
-            const user = users.find(({ accountEmail }) => accountEmail === this.currentUserEmail);
+        try {
+            const user = await firstValueFrom(
+                this.cloudApi
+                    .users(this.systemId)
+                    .pipe(
+                        map(users =>
+                            users.find(
+                                ({ accountEmail }) => accountEmail === this.currentUserEmail,
+                            ),
+                        ),
+                    ),
+            );
             if (user) {
                 const { customPermissions, permissions } = user || {};
                 this.user$$.set({
@@ -309,7 +321,24 @@ export class PermissionManager {
                     groupIds: [],
                 });
             }
-        });
+        } catch {
+            const [systemInfo] = await firstValueFrom(this.cloudApi.systems(this.systemId));
+            this.user$$.set({
+                accessRole: systemInfo.accessRole,
+                email: this.currentUserEmail,
+                fullName: '',
+                id: '',
+                isEnabled: true,
+                isCloud: true,
+                name: '',
+                permissions: '',
+                type: 'cloud',
+                attributes: '',
+                groupIds: [],
+                resourceAccessRights: {},
+                hasCustomPermissions: false,
+            });
+        }
     }
 
     async checkCurrentUser(): Promise<void> {

front_end/libs/services/system/modules/nx-system-old-module.ts
@@ -9,8 +9,17 @@ import {
     Subject,
     timer,
     ReplaySubject,
+    of,
 } from 'rxjs';
-import { auditTime, catchError, map, shareReplay, switchMap, takeUntil } from 'rxjs/operators';
+import {
+    auditTime,
+    catchError,
+    concatMap,
+    map,
+    shareReplay,
+    switchMap,
+    takeUntil,
+} from 'rxjs/operators';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import stringify from 'safe-stable-stringify';
 import { v4 as uuid } from 'uuid';
@@ -246,8 +255,11 @@ export class NxSystemOldModule extends NxSystemModuleBase {
     private jsonRpc: WebSocketSubject<unknown>;
     private systemReady$ = new ReplaySubject<this>(1);
     private useRpcOverPolling(): void {
-        if (this.mediaserver instanceof NxSystemRestAPI3) {
-            this.mediaserver.buildRpcUrl().subscribe(url => {
+        if (!(this.mediaserver instanceof NxSystemRestAPI3)) {
+            return;
+        }
+        this.mediaserver.buildRpcUrl().subscribe({
+            next: url => {
                 this.jsonRpc = webSocket(url);
                 this.jsonRpc.pipe(takeUntil(this.killPoll$)).subscribe({
                     next: (v: Partial<{ method: string }>) => {
@@ -298,26 +310,25 @@ export class NxSystemOldModule extends NxSystemModuleBase {
 
                 // Replicates the system poll w/o updates
                 timer(0, updateInterval)
-                    .pipe(takeUntil(this.killPoll$))
+                    .pipe(
+                        switchMap(() => this.getInfo(true, false, true)),
+                        concatMap((value, index) => {
+                            if (index === 1) {
+                                this.systemReady$.next(this);
+                            }
+                            return of(value);
+                        }),
+                        takeUntil(this.killPoll$),
+                    )
                     .subscribe(() => {
                         this.infoSubject.next(this);
                     });
-
-                // Does the initial setup for the system
-                this.getInfo(true, false, true)
-                    .then(() =>
-                        this.isOnline
-                            ? this.proxied.cameraManager.updateSystemCameras()
-                            : Promise.reject({ offline: true }),
-                    )
-                    .then(() => firstValueFrom(this.proxied.serverManager.getForceServers(false)))
-                    .then(() => this.getUsers(true, true))
-                    .finally(() => {
-                        this.infoSubject.next(this);
-                        this.systemReady$.next(this);
-                    });
-            });
-        }
+            },
+            error: _ => {
+                this.isOnline = false;
+                this.systemReady$.next(this);
+            },
+        });
     }
 
     initSystem = (

front_end/libs/routeGuards/systemGuard.ts
@@ -85,7 +85,7 @@ export const SystemGuard: CanActivateFn = (
 
     return accountPromise.then(async account => {
         if (!account) {
-            return;
+            return false;
         }
         let currSystem = systemService.getCurrentSystem();
 

commit 7d9c20be72abd51d79e2dc24c011211364e3b751

CLOUD-13164, CLOUD-13165, CLOUD-13170, CLOUD-13171: Batch of 6.0 user fixes
# CLOUD-13171: Fixes 6.0 user group names when offline

## Problem

CDB had changes that removed customPermissions for 6.0 system. Now it only provides roles and userRoleId.

## Solution

For 6.0 system add special logic to add default row info for offline systems. To make the systems resilient when offline the default groups were added to the codebase.

# CLOUD-13170: Fixes issue where Custom users didn't have accessRoles

## Problem

Roles can load later causing the user Access Role to be blank

## Solution

Return the users accessRole at the end of the function. If it's not truth return it as custom.

# 

# CLOUD-13164, CLOUD-13165: Fixed system initialization issues

## Problem

User groups do not load unless you are a power user, admin, or something with the current user changed in the system. Fixes for CLOUD-13024 9cc4a2c3bcee6e60957efc1ac575ff160c5d9224 caused parts of the system to not initialize when expected.

## Solution

Partially revert some of the changes and add it into the first poll for the system.

front_end/libs/services/system.service/permission-manager/permission-manager.ts
@@ -238,16 +238,16 @@ export class PermissionManager {
         if (!user) {
             return '';
         }
-
+        let accessRole = '';
         const permissionsString = user.permissions.split('|').sort().join('|');
 
         if (this.mediaserver instanceof NxSystemRestAPI3 && (user as RestV3User).groupIds.length) {
-            return (user as RestV3User).groupIds
+            accessRole = (user as RestV3User).groupIds
                 .map(groupId => groups.find(({ id }) => groupId === id)?.name)
                 .filter(role => !!role)
                 .join(', ');
         } else if (roles.length) {
-            return (
+            accessRole =
                 roles.find(
                     role =>
                         'isOwner' in role &&
@@ -255,12 +255,15 @@ export class PermissionManager {
                         role.permissions === permissionsString,
                 )?.name ||
                 customRole?.name ||
-                ''
-            );
+                '';
         } else if (!environment.isLocal) {
             // If roles is empty that means we couldn't fetch them from the system.
             // As a fallback for cloud we can try to get the accessRole from cdb.
-            return (user as CloudUserCompat)?.accessRole;
+            accessRole = (user as CloudUserCompat).accessRole;
+        }
+
+        if (accessRole) {
+            return accessRole;
         }
 
         return this.LANG.accessRoles.custom.label;

front_end/libs/services/system.service/user-manager/default-groups.ts
@@ -1,0 +1,132 @@
+import { UserGroup } from '@services/system-user.types';
+
+export const DefaultUserGroups: UserGroup[] = [
+    {
+        attributes: 'readonly',
+        description:
+            'Members of this group have unlimited System privileges. Administrators can create and modify Power Users, merge Systems and connect or disconnect System to  Nx Cloud.',
+        id: '00000000-0000-0000-0000-100000000000',
+        name: 'Administrators',
+        parentGroupIds: [],
+        permissions: 'powerUser|viewLogs|viewMetrics|generateEvents|administrator',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000001}':
+                'view|viewArchive|exportArchive|viewBookmarks|manageBookmarks|userInput|edit',
+            '{00000000-0000-0000-0000-200000000002}': 'view',
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+            '{00000000-0000-0000-0000-200000000004}': 'edit',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+    {
+        attributes: 'readonly',
+        description:
+            'Members of this group can, in addition to the permissions granted by the Advanced Viewers group, control most of the System configuration, but are not allowed to change any Administrator related settings, like delete or change their own groups and permissions, and cannot create or edit other Power Users.',
+        id: '00000000-0000-0000-0000-100000000001',
+        name: 'Power Users',
+        parentGroupIds: [],
+        permissions: 'powerUser|viewLogs|viewMetrics|generateEvents',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000001}':
+                'view|viewArchive|exportArchive|viewBookmarks|manageBookmarks|userInput|edit',
+            '{00000000-0000-0000-0000-200000000002}': 'view',
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+            '{00000000-0000-0000-0000-200000000004}': 'edit',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+    {
+        attributes: 'readonly',
+        description:
+            'Members of this group can, in addition to the permissions granted by the Viewers group, see and activate PTZ positions and PTZ tours, use 2-way audio, operate I/O module buttons, create and edit bookmarks, and view the Event Log.',
+        id: '00000000-0000-0000-0000-100000000002',
+        name: 'Advanced Viewers',
+        parentGroupIds: [],
+        permissions: 'viewLogs',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000001}':
+                'view|viewArchive|exportArchive|viewBookmarks|manageBookmarks|userInput',
+            '{00000000-0000-0000-0000-200000000002}': 'view',
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+    {
+        attributes: 'readonly',
+        description:
+            'Members of this group can, in addition to the permissions granted by the Live Viewers group, view and export archive and Bookmarks.',
+        id: '00000000-0000-0000-0000-100000000003',
+        name: 'Viewers',
+        parentGroupIds: [],
+        permissions: 'none',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000001}':
+                'view|viewArchive|exportArchive|viewBookmarks',
+            '{00000000-0000-0000-0000-200000000002}': 'view',
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+    {
+        attributes: 'readonly',
+        description: 'Members of this group can view live videos, I/O modules and web pages.',
+        id: '00000000-0000-0000-0000-100000000004',
+        name: 'Live Viewers',
+        parentGroupIds: [],
+        permissions: 'none',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000001}': 'view',
+            '{00000000-0000-0000-0000-200000000002}': 'view',
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+    {
+        attributes: 'readonly',
+        description:
+            'Members of this group can view System Health Monitoring information and server processor load in real-time (Server Monitoring).',
+        id: '00000000-0000-0000-0000-100000000005',
+        name: 'System Health Viewers',
+        parentGroupIds: [],
+        permissions: 'viewMetrics',
+        resourceAccessRights: {
+            '{00000000-0000-0000-0000-200000000003}': 'view',
+        },
+        type: 'local',
+        externalId: {
+            dn: '',
+            syncId: '',
+            synced: false,
+        },
+    },
+];
+
+export const DefaultUserGroupsToId: Record<string, UserGroup> = DefaultUserGroups.reduce(
+    (groups, group) => ({ ...groups, [group.id]: group }),
+    {},
+);

front_end/libs/services/system.service/user-manager/user-with-groups-manager.ts
@@ -15,6 +15,7 @@ import {
     UserGroupDropdown,
     UserType,
 } from '@services/system-user.types';
+import { DefaultUserGroupsToId } from '@services/system.service/user-manager/default-groups';
 import { alphabeticalSort, cleanIdLegacy } from '@utils/general';
 
 import { UserManager } from './user-manager';
@@ -27,7 +28,7 @@ export class UserWithGroupsManager extends UserManager {
     LANG = staticLang;
 
     protected mediaserver: NxSystemRestAPI3;
-    userGroups: IdToGroup;
+    userGroups: IdToGroup = DefaultUserGroupsToId;
     private powerUserGroups = new Set<string>([AdminGroups.powerUserGroup]);
     protected _ownerEmail: string;
     protected locale: string;
@@ -235,6 +236,17 @@ export class UserWithGroupsManager extends UserManager {
     override processUsers(usersWithGroups: NxUser[]): NxUser[] {
         if (!Array.isArray(usersWithGroups)) {
             return [];
+        }
+        // This is only true when you get users from clouddb.
+        if (usersWithGroups?.[0].accessRole) {
+            usersWithGroups = usersWithGroups.map(user => ({
+                ...user,
+                attributes: 'readonly',
+                groupIds: [user.userRoleId],
+                permissions: 'none',
+                resourceAccessRights: {},
+                type: 'cloud',
+            })) as NxUser[];
         }
         /**
          * individual camera rights set by `resourceAccessRights` on the user object, but not implemented yet

front_end/libs/services/system/modules/nx-system-old-module.ts
@@ -9,7 +9,6 @@ import {
     Subject,
     timer,
     ReplaySubject,
-    of,
 } from 'rxjs';
 import {
     auditTime,
@@ -312,11 +311,22 @@ export class NxSystemOldModule extends NxSystemModuleBase {
                 timer(0, updateInterval)
                     .pipe(
                         switchMap(() => this.getInfo(true, false, true)),
-                        concatMap((value, index) => {
+                        concatMap(async (value, index) => {
                             if (index === 0) {
+                                if (this.isOnline) {
+                                    try {
+                                        await this.proxied.cameraManager.updateSystemCameras();
+                                        await firstValueFrom(
+                                            this.proxied.serverManager.getForceServers(false),
+                                        );
+                                        await this.getUsers(true, true);
+                                    } catch {
+                                        // Something went wrong getting the initial data
+                                    }
+                                }
                                 this.systemReady$.next(this);
                             }
-                            return of(value);
+                            return value;
                         }),
                         takeUntil(this.killPoll$),
                     )