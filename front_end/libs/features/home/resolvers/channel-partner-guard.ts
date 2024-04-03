d3fb87b85ab6daac8e1570506abb6c03d0e3a634
Chris Czach <czach@networkoptix.com>
2024-04-03 15:35:50 +0000

    CLOUD-13193: Add guard for report viewer only access
    # Problem
    Channel partners page shouldn't be show for report viewer only access
    
    # Solution
    Add route guard for when user only has report viewer access
    
    (cherry picked from commit 37ccfe0fe62f9167d389a614824cf8f127e6654b)

---
+++ front_end/libs/features/home/resolvers/channel-partner-guard.ts
@@ -0,0 +1,27 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import { selectCurrentPartner } from '@store/channel-partners/channel-partners.selectors';
+
+import { PermissionsStore } from '../store/permissions/permissions.store';
+
+export const ChannelPartnerGuard: CanActivateFn = async () => {
+    const permissionsStore = inject(PermissionsStore);
+    const canViewChannelPartner = [
+        permissionsStore.canViewSubChannels$$(),
+        permissionsStore.canViewPartnerSettings$$(),
+        permissionsStore.canViewPartnerSettings$$(),
+        permissionsStore.canViewPartnerUsers$$(),
+    ].some(Boolean);
+
+    if (!canViewChannelPartner) {
+        const currentPartnerId = inject(Store).selectSignal(selectCurrentPartner)()?.id;
+        await inject(Router).navigate(
+            permissionsStore.canViewPartnerReports$$()
+                ? ['reports', currentPartnerId]
+                : ['home', 'shared'],
+        );
+    }
+    return canViewChannelPartner;
+};
