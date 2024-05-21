5a0506e0abc0956fcb7ba552d3870686458bb397
Kyrylo Apanovich <kapanovich@networkoptix.com>
2024-05-21 05:22:40 +0300

    CLOUD-13730: Investigate 4xx and 5xx
    # Problem
    - 400 errors at internal endpoint caused by X-Original-Host header value with port number
    - "group" membership in internal user's systems response
    # Solution
    - remove port number from host name string
    - rename `group` membership to `organization`

--- channel_partners/src/partners/tests/test_middleware.py
+++ channel_partners/src/partners/tests/test_middleware.py
@@ -6,7 +6,7 @@ from rest_framework.test import APIRequestFactory
 from partners.middleware import cloud_host_middleware
 
 
-def test_cloud_host_middleware(cloud_test_host):
+def test_cloud_host_middleware(cloud_test_host, mocker):
     get_response = MagicMock()
     hostname = f"{uuid4()}"
     middleware = cloud_host_middleware(get_response)
@@ -18,3 +18,15 @@ def test_cloud_host_middleware(cloud_test_host):
     request = APIRequestFactory().get('/')
     response = middleware(request)
     assert request.cloud_host is None
+
+    # Test the case where the X-Original-Host passed with host number
+
+    request = APIRequestFactory(SERVER_NAME='localhost').get('/')
+    headers_mock = MagicMock()
+    headers_mock.get = MagicMock(return_value=f"{cloud_test_host.hostname}:8080")
+    mocker.patch.object(request, 'headers', headers_mock)
+    request.headers = headers_mock
+
+    response = middleware(request)
+    headers_mock.get.assert_called_with('X-Original-Host')
+    assert request.cloud_host == cloud_test_host
