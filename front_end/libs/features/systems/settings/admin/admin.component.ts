325f3966cb5a53f11738f87621625c27a3e7f1eb
Nick Hartleb <nhartleb@networkoptix.com>
2024-05-30 11:50:13 -0700

    CLOUD-13779: Updated permission check so that all "Administrator" Groups can disconnect
    # Problem
    We want all admins to be able to remove the system from cloud.
    
    # Solution
    Remove the check for if the system is in an org or not.
    
    (cherry picked from commit fa01957c2a8bcd97b01f249f452a0b0d339f43a4)

--- front_end/libs/features/systems/settings/admin/admin.component.ts
+++ front_end/libs/features/systems/settings/admin/admin.component.ts
@@ -235,9 +235,7 @@ export class NxSystemAdminComponent implements OnInit, OnDestroy, AfterViewInit 
     );
 
     canManageSystem$$ = computed(
-        () =>
-            this.system.permissionManager.isOwner$$() &&
-            (this.cloudSystemType$$() !== 'org' || this.manageOrgSystems$$()),
+        () => this.system.permissionManager.isOwner$$() || this.manageOrgSystems$$(),
     );
 
     systemOfferedToUser$$ = computed<boolean>(() => {
