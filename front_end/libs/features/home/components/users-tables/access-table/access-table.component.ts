1c9151c85dafd77d18ad4cfe6838bfba9e1d750e
Vincent Lee <vlee@networkoptix.com>
2024-06-04 03:36:53 +0300

    CLOUD-13737: Hide permission drop downs for Org Admins
    # Problem
    
    The Org Admin's permission dropdown should be hidden when there's only 1 Org Admin left in the Users/Access table
    
    Edit: The ticket says that the Logged in Org Admin should not be able to change their own permissions, but this condition is outdated. This should only be the case if there are no other Org Admin in the Org
    
    # Changes
    
    * Enable dropdown for logged in Org Admin if there is another Org Admin in the Org
    * Move reused `getDisplayRole()` function to the abstract since it's being used in multiple places
    
    ![Screen Recording 2024-06-03 at 1.58.54 PM.mov](/uploads/f07fed622fe08333c9876accc2dc0b23/Screen_Recording_2024-06-03_at_1.58.54_PM.mov)
    
    Related to CLOUD-13737
    
    (cherry picked from commit 282f7a533c27ef6dd71ea6039f759768305cb116)

--- front_end/libs/features/home/components/users-tables/access-table/access-table.component.ts
+++ front_end/libs/features/home/components/users-tables/access-table/access-table.component.ts
@@ -151,8 +151,8 @@ export class NxUsersAccessTableComponent extends AbstractUserTableDirective {
         const hasOnlyOneAdmin = checkForOneAdmin();
         if (
             currentUser?.isOrgUser &&
-            hasOnlyOneAdmin &&
-            currentUser.rolesIds?.includes(OrgRoleIds.OrgAdmin)
+            currentUser.rolesIds?.includes(OrgRoleIds.OrgAdmin) &&
+            hasOnlyOneAdmin
         ) {
             return false;
         }
