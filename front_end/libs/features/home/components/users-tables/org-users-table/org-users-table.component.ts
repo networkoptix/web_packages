efc5fd5bc4c71b0bc4dfeecc37ed4911162f644f
Kimberly Vandiver <kvandiver@networkoptix.com>
2024-06-24 21:37:05 +0000

    CLOUD-13721: Update Delete Org Admin Tooltip
    # Changes
    Updated the tooltip for deleting single org admin to match new spec.

--- front_end/libs/features/home/components/users-tables/org-users-table/org-users-table.component.ts
+++ front_end/libs/features/home/components/users-tables/org-users-table/org-users-table.component.ts
@@ -6,6 +6,7 @@ import { NxCheckAllContainerDirective } from '@components/checkbox/checkbox-chec
 import { NxCheckAllDirective } from '@components/checkbox/checkbox-check-all.directive';
 import { NxSelectV2Module } from '@components/select-v2/select-v2.module';
 import { DIALOG_SIZE } from '@dialogs/dialog-config-v2';
+import { NxTooltipV2Directive } from '@directives/tooltip-v2/tooltip-v2.directive';
 import { UserRecord } from '@pages/home/components/users/channel-partner-users/channel-partner-users.types';
 import { HEADER_ITEM } from '@pages/home/home.types';
 import { OrgRoleIds } from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
@@ -24,6 +25,7 @@ import { AbstractUserTableDirective } from '../abstract-user-table/abstract-user
         NxCheckAllContainerDirective,
         NxCheckAllDirective,
         NxSelectV2Module,
+        NxTooltipV2Directive,
     ],
 })
 export class NxOrgUsersTableComponent extends AbstractUserTableDirective {
