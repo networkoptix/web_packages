0c8740c2bb25a484d23150e2376adb268ce3728c
Kyrylo Apanovich <kapanovich@networkoptix.com>
2024-06-27 02:43:25 +0300

    CLOUD-14028: Add ROOT_NAME variable to creating root channel partner command in container
    # Changes
    - add $ROOT_NAME env variable to `create_root_channel_partner` call in container entrypoint. 
    # Reason for the change
    - This variable must be passed to command to define name of root channel partner in private cloud
    
    (cherry picked from commit 9c26be1b8921fc2695071a85d5fdc85b0201f047)

--- channel_partners/build/channel_partners_prod/entrypoint.sh
+++ channel_partners/build/channel_partners_prod/entrypoint.sh
@@ -8,7 +8,7 @@ do
     case "$command" in
         migratedb)
             python manage.py migrate
-            python manage.py runscript create_root_channel_partner --script-args ${INSTANCE_NAME}
+            python manage.py runscript create_root_channel_partner --script-args ${INSTANCE_NAME} "${ROOT_NAME}"
         ;;
         web)
             exec gunicorn channel_partners.wsgi:application --capture-output --workers ${WEB_WORKERS} --bind :8000 --log-level=info --timeout 300 -k gevent
