023d591cf7ece58ae1aa8c791cb3d6c902b809fa
Nick Hartleb <nhartleb@networkoptix.com>
2024-06-27 20:30:22 -0700

    CLOUD-13896: Fixed issue where the error counter never reset
    # Problem
    When a device had playback issues the error count never restarted when switching devices. As a result the player wouldn't give the device time to fix itself.
    
    # Solution
    Added a computed signal that fires anytime the selected camera changes to ensure that error counts are used from the previous device.

--- front_end/libs/features/systems/view/vms-client/submodules/playback/components/player/player.component.ts
+++ front_end/libs/features/systems/view/vms-client/submodules/playback/components/player/player.component.ts
@@ -1,5 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { AfterViewInit, Component, effect, EventEmitter, OnDestroy, Output } from '@angular/core';
+import {
+    AfterViewInit,
+    Component,
+    computed,
+    effect,
+    EventEmitter,
+    OnDestroy,
+    Output,
+} from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { SessionStorageService } from 'ngx-webstorage';
 import { skip } from 'rxjs/operators';
@@ -67,6 +75,12 @@ export class PlayerComponent implements OnDestroy, AfterViewInit {
 
         effect(() => {
             this.rotateDeg = this.vms.state$$().selectedCamera?.rotation || 0;
+        });
+
+        const selectedCameraId$$ = computed(() => this.vms.state$$().selectedCameraId);
+        effect(() => {
+            selectedCameraId$$();
+            this.playerErrorCount = 0;
         });
     }
 
