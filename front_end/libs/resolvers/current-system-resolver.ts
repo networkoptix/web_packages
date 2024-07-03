719685a13ef7f649081e736cc307a48c456e1162
Chris Czach <czach@networkoptix.com>
2024-07-03 18:54:04 +0300

    CLOUD-12510: Ensure that permissions are initialized within currentSystemResolver
    # Changes
    Update currentSystemResolver to ensure that permissions are loaded before resolving.
    # Reason for the change
    Redirect to home can get triggered in multiple places if permissions aren't initialized.

--- front_end/libs/resolvers/current-system-resolver.ts
+++ front_end/libs/resolvers/current-system-resolver.ts
@@ -1,10 +1,15 @@
-import { inject } from '@angular/core';
+import { Injector, inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 
 import { NxSystem } from '@services/system.service/system';
 import { NxSystemService } from '@services/system.service/system.service';
 
-export const currentSystemResolver: ResolveFn<NxSystem> = (
+export const currentSystemResolver: ResolveFn<NxSystem> = async (
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-) => inject(NxSystemService).getCurrentSystem();
+) => {
+    const system = inject(NxSystemService).getCurrentSystem();
+    const injector = inject(Injector);
+    await system.permissionManager.permissionsInitialized(injector);
+    return system;
+};
