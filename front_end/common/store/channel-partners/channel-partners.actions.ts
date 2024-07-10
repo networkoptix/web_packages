networkoptix/web_packages

commit 078bece0c000c9f01091199642bea26630316897

CLOUD-14079: Added feature flag for service usage reports
# Summary
Hiding service until next phase of channel partners

front_end/libs/services/nx-header.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
-import { BehaviorSubject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
 
 import { environment } from '@environments/environment';
 import staticLang from '@language_static';
+import { nxConfig } from '@services/nx-config/config';
 import { NxSystem } from '@services/system.service/system';
 import { reportsRegex } from '@static-variables';
 
@@ -47,6 +48,8 @@ export class NxHeaderService {
         this.nodes$.next(menunodes);
     }
 
+    reportsSub: Subscription | undefined;
+
     constructor(
         private router: Router,
         private menusService: NxMenusService,
@@ -63,7 +66,20 @@ export class NxHeaderService {
                     if (matches) {
                         const entityType = matches[1];
                         const entityID = matches[2];
-                        this.dynamicallyUpdateReportsNode(entityType, entityID);
+                        const reportsLang = this.LANG.appHeader.headerMenuNodes.reports;
+                        if (this.reportsSub) {
+                            this.reportsSub.unsubscribe();
+                        }
+                        this.reportsSub = this.nodes$
+                            .pipe(
+                                filter(nodes =>
+                                    nodes?.some(node => node.name === reportsLang.displayName),
+                                ),
+                                take(1),
+                            )
+                            .subscribe(() => {
+                                this.dynamicallyUpdateReportsNode(entityType, entityID);
+                            });
                     }
                 }
             }
@@ -177,6 +193,13 @@ export class NxHeaderService {
             if (serviceChangesNode) {
                 serviceChangesNode.url = `/reports/${entityType}/${entityId}/service-changes`;
             }
+
+            if (!nxConfig.featureFlags.channelPartnersAccessServiceUsage) {
+                reportsNode.nodes = reportsNode.nodes.filter(
+                    node => node.name !== reportsLang.nodes.serviceUsage.displayName,
+                );
+            }
+
             const newNodes = this.nodes.map(node =>
                 node.name === reportsLang.displayName ? reportsNode : node,
             );

cloud/cms/feature_flags/feature_flags.py
@@ -98,6 +98,7 @@ class FLAGS(metaclass=_FlagType):
     channel_partners_change_services_ui = ('Channel Partners Change Services UI', 'channelPartnersChangeServicesUI')
     channel_partners_reports_ui = ('Channel Partners Reports UI', 'channelPartnersReportsUI')
     channel_partners_support_ui = ('Channel Partners Support UI', 'channelPartnersSupportUI')
+    channel_partners_access_service_usage = ('Channel Partners - Access Usage Reports', 'channelPartnersAccessServiceUsage')
 
     rest_cookie_login = ('Rest Cookie Login', 'restCookieLogin')
 

front_end/libs/features/home/components/reports/reports.component.ts
@@ -6,6 +6,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import staticLang from '@language_static';
 import { Mode } from '@pages/home/components/reports/reports.types';
 import { NxChannelPartnersService } from '@services/channel-partners.service';
+import { nxConfig } from '@services/nx-config/config';
 import { icons } from '@static-variables';
 
 import { NxCardComponent } from '../card/card.component';
@@ -49,18 +50,21 @@ export class NxReportsComponent {
 
     services$$ = computed<ServiceCard[]>(() => {
         const entityUrl = this.entityUrl$$();
-        return [
+        const services = [
             {
                 name: staticLang.appHeader.headerMenuNodes.reports.nodes.serviceChanges.displayName,
                 icon: 'donut_chart.svg',
                 url: `/reports/${entityUrl}/service-changes`,
             },
-            {
+        ];
+        if (nxConfig.featureFlags.channelPartnersAccessServiceUsage) {
+            services.push({
                 name: staticLang.appHeader.headerMenuNodes.reports.nodes.serviceUsage.displayName,
                 icon: 'bar_chart.svg',
                 url: `/reports/${entityUrl}/service-usage`,
-            },
-        ];
+            });
+        }
+        return services;
     });
 
     protected readonly icons = icons;

front_end/libs/features/reports/entity-resolver.ts
@@ -53,12 +53,12 @@ export const entityResolver: ResolveFn<void> = async (
         const firstPartnerId = partners.get(partners.keys().next().value)?.id;
         const firstOrgId = organizations.get(organizations.keys().next().value)?.id;
         const defaultEntityId = partners.size ? firstPartnerId : firstOrgId;
-        await router.navigate(['reports', defaultEntityType, defaultEntityId, 'service-usage']);
+        await router.navigate(['reports', defaultEntityType, defaultEntityId, 'service-changes']);
     } else if (!urlHasValidEntityType || (!urlHasValidPartner && !urlHasValidOrg)) {
         await router.navigate(['404']);
     } else if (urlHasValidPartner && !tab) {
-        await router.navigate(['reports', 'channel-partner', entityIdFromUrl, 'service-usage']);
+        await router.navigate(['reports', 'channel-partner', entityIdFromUrl, 'service-changes']);
     } else if (urlHasValidOrg && !tab) {
-        await router.navigate(['reports', 'organization', entityIdFromUrl, 'service-usage']);
+        await router.navigate(['reports', 'organization', entityIdFromUrl, 'service-changes']);
     }
 };

front_end/libs/features/reports/reports.component.ts
@@ -77,7 +77,7 @@ export class NxReportsComponent implements OnInit {
                 ...urlSegments.slice(0, 2),
                 entityType,
                 entityId,
-                'service-usage',
+                'service-changes',
             ]);
         }
     }

front_end/libs/features/reports/reports.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+import { nxConfig } from '@services/nx-config/config';
 
 import { entityNameResolver } from './entity-name-resolver';
 import { entityResolver } from './entity-resolver';
@@ -22,6 +24,11 @@ const routes: Routes = [
                         children: [
                             {
                                 path: 'service-usage',
+                                canActivate: [
+                                    () =>
+                                        nxConfig.featureFlags.channelPartnersAccessServiceUsage ||
+                                        inject(Router).navigate(['/reports']),
+                                ],
                                 children: [
                                     {
                                         path: '',

front_end/libs/services/nx-config/base-config.ts
@@ -193,6 +193,7 @@ const FeatureFlagKeys = [
     'channelPartnersChangeServicesUI',
     'channelPartnersReportsUI',
     'channelPartnersSupportUI',
+    'channelPartnersAccessServiceUsage',
     // Layouts Feature
     'layouts',
     'layoutsNonChrome',

commit b8b8ffd24058a6df0e32279686d6ba06ebd12e2c

CLOUD-14094, CLOUD-14133: Fixes for loading home pages + l2 menu
# CLOUD-14133
## Problem
Selectors weren't setting the rootOrgs in the store when getting all organizations resulting in users not being able to access their orgs.
## Solution
Updated the setChannelPartnersAndOrgs action to set the rootOrganizations.

# CLOUD-14094
## Problem
Checks added for reports in 078bece0c000c9f01091199642bea26630316897 caused a timing issue resulting in the menu being blank.
## Solution
Added an escape hatch cycleL2Menu$ that updates the L2 menu.

# Changes
- Added a new method for forcing the L2 menu to update.
- Updates to the change-partners store
  - Changed setChannelPartnersAndOrgs selectors to set rootOrganizations.
  - Change filters for filtering out org systems using the selectAllOrganizations selector.

front_end/common/store/channel-partners/channel-partners.actions.ts
@@ -41,7 +41,11 @@ export const setRootOrganizations = createAction(
 
 export const setChannelPartnersAndOrgs = createAction(
     '[Channel Partners] Set Channel Partners and Orgs',
-    props<{ channelPartners: ChannelPartner[]; organizations: Organization[] }>(),
+    props<{
+        channelPartners: ChannelPartner[];
+        organizations: Organization[];
+        rootOrganizations: Organization[];
+    }>(),
 );
 
 export const setChannelPartnersAndRootOrgs = createAction(

front_end/common/store/channel-partners/channel-partners.effects.ts
@@ -32,19 +32,25 @@ export class ChannelPartnersEffects {
                     this.CPService.getChannelPartners(),
                     this.CPService.getOrganizations(includeChildOrgs),
                 ]).pipe(
-                    map(([channelPartners, organizations]) =>
-                        includeChildOrgs
+                    map(([channelPartners, organizations]) => {
+                        const partnerIds = new Set(channelPartners.map(cp => cp.id));
+                        return includeChildOrgs
                             ? {
                                   type: ChannelPartnerActions.setChannelPartnersAndOrgs.type,
                                   channelPartners,
-                                  organizations,
+                                  organizations: organizations.filter(org =>
+                                      partnerIds.has(org.channelPartner),
+                                  ),
+                                  rootOrganizations: organizations.filter(
+                                      org => !partnerIds.has(org.channelPartner),
+                                  ),
                               }
                             : {
                                   type: ChannelPartnerActions.setChannelPartnersAndRootOrgs.type,
                                   channelPartners,
                                   rootOrganizations: organizations,
-                              },
-                    ),
+                              };
+                    }),
                 ),
             ),
         );

front_end/common/store/channel-partners/channel-partners.reducer.ts
@@ -78,12 +78,13 @@ export const channelPartnersReducer = createReducer(
     ),
     on(
         ChannelPartnerActions.setChannelPartnersAndOrgs,
-        (state, { channelPartners, organizations }): ChannelPartnersState => ({
+        (state, { channelPartners, organizations, rootOrganizations }): ChannelPartnersState => ({
             ...state,
             hasStoreLoaded: true,
             channelPartnersAndOrgsLoadState: LoadingState.LOADED,
             channelPartners: sortEntityByName(channelPartners),
             organizations: sortEntityByName(organizations),
+            rootOrganizations: sortEntityByName(rootOrganizations),
         }),
     ),
     on(

front_end/common/store/channel-partners/channel-partners.selectors.ts
@@ -56,6 +56,10 @@ export const selectOrganizations = createSelector(
     selectChannelPartnersState,
     state => state.organizations,
 );
+export const selectAllOrganizations = createSelector(selectChannelPartnersState, state => [
+    ...state.organizations,
+    ...state.rootOrganizations,
+]);
 
 export const selectChannelStructure = createSelector(
     selectChannelPartnersState,

front_end/libs/components/header/new-header/new-header.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NavigationEnd, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Store } from '@ngrx/store';
 import { cloneDeep } from 'lodash-es';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { debounceTime, filter, map } from 'rxjs/operators';
 
 import { accountSelectors } from '@common/store/account';
 import { NxMenusService } from '@services/menus.service';
@@ -30,6 +31,19 @@ export class NxNewHeaderComponent {
     loggedIn$ = this.store.select(accountSelectors.selectIsAuthenticated);
     isMobile$ = new BehaviorSubject<boolean>(false);
     systemCount$: Observable<number>;
+
+    cycleSub = this.headerService.cycleL2Menu$
+        .pipe(debounceTime(500), takeUntilDestroyed())
+        .subscribe(() => {
+            const nodes = this.displayedNodes;
+            if (nodes) {
+                const nodes = this.displayedNodes;
+                this.selectedNode = this.findNodeBasedOnURL(
+                    nodes,
+                    this.headerService.currentLocation?.path || this.router.url,
+                );
+            }
+        });
 
     constructor(
         public headerService: NxHeaderService,

front_end/libs/features/home/home.component.ts
@@ -9,9 +9,9 @@ import { filter, map, switchMap } from 'rxjs/operators';
 
 import * as CPActions from '@common/store/channel-partners/channel-partners.actions';
 import {
+    selectAllOrganizations,
     selectAreChannelPartnersAndOrgsLoading,
     selectChannelPartners,
-    selectOrganizations,
 } from '@common/store/channel-partners/channel-partners.selectors';
 import { NxNoSystemsComponent } from '@components/no-systems/no-systems.component';
 import { NxPreLoaderComponent } from '@components/placeholders/pre-loader/pre-loader.component';
@@ -45,7 +45,7 @@ export class NxHomeComponent implements OnInit {
         selectAreChannelPartnersAndOrgsLoading,
     );
 
-    organizations$ = this.store.select<Organization[]>(selectOrganizations);
+    organizations$ = this.store.select<Organization[]>(selectAllOrganizations);
     channelPartners$ = this.store.select<ChannelPartner[]>(selectChannelPartners);
     isPageLoading: boolean = true;
     isNoSystemsOrgOrChP: boolean = false;
@@ -176,7 +176,11 @@ export class NxHomeComponent implements OnInit {
                 }
             }
         }
+
         homeNode.nodes = nodes;
+        // For the l2 menu to reset itself.
+        this.headerService.cycleL2Menu$.next();
+
         if (redirect && redirectPath && this.isPageLoading) {
             this.router.navigateByUrl(`home/${redirectPath}`).then(() => {
                 this.isNoSystemsOrgOrChP = false;

front_end/libs/features/home/organizations/organization.component.ts
@@ -26,11 +26,9 @@ import { selectCurrentUser } from '@common/store/account/account.selectors';
 import * as CPActions from '@common/store/channel-partners/channel-partners.actions';
 import {
     selectCurrentOrganization,
-    selectCurrentPartnerId,
-    selectCurrentPartnerOrgs,
-    selectRootOrganizations,
     selectBanner,
     selectCurrentParentPartnerForChild,
+    selectAllOrganizations,
 } from '@common/store/channel-partners/channel-partners.selectors';
 import { NxAlertBlockComponent } from '@components/content-block/alert/block.component';
 import { NxHidableModule } from '@components/hidable/hidable.module';
@@ -163,10 +161,7 @@ export class NxOrganizationsComponent implements OnInit, OnDestroy {
     @Input() inChannelPartner: boolean = false;
 
     private account$$ = this.store.selectSignal<Account>(selectCurrentUser);
-    organizations$$ = this.store.selectSignal<Organization[]>(selectRootOrganizations);
-    currentPartnerOrganizations$$ =
-        this.store.selectSignal<Organization[]>(selectCurrentPartnerOrgs);
-    currentPartnerId$$ = this.store.selectSignal<string>(selectCurrentPartnerId);
+    organizations$$ = this.store.selectSignal<Organization[]>(selectAllOrganizations);
 
     sidebarSettings: CustomAccountProperty<SidebarSettings>;
     currentGroupId$$ = computed(() => this.cpService.paramStateHandler.state$$()?.params?.groupId);
@@ -255,12 +250,8 @@ export class NxOrganizationsComponent implements OnInit, OnDestroy {
             )
             .subscribe(id => {
                 const orgs = this.organizations$$();
-                const partnerOrgs = this.currentPartnerOrganizations$$();
                 const currOrg = this.currentOrganization$$();
-                if (
-                    (!orgs.find(o => o.id === id) && !partnerOrgs.find(o => o.id === id)) ||
-                    !currOrg
-                ) {
+                if (!orgs.find(o => o.id === id) || !currOrg) {
                     this.isValidOrg = false;
                     this.isLoading = false;
                     return;

front_end/libs/features/home/systems/systems.component.ts
@@ -9,7 +9,7 @@ import { HomeSystemListComponent } from '@pages/home/components/systems-list/sys
 import { Account } from '@services/account.service/account';
 import { NxSystemsService } from '@services/systems.service';
 import type { NxSystemInfo } from '@services/systems.service.types';
-import { selectOrganizations } from '@store/channel-partners/channel-partners.selectors';
+import { selectAllOrganizations } from '@store/channel-partners/channel-partners.selectors';
 import { NgChanges } from '@utils/ng-changes';
 
 import { SystemsDisplayMode } from '../home.types';
@@ -40,11 +40,11 @@ export class NxSystemsComponent implements OnChanges {
         const filterByIsMine = showPersonal
             ? (sys: NxSystemInfo) => sys.isMine
             : (sys: NxSystemInfo) => !sys.isMine;
-        const systems = this.systemsService.directAccessSystems$$().filter(filterByIsMine);
+        const systems = this.systemsService.directAccessSystems$$();
         if (!showPersonal) {
             const orgSet = new Set<string>(
                 this.store
-                    .selectSignal(selectOrganizations)()
+                    .selectSignal(selectAllOrganizations)()
                     .map(org => org.id),
             );
             return systems.filter(

front_end/libs/services/nx-header.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { BehaviorSubject, Subject, Subscription } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
 
 import { environment } from '@environments/environment';
@@ -49,6 +49,7 @@ export class NxHeaderService {
     }
 
     reportsSub: Subscription | undefined;
+    cycleL2Menu$ = new Subject<void>();
 
     constructor(
         private router: Router,