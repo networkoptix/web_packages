1b8b232623cdd2b5c9fc2dc0911f03b56bc6ba12
Sean Pagnon <spagnon@networkoptix.com>
2024-07-14 23:56:00 +0000

    CLOUD-14121: Track report dates as url query param
    For Service Changes page - read date url query param on load to initialize date filter value, and update url param on date filter change
    
    (cherry picked from commit 5110b3cc4c594f27f881415ad599dcafc89f5972)

--- front_end/libs/features/reports/month-select/base-month-page.component.ts
+++ front_end/libs/features/reports/month-select/base-month-page.component.ts
@@ -1,7 +1,10 @@
-import { Injectable, computed, inject, signal } from '@angular/core';
+import { DestroyRef, Injectable, computed, effect, inject, signal } from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { take } from 'rxjs';
 
 import { NxDateTimeFormatService } from '@services/datetime-format.service';
 import { offsetDate } from '@utils/general';
+import { paramModel } from '@utils/signals';
 
 /** Monthly filtering functionality for service pages */
 @Injectable()
@@ -32,4 +35,46 @@ export abstract class BaseMonthPageComponent {
         const d = `${date.getDate()}`.padStart(2, '0');
         return `${Y}-${m}-${d}`;
     }
+
+    protected parseDateParam(dateParam: string | undefined):
+        | {
+              year: number;
+              month: number;
+          }
+        | undefined {
+        if (!dateParam) {
+            return undefined;
+        }
+
+        const [year, month] = dateParam.split('-').map(part => parseInt(part));
+        const isMonthValid = !!month && month >= 1 && month <= 12;
+        if (!year || !isMonthValid) {
+            return undefined;
+        }
+        return { year, month };
+    }
+
+    dateParam = paramModel('date');
+    dateParam$ = toObservable(this.dateParam);
+    constructor(private destroyRef: DestroyRef) {
+        // Set year/month using date query param if available + valid on initial page load
+        this.dateParam$.pipe(take(1), takeUntilDestroyed(this.destroyRef)).subscribe(dateParam => {
+            const parsedDate = this.parseDateParam(dateParam);
+            if (parsedDate) {
+                this.year.set(parsedDate.year);
+                this.monthIndex.set(parsedDate.month - 1);
+            } else {
+                const currentDate = new Date();
+                this.year.set(currentDate.getFullYear());
+                this.monthIndex.set(currentDate.getMonth());
+            }
+        });
+    }
+
+    dateEffect = effect(
+        () => {
+            this.dateParam.set(this.requestStartString());
+        },
+        { allowSignalWrites: true },
+    );
 }
