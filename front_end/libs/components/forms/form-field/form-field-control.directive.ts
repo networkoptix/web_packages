adb0d5adcc4188f71cfb3fdce98fa289855adcc0
Andrew Wu <awu@networkoptix.com>
2024-07-16 00:19:12 +0000

    CLOUD-14179: Dialog and error spec updates
    ## Changes
    - Hid character counter until character limit is exceeded (appeasement for design while exact design of error state is hashed out)
    - Added ability for arbitrary character limits
    - Documented pattern for dialog buttons

--- front_end/libs/components/forms/form-field/form-field-control.directive.ts
+++ front_end/libs/components/forms/form-field/form-field-control.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, input } from '@angular/core';
 
 import { NxEscapeGlobalStyleDirective } from '@directives/escape-global-style.directive';
 
@@ -10,5 +10,14 @@ import { NxEscapeGlobalStyleDirective } from '@directives/escape-global-style.di
     hostDirectives: [NxEscapeGlobalStyleDirective],
 })
 export class NxFormFieldControlDirective {
+    /** Max length for character counter in form field.
+     *
+     * Doesn't limit actual string value.
+     *
+     * Syncs with max lengths used in `NxValidators` if `auto` is used
+     * AND `type` property is set on `<input />` element.
+     */
+    maxLength = input<number | 'auto'>('auto', { alias: 'nxFormFieldMaxLength' });
+
     constructor(public host: ElementRef<HTMLElement>) {}
 }
