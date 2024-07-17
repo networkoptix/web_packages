645c8149d568457baa3c8027742844bed6164b7e
Kimberly Vandiver <kvandiver@networkoptix.com>
2024-07-17 20:17:31 +0000

    CLOUD-13776: Allow Org Admin Edit/Delete
    # Problem
    It wasn't possible to delete or edit the only Organization Administrator if CP Access was 'Organization Administrator'.
    
    # Changes
    Added check to determine if CP Access is 'Organization Administrator' to allow editing/deleting if true.

--- front_end/libs/features/home/components/users-tables/access-table/access-table.component.ts
+++ front_end/libs/features/home/components/users-tables/access-table/access-table.component.ts
@@ -38,6 +38,7 @@ export class NxUsersAccessTableComponent extends AbstractUserTableDirective {
     searchQueryModel = paramModel('search');
 
     currentOrg$$ = this.store.selectSignal(selectCurrentOrganization);
+    currAccessLevel = this.currentOrg$$()?.channelPartnerAccessLevel;
     email$$ = this.routerState.email;
     orgRoles$$ = this.cpService.organizationRoles$$;
     orgRecords$$ = this.orgUsersStore.usersByGroupSignalFactory();
@@ -141,9 +142,10 @@ export class NxUsersAccessTableComponent extends AbstractUserTableDirective {
         }
         const users = this.orgUsersStore.tableUsers$$();
         const currentUser = users.find(user => user.email === this.email$$());
-        if (!currentUser?.isOrgUser) {
+        if (this.currAccessLevel === OrgRoleIds.OrgAdmin || !currentUser?.isOrgUser) {
             return true;
         }
+
         const checkForOneAdmin = (): boolean => {
             let admins = 0;
             for (const user of users) {
