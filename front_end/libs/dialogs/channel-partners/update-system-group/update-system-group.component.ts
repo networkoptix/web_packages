e97b745878aee54e952e57eb9a104ffcbb35c476
Vincent Lee <vlee@networkoptix.com>
2024-07-18 20:31:03 +0000

    CLOUD-13930: Migrate update-system-group dialog
    # Problem
    
    We need to migrate CP dialogs to V2 and to use the new dialog structure
    
    # Changes
    
    * Update `update-system-group` to the new dialog structure
    * Updated to dialogs V2
    
    Notes:
    
    The component is not included in `non-control-flow-templates.js`, so there will be no changes here

--- front_end/libs/dialogs/channel-partners/update-system-group/update-system-group.component.ts
+++ front_end/libs/dialogs/channel-partners/update-system-group/update-system-group.component.ts
@@ -1,20 +1,19 @@
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
-import { Component, Inject, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { firstValueFrom } from 'rxjs';
 
-import { NxProcessButtonComponent } from '@components/process-button/process-button.component';
-import { NxProcessCancelButtonComponent } from '@components/process-cancel-Button/process-cancel-button.component';
+import { NxFormFieldModule } from '@components/forms/forms.module';
+import { NxInputComponent } from '@components/forms/input/input.component';
+import { NxValidators } from '@components/forms/validators';
+import { NxAsyncActionButtonComponent } from '@dialogs/async-action-button/async-action-button.component';
+import { createAsyncAction } from '@dialogs/async-action-button/create-async-action';
 import { ModalBase } from '@dialogs/modal-base';
 import { NxChannelPartnersService } from '@services/channel-partners.service';
 import { PatchGroup } from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
-import { NxProcessService } from '@services/process.service';
-import { Process } from '@services/process.service/process';
 
 import type { UpdateSystemGroup as DT } from '../../dialogs.types';
-
-const FIELDS_MISSING = 'FIELDS_MISSING';
 
 @Component({
     selector: 'nx-modal-update-system-group-content',
@@ -22,42 +21,40 @@ const FIELDS_MISSING = 'FIELDS_MISSING';
     styleUrl: 'update-system-group.component.scss',
     standalone: true,
     imports: [
-        FormsModule,
-        NxProcessButtonComponent,
-        NxProcessCancelButtonComponent,
+        ReactiveFormsModule,
+        NxFormFieldModule,
+        NxInputComponent,
+        NxAsyncActionButtonComponent,
         TranslateModule,
     ],
 })
 export class UpdateSystemGroupModalContent extends ModalBase<DT['return']> {
-    name: string = '';
-    updateSystemGroupProcess: Process;
+    private folderNameControl = new FormControl('', {
+        validators: NxValidators.text(),
+        nonNullable: true,
+    });
 
-    @ViewChild('updateSystemGroupFrom') form: NgForm;
+    formGroup = new FormGroup({
+        renameFolder: this.folderNameControl,
+    });
 
     constructor(
-        private processService: NxProcessService,
         private cpService: NxChannelPartnersService,
         public dialogRef: DialogRef<DT['return']>,
         @Inject(DIALOG_DATA) private groupId: DT['data'],
     ) {
         super(dialogRef);
-        this.updateSystemGroupProcess = this.processService.createProcess(
-            () => {
-                if (!this.name) {
-                    return Promise.reject({ status: FIELDS_MISSING });
-                }
-                const data: PatchGroup = {
-                    name: this.name,
-                };
-                return firstValueFrom(this.cpService.patchGroup(this.groupId, data));
-            },
-            { ignoreError: true },
-            res => this.close(res),
-            err => {
-                if (err.status === FIELDS_MISSING) {
-                    this.form.form.markAllAsTouched();
-                }
-            },
-        );
     }
+
+    updateSystemGroupProcess = createAsyncAction({
+        action: () => {
+            const data: PatchGroup = {
+                name: this.folderNameControl.value,
+            };
+            return firstValueFrom(this.cpService.patchGroup(this.groupId, data));
+        },
+        success: res => {
+            this.close(res);
+        },
+    });
 }
