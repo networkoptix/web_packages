76cea0e5fecbb8464ae2cf1b337d96ecc40ef85d
Chris Czach <czach@networkoptix.com>
2024-07-25 05:07:36 +0300

    CLOUD-14149: Update timeout for hiding ribbons
    # Problem
    Ribbon hidding too quickly with inactivity
    # Solution
    Increase timeout

--- front_end/libs/components/layout-grid/layout-grid.component.ts
+++ front_end/libs/components/layout-grid/layout-grid.component.ts
@@ -568,7 +568,7 @@ export class NxLayoutGridComponent {
     unsavedStates = staticLang.layouts.unsavedStates;
 
     mouseMoving$ = fromEvent(document, 'mousemove').pipe(
-        switchMap(() => of(false).pipe(delay(10000), startWith(true))),
+        switchMap(() => of(false).pipe(delay(15_000), startWith(true))),
         distinctUntilChanged(),
         shareReplay({ bufferSize: 1, refCount: false }),
     );
