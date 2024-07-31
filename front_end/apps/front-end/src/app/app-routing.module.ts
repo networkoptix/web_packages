251e0e985a3b17709cf552282568dd615d5ce1ab
Andrew Wu <awu@networkoptix.com>
2024-07-31 16:33:12 -0700

    CLOUD-14034: Redirect to no system access page
    The no system access placeholder is supposed to be shown on the system's page, but the guard was redirecting to root instead when a user didn't have permission to access the system.
    
    Silently redirect the user a placeholder page without modifying the url to make it behave like pre-channel partners. Also restored the no access svg accidentally removed in !7271.
    
    ![2024-07-29_16-40-12](/uploads/c65fcb7b7aba7961e5ad19671456138b/2024-07-29_16-40-12.mp4)

--- front_end/apps/front-end/src/app/app-routing.module.ts
+++ front_end/apps/front-end/src/app/app-routing.module.ts
@@ -61,6 +61,18 @@ const lazyRoutes: Routes = [
         },
     },
     {
+        path: 'systems/no-access',
+        loadComponent: () =>
+            import('@components/placeholders/page/page-placeholder.component').then(
+                c => c.NxPagePlaceholderComponent,
+            ),
+        resolve: {
+            type: () => 'FAILED_TO_ACCESS_SYSTEM',
+            withFooter: () => true,
+            showMainButton: () => true,
+        },
+    },
+    {
         path: 'systems/:systemId/no-access/:systemName',
         loadComponent: () =>
             import('@components/placeholders/no-access/no-access.component').then(
