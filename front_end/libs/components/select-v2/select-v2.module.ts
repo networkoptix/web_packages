20c5884bb01ae26f80ba67b29b4772446ca5923e
Vincent Lee <vlee@networkoptix.com>
2024-08-08 19:32:58 +0000

    CLOUD-12776: Add descriptions to CP roles dropdown
    # Problem
    
    For CP User Table, role descriptions should be displayed under the roles in Role Dropdowns
    
    Figma: https://www.figma.com/design/o0rIklvaLhnV91RTaEus7C/CP?node-id=21274-28816&m=dev
    
    ![Screenshot 2024-07-30 at 4.26.27 PM.png](/uploads/cdab2ac7fa041420bf5ecaabe6205883/Screenshot_2024-07-30_at_4.26.27_PM.png)
    
    # Changes
    
    Added new component `paragraph-select-item.component.ts`. If there are 2 children, the 1st will be for the Option Name, 2nd child will be the text that displays under the option _in the dropdown_
    
    **Let me know if the docs are unclear**
    
    The background for each Option becomes blue when hovered
    
    ![Screenshot 2024-08-02 at 1.23.50 PM.png](/uploads/72c9b8387e37e5cde649e807b2efcaa2/Screenshot_2024-08-02_at_1.23.50_PM.png)
    
    ![Screenshot 2024-08-02 at 1.24.20 PM.png](/uploads/3cc5d27aa0370c5017510b54ec05e0cc/Screenshot_2024-08-02_at_1.24.20_PM.png)
    
    (cherry picked from commit 06b23c778e74e04bb7da8a3c24ded53763bb9395)

--- front_end/libs/components/select-v2/select-v2.module.ts
+++ front_end/libs/components/select-v2/select-v2.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { NxMultiSelectV2ItemComponent } from './items/multi-select-item/multi-select-item.component';
+import { NxParagraphSelectItemComponent } from './items/paragraph-select-item/paragraph-select-item.component';
 import { NxSelectV2ItemComponent } from './items/select-item/select-item.component';
 import { NxMultiSelectV2Component } from './multi-select-v2.component';
 import { NxSelectV2Component } from './select-v2.component';
@@ -10,12 +11,14 @@ import { NxSelectV2Component } from './select-v2.component';
         NxSelectV2Component,
         NxMultiSelectV2Component,
         NxSelectV2ItemComponent,
+        NxParagraphSelectItemComponent,
         NxMultiSelectV2ItemComponent,
     ],
     exports: [
         NxSelectV2Component,
         NxMultiSelectV2Component,
         NxSelectV2ItemComponent,
+        NxParagraphSelectItemComponent,
         NxMultiSelectV2ItemComponent,
     ],
 })
