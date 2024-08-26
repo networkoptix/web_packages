e7721abbce63b8932017353c404102ec641f8e9c
Vincent Lee <vlee@networkoptix.com>
2024-08-27 01:01:33 +0300

    CLOUD-14420: Fix Org filters "undefined" console error
    # Problem
    
    Org User's table \> Left panel open \> Click on a folder \> this will filter users by the selected folder \> Type in the search query for the table
    
    * This will break the Org User's table filter
    * We get a bunch of console errors for `undefined` properties
    
    # Changes
    
    Check to see if the `user.accesslevel` and `user.groupRoles` exist before adding these queries to the filter
    
    Search is now working properly:
    
    ![Screen Recording 2024-08-19 at 2.13.08 PM.mov](/uploads/99aa58b770992cddfbb6a92997ba1b67/Screen_Recording_2024-08-19_at_2.13.08_PM.mov)
    
    (cherry picked from commit 96c34acdf43504898430523573e908c69b6620ad)

--- front_end/libs/features/home/store/org-users/org-users.store.ts
+++ front_end/libs/features/home/store/org-users/org-users.store.ts
@@ -199,13 +199,20 @@ function getUsersByModel(
             user.email,
             user.fullName,
             ...user.rolesIds.map(roleId => LANG.channelPartners.orgs.orgRoleInfo[roleId].name),
-            ...user.groupRoles.map(
-                groupRole => LANG.channelPartners.orgs.orgRoleInfo[groupRole.rolesIds[0]].name,
-            ),
-            ...user.groupRoles.map(groupRole => groupRole.name),
         ];
 
-        if (user.userType === UserType.ORGANIZATION) {
+        if (user.groupRoles) {
+            fieldsToFilterBy.push(
+                ...user.groupRoles.map(
+                    groupRole => LANG.channelPartners.orgs.orgRoleInfo[groupRole.rolesIds[0]].name,
+                ),
+                ...user.groupRoles.map(groupRole => groupRole.name),
+            );
+        }
+
+        if (user.accessLevel) {
+            fieldsToFilterBy.push(user.accessLevel.name);
+        } else if (user.userType === UserType.ORGANIZATION) {
             fieldsToFilterBy.push(orgName);
         }
 
