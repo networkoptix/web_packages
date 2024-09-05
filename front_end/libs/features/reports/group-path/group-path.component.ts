c1865228b104401084ac6de7a0c5d00558a44a4a
Tsanko Tsolov <ttsolov@networkoptix.com>
2024-09-06 00:21:03 +0300

    CLOUD-14155: In case of Long Name Part of the Name Should be Hidden
    # Added
    
    * truncating group path text in no space to show it
    
    ![Screenshot 2024-09-05 at 10.42.18 AM.png](/uploads/5f567adb361af2c74a0d910c5ec37648/Screenshot_2024-09-05_at_10.42.18_AM.png){width="525" height="467"}
    
    ![Screenshot 2024-09-05 at 10.42.43 AM.png](/uploads/b560b45c2180a2c6793dd385f37d6aff/Screenshot_2024-09-05_at_10.42.43_AM.png){width="523" height="491"}

--- front_end/libs/features/reports/group-path/group-path.component.ts
+++ front_end/libs/features/reports/group-path/group-path.component.ts
@@ -1,21 +1,100 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, ElementRef, input, OnDestroy } from '@angular/core';
 
 import { NxThemeAttributeDirective } from '@directives/theme-attribute.directive';
 
 @Component({
     selector: 'nx-group-path',
-    template: `
-        <span class="group-path">{{ groupPath()[0] }}</span>
-        {{ groupPath()[1] }}
-    `,
+    template: '', // No template, content is set in computedText$$
     styleUrls: ['./group-path.component.scss'],
     imports: [CommonModule],
     standalone: true,
     hostDirectives: [NxThemeAttributeDirective],
 })
-export class NxGroupPathComponent {
+export class NxGroupPathComponent implements OnDestroy {
     // groupPath is the output of getFormattedGroupPath() in NxGroupPathService
     // It should have the format [groupPathString, systemName]
     groupPath = input.required<string[]>();
+
+    measurement: HTMLDivElement;
+    target: ElementRef<HTMLElement>;
+
+    constructor(protected el: ElementRef<HTMLElement>) {
+        this.target = el;
+        this.createMeasurementElement();
+
+        effect(() => {
+            this.target.nativeElement.classList.add('nx-breadcrumbs');
+            this.target.nativeElement.innerHTML = this.computedText$$() || '';
+        });
+    }
+
+    computedText$$ = computed(() => {
+        if (this.groupPath()[0] === '') {
+            return this.groupPath()[1];
+        }
+
+        let txt = `<span class="group-path">${this.groupPath()[0]}</span>&nbsp;${this.groupPath()[1]}`;
+        const styleTarget = window.getComputedStyle(this.target.nativeElement);
+
+        // Find width of text displayed
+        this.measurement.innerHTML = txt;
+        // Ridiculous default values to help detect missing styling
+        this.measurement.style.fontWeight = styleTarget.fontWeight || '900';
+        this.measurement.style.fontSize = styleTarget.fontSize || '5px';
+        this.measurement.style.fontFamily = styleTarget.fontFamily || 'Roboto, sans-serif';
+
+        const styleElement = window.getComputedStyle(this.measurement);
+
+        const textWidth = parseInt(styleElement.width);
+        const maxTextWidth =
+            parseInt(styleTarget.width) -
+            parseInt(styleTarget.paddingLeft) -
+            parseInt(styleTarget.paddingRight);
+
+        if (textWidth > maxTextWidth) {
+            const segments = this.groupPath()[0].split('/');
+            if (segments[segments.length - 1] === '') {
+                segments.pop();
+            }
+
+            let idx = 1;
+            while (idx < segments.length) {
+                segments[idx] = '... ';
+                idx++;
+            }
+
+            const path = segments.join('/ ');
+            txt = `<span class="group-path">${path}/</span>&nbsp;${this.groupPath()[1]}`;
+            this.measurement.innerHTML = txt;
+            if (parseInt(this.getCssStyle(this.measurement, 'width')) > maxTextWidth) {
+                const truncated = '.../'; // in string is breaking closing span tag
+                txt = `<span class="group-path">${truncated}&nbsp;</span>${this.groupPath()[1]}`;
+            }
+        }
+
+        return txt;
+    });
+
+    getCssStyle(element: HTMLDivElement, prop: string): string {
+        return window.getComputedStyle(element, null).getPropertyValue(prop);
+    }
+
+    ngOnDestroy(): void {
+        this.measurement.remove();
+    }
+
+    private createMeasurementElement(): void {
+        this.measurement = document.createElement('div');
+        // styles needed to measure text width
+        this.measurement.style.position = 'absolute';
+        this.measurement.style.visibility = 'hidden';
+        this.measurement.style.height = 'auto';
+        this.measurement.style.width = 'auto';
+        this.measurement.style.whiteSpace = 'nowrap';
+
+        const newContent = document.createTextNode('');
+        this.measurement.appendChild(newContent);
+        document.body.appendChild(this.measurement);
+    }
 }
