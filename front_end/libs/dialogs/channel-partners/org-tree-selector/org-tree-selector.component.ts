e08b4e5c3e82a1bcde7fbaf94fbaff255c035475
Tsanko Tsolov <ttsolov@networkoptix.com>
2024-09-06 22:52:22 +0300

    CLOUD-12972: The folder that contains subfolder with search term
    # Fixes
    
    * auto expand folder's tree view to show search matches.

--- front_end/libs/dialogs/channel-partners/org-tree-selector/org-tree-selector.component.ts
+++ front_end/libs/dialogs/channel-partners/org-tree-selector/org-tree-selector.component.ts
@@ -294,8 +294,8 @@ export class NxOrgTreeSelectorComponent
         this.updateFolderState(highlightedId, newState);
     }
 
-    toggleFolderOpen(groupId: string): void {
-        const newState = !this.openFolders.has(groupId);
+    toggleFolderOpen(groupId: string, forceOpen = false): void {
+        const newState = forceOpen || !this.openFolders.has(groupId);
         this.updateFolderState(groupId, newState);
     }
 
@@ -432,7 +432,9 @@ export class NxOrgTreeSelectorComponent
                     lastAddedIndex = upTraversal[0];
                 }
                 while (upTraversal.length) {
-                    results.push(this.flatGroups[upTraversal.pop()!]);
+                    const upTraversalItem = this.flatGroups[upTraversal.pop()!];
+                    this.toggleFolderOpen(upTraversalItem.id, true);
+                    results.push(upTraversalItem);
                 }
 
                 results.push(group);
