3d045f125e0d7b763049f669aeac5259debbdb4b
Nick Hartleb <nhartleb@networkoptix.com>
2024-09-16 14:10:12 -0700

    CLOUD-14685: Fixed missing reports tab
    # Problem
    When reports reloads teh L2 menu is empty due to a timing issue with the header.
    
    # Solution
    Used the cycleL2Menu subject to force the L2 to update.
    
    # Changes
    - Added the Auth guard to reports.
    - Cleaned up dead code in the reports component.

--- front_end/apps/front-end/src/app/app-routing.module.ts
+++ front_end/apps/front-end/src/app/app-routing.module.ts
@@ -305,6 +305,7 @@ const lazyRoutes: Routes = [
     {
         path: 'reports',
         canMatch: [FeatureGuardActivate],
+        canActivate: [AuthGuard],
         data: {
             flag: FeatureFlagStrings.channelPartnersReportsUI,
         },
--- front_end/libs/features/reports/reports.component.ts
+++ front_end/libs/features/reports/reports.component.ts
@@ -1,29 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, computed } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
-import {
-    selectChannelPartners,
-    selectOrganizations,
-} from '@common/store/channel-partners/channel-partners.selectors';
 import { NxPreLoaderComponent } from '@components/placeholders/pre-loader/pre-loader.component';
 import { NxThemeAttributeDirective } from '@directives/theme-attribute.directive';
 import { NxAppStateService } from '@services/nx-app-state.service';
-import type {
-    ChannelPartner,
-    Organization,
-} from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
+import { NxHeaderService } from '@services/nx-header.service';
 
 import { NxReportsSidebarComponent } from './reports-sidebar/reports-sidebar.component';
-
-interface Item {
-    name: string;
-    value: string;
-    prefix?: string;
-}
 
 @Component({
     selector: 'nx-reports',
@@ -41,44 +27,11 @@ interface Item {
     standalone: true,
 })
 export class NxReportsComponent implements OnInit {
-    search = '';
-    channelPartners$$ = this.store.selectSignal<ChannelPartner[]>(selectChannelPartners);
-    organizations$$ = this.store.selectSignal<Organization[]>(selectOrganizations);
-
-    dropdownItems$$ = computed<Item[]>(() => {
-        const channelPartners = this.channelPartners$$();
-        const organizations = this.organizations$$();
-        const items = [
-            ...channelPartners.map(({ id, name }) => ({ name, value: `channel-partner/${id}` })),
-            ...organizations.map(({ id, name }) => ({
-                name,
-                value: `organization/${id}`,
-                prefix: '[org] ',
-            })),
-        ];
-        return items;
-    });
-
-    constructor(
-        private router: Router,
-        private store: Store,
-        private appStateService: NxAppStateService,
-    ) {}
+    private appStateService = inject(NxAppStateService);
+    private headerService = inject(NxHeaderService);
 
     ngOnInit(): void {
         this.appStateService.ready = true;
-    }
-
-    selectItem(selectedItem: Item | undefined): void {
-        if (selectedItem) {
-            const urlSegments = this.router.url.split('/');
-            const [entityType, entityId] = selectedItem.value.split('/');
-            this.router.navigate([
-                ...urlSegments.slice(0, 2),
-                entityType,
-                entityId,
-                'service-changes',
-            ]);
-        }
+        this.headerService.cycleL2Menu$.next();
     }
 }
