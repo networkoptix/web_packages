f755f06be4d04c81acb2f87b86e4ad9d2c5f0122
Kyrylo Apanovych <kapanovich@networkoptix.com>
2024-09-20 03:15:49 +0000

    CLOUD-14708: Create a middleware to simulate broken ChP and CLOUD-14710: Error in creating context for email emassages
    # Changes
    - CLOUD-14708. Created middleware to return 500 on all API requests when waffle switch is action. Middleware is not loaded on prod instance. 
    - CLOUD-14710. Fix context value for email templates.
    # Reason for the change
    {Reason(s)}
    
    (cherry picked from commit 8704836de3803d75cdd7d24e61a4b524ad23de5f)

---
+++ channel_partners/src/channel_partners/middleware.py
@@ -0,0 +1,18 @@
+import waffle
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
+
+
+TEST_BROKEN_SWITCH = 'broken_service'
+
+
+def broken_service_middleware(get_response):
+
+    def middleware(request: HttpRequest):
+        if not request.path.startswith('/partners/api') or not waffle.switch_is_active(TEST_BROKEN_SWITCH):
+            return get_response(request)
+        return HttpResponse(content=b'Broken Service Simulation', status=500)
+
+    return middleware
