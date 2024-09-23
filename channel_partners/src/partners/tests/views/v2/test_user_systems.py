4dff8fbcfe89465edf85d06e6a2dc32ebcb7cc3d
Kevin Brostoff <kbrostoff@networkoptix.com>
2024-09-23 19:31:51 +0000

    CLOUD-14574: Refactor Channel Partner User Access and System Permissions
    # Overview
    
    This merge request includes several changes to improve and fix issues related to channel partner user access and system permissions.
    ## Changes
    
    1. Updated `CloudUser.systems_memberships()` method to use `ChannelPartnerRoles.cpal_roles()` instead of hardcoded roles.
    
    2. Refactored `CloudSystemId.get_channel_partner_users()` method:
       - Excluded users with REPORTS_VIEWER role
       - Simplified return statement
    
    3. Added new `ChannelPartnerRoles.cpal_roles()` static method to return a list of channel partner administrator and manager roles.
    
    4. Added new test files:
       - `test_system_user.py`: Tests for system user access and permissions
       - `test_user_systems.py`: Tests for user systems access and permissions
    
    
    ## Testing
    
    New test cases have been added to cover the following scenarios:
    
    - System user access by different channel partner roles (admin, manager, viewer)
    - User systems access by different roles

---
+++ channel_partners/src/partners/tests/views/v2/test_user_systems.py
@@ -0,0 +1,187 @@
+import pytest
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+from partners.models import (
+    ChannelPartnerRoles,
+    OrganizationRoles,
+)
+from tools.helpers import cast_uuid
+
+
+class TestUserSystemsBugs:
+
+    @pytest.mark.parametrize("user, expected", [
+        ("admin", {"7121dc6f-8e56-4649-9db5-3645b92c5ab2"}),
+        ("manager", {"7121dc6f-8e56-4649-9db5-3645b92c5ab2"}),
+        ("viewer", set())
+    ], ids=["admin access should have data", "manager access should have data", "viewer access should not have data"])
+    def test_system_users_access_by_role(
+            self, channel_partner_factory, cp_user_factory, organization_factory,
+            org_user_factory, system_group_factory, system_factory, cloud_test_host,
+            sys_group_user_factory, cloud_user_factory, arf, mock_internal_token_auth, user, expected):
+        # Test Setup
+        channel_partner_a = channel_partner_factory(name="[CP] - A")
+
+        organization_b = organization_factory(
+            channel_partner=channel_partner_a,
+            name="[ORG] - B")
+        system_x = system_factory(
+            organization=organization_b,
+            name="[SYS] - X",
+            system_id="7121dc6f-8e56-4649-9db5-3645b92c5ab2")
+
+        # Add users to Channel Partner (1x: Administrator; 1x: Manager; 1x: Reports Viewer)
+        cp_user_admin = cp_user_factory(
+            channel_partner=channel_partner_a,
+            email="example-admin@networkoptix.com",
+            role=ChannelPartnerRoles.ADMINISTRATOR).user
+
+        cp_user_manager = cp_user_factory(
+            channel_partner=channel_partner_a,
+            email="example-manager@networkoptix.com",
+            role=ChannelPartnerRoles.MANAGER).user
+
+        cp_user_viewer = cp_user_factory(
+            channel_partner=channel_partner_a,
+            email="example-report_viewer@networkoptix.com",
+            role=ChannelPartnerRoles.REPORTS_VIEWER).user
+
+        # Setting up API Client
+        client = APIClient(SERVER_NAME=cloud_test_host.hostname)
+        if user == "admin":
+            client.force_authenticate(cp_user_admin)
+            url_args = {"email": cp_user_admin.email}
+        elif user == "manager":
+            client.force_authenticate(cp_user_manager)
+            url_args = {"email": cp_user_manager.email}
+        elif user == "viewer":
+            client.force_authenticate(cp_user_viewer)
+            url_args = {"email": cp_user_viewer.email}
+        else:
+            raise ValueError(f"Invalid user role: {user}")
+
+        # Test Execution
+        url = reverse("v2:user_systems", kwargs=url_args)
+
+        # Make request | Get all systems for the user
+        response = client.get(url)
+        if isinstance(response.data, list):
+            actual = {system_membership['system_id'] for system_membership in response.data}
+        else:
+            actual = set()
+
+        assert actual == expected
+
+
+class TestUserSystems:
+
+    @pytest.fixture(autouse=True)
+    def setup(self, channel_partner_factory, cp_user_factory, organization_factory,
+              org_user_factory, system_group_factory, system_factory, cloud_test_host,
+              sys_group_user_factory, cloud_user_factory, arf, mock_internal_token_auth):
+        # Create a channel partner and an admin user for the channel partner
+        cp = channel_partner_factory()
+        self.cp_user_admin = cp_user_factory(channel_partner=cp)
+
+        # Create an organization and set its access level to POWER_USER
+        org = organization_factory(channel_partner=cp)
+        org.channel_partner_access_level_id = OrganizationRoles.POWER_USER
+        org.save()
+
+        # Create systems for the organization
+        org_sys = system_factory(organization=org)
+
+        # Create a system group and a system within that group
+        group = system_group_factory(organization=org)
+        group_sys = system_factory(organization=org, system_group=group)
+
+        # Create a subgroup and a system within that subgroup
+        sub_group = system_group_factory(organization=org, parent=group)
+        sub_group_sys = system_factory(organization=org, system_group=sub_group)
+
+        # Create users with different roles within the organization
+        self.org_admin = org_user_factory(organization=org)
+        self.org_viewer = org_user_factory(organization=org, role=OrganizationRoles.VIEWER)
+        self.group_user = sys_group_user_factory(
+            organization=org, group=group,
+            role_id=OrganizationRoles.SYSTEM_HEALTH_VIEWER)
+
+        # Initialize the API client with the test host
+        self.client = APIClient(SERVER_NAME=cloud_test_host.hostname)
+
+    def test_system_user_has_all_fields(self, channel_partner_factory, cp_user_factory, arf):
+        url_args = {
+            "email": self.cp_user_admin.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.cp_user_admin.user)
+        response = self.client.get(url)
+        actual_records = response.data
+
+        required_fields = ['system_id', 'systemId', 'vmsRoles', 'membership_type', 'membershipType']
+        results = []
+        for record in actual_records:
+            results.append(not (set(required_fields) - record.keys()))
+        assert all(results)
+
+    def test_cp_admin_ok(self):
+        url_args = {
+            "email": self.cp_user_admin.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.cp_user_admin.user)
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert len(response.data) == 3
+
+    def test_org_admin_ok(self):
+        url_args = {
+            "email": self.org_admin.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.org_admin.user)
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert len(response.data) == 3
+        for sys in response.data:
+            assert cast_uuid(sys['systemId'])
+            assert sys['organizationId'] == str(self.org_admin.organization_id)
+            assert sys['organizationName'] == self.org_admin.organization.name
+
+    def test_group_user_ok(self):
+        url_args = {
+            "email": self.group_user.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.group_user.user)
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert len(response.data) == 2
+
+    def test_org_viewer(self):
+        url_args = {
+            "email": self.org_viewer.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.org_viewer.user)
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert len(response.data) == 3
+
+    def test_incorrect_email(self):
+        url_args = {
+            "email": self.org_viewer.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        self.client.force_authenticate(self.org_admin.user)
+        response = self.client.get(url)
+        assert response.status_code == 403
+
+    def test_unauthenticated(self):
+        url_args = {
+            "email": self.org_viewer.user.email
+        }
+        url = reverse("v2:user_systems", kwargs=url_args)
+        response = self.client.get(url)
+        assert response.status_code == 401
