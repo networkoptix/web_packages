fbc45ad58ba7611f7b9e9cf2c9ef2bba896de665
Nick Hartleb <nhartleb@networkoptix.com>
2024-10-02 14:36:03 -0700

    CLOUD-14808: Updated credit services so that they can only be decreased
    # Problem
    Users were able to increase credits, but this should only be by the partner service when converting licenses from perpetual to SaaS.
    
    # Solution
    In the service-change dialog add a case that sets the limit in the UI to the current quantity.
    
    (cherry picked from commit 3973e5e49c46445460a2223950c0720c9810a766)

--- front_end/libs/features/systems/services/services.types.ts
+++ front_end/libs/features/systems/services/services.types.ts
@@ -1,6 +1,9 @@
+import { ServiceType } from '@services/nx-cloud-api/cloud-services/channel-partners/channel-partners-api.types';
+
 export interface Row {
     id: string;
     type: string;
+    subType: ServiceType;
     displayName: string;
     used: number;
     quantity: number;
