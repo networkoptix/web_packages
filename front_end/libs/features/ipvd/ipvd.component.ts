afe6ac4f6aeb3cbd4cc454e575bdf6e340d50ef1
Chris Czach <czach@networkoptix.com>
2024-10-18 05:26:52 +0300

    CLOUD-12723: Fix ipvd change detection not running on filter
    # Problem
    Change detection sometimes doesn't run after filtering cameras
    # Solution
    Add call to detect changes

--- front_end/libs/features/ipvd/ipvd.component.ts
+++ front_end/libs/features/ipvd/ipvd.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Location } from '@angular/common';
 import {
+    ChangeDetectorRef,
     Component,
     DestroyRef,
     effect,
@@ -263,6 +264,8 @@ export class NxIpvdComponent implements OnInit, OnDestroy {
         }
     }
 
+    cdr = inject(ChangeDetectorRef);
+
     calcRows(isCameraActive?: boolean): void {
         const otherElements = this.isMobile ? OTHER_PAGE_ELEMENTS_MOBILE : OTHER_PAGE_ELEMENTS;
         this.templateRows = this.camerasTable.length
@@ -275,6 +278,7 @@ export class NxIpvdComponent implements OnInit, OnDestroy {
             : isCamera
               ? `1fr ${RIGHT_PANEL_WIDTH}px`
               : '1fr';
+        this.cdr.detectChanges();
     }
 
     menuChanged(isHeight: boolean): void {
