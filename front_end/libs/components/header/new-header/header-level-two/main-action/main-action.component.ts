7a8b404f7dabf4e6288a088c2b6c656315b99227
Nick Hartleb <nhartleb@networkoptix.com>
2024-10-19 03:23:37 +0300

    CLOUD-14943: Fixed missing login button
    Sometimes the user account wasn't completely logged out so the UI state assumed the user was logged in and hide the login button.
    
    Replaced nxConfig account usage with the account from ngrx store.
    
    Refactored the main-action button in the header.
    
    (cherry picked from commit 15072f82fb34fb36ff3c98c74a518c8f7fd5b608)

--- front_end/libs/components/header/new-header/header-level-two/main-action/main-action.component.ts
+++ front_end/libs/components/header/new-header/header-level-two/main-action/main-action.component.ts
@@ -4,20 +4,21 @@ import {
     Component,
     ElementRef,
     EventEmitter,
+    inject,
     Output,
     ViewChild,
 } from '@angular/core';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Store } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { map, startWith } from 'rxjs';
+import { combineLatestWith, debounceTime, map, Observable } from 'rxjs';
 
 import { NxAccountService } from '@services/account.service';
 import { NxHeaderService } from '@services/nx-header.service';
 import { NxScrollMechanicsService } from '@services/scroll-mechanics.service';
 import { icons } from '@static-variables';
 
-@UntilDestroy()
 @Component({
     selector: 'nx-main-action',
     templateUrl: './main-action.component.html',
@@ -26,39 +27,34 @@ import { icons } from '@static-variables';
     imports: [CommonModule, AngularSvgIconModule, TranslateModule],
 })
 export class NxMainActionComponent implements AfterViewInit {
+    icons = icons;
     @Output() widthChange = new EventEmitter<number>();
     @ViewChild('mainAction') mainActionRef: ElementRef<HTMLElement>;
 
-    action$ = this.headerService.currentLocation$.pipe(
-        map(currentLocation => currentLocation?.path === '/account'),
-        map(isAccountPage =>
-            isAccountPage
-                ? 'logout'
-                : this.accountService.account?.is_authenticated
-                  ? 'none'
-                  : 'login',
-        ),
-        startWith('none'),
-    );
-    icons = icons;
+    headerService = inject(NxHeaderService);
+    private accountService = inject(NxAccountService);
+    private scrollMechanicsService = inject(NxScrollMechanicsService);
+    private store = inject(Store);
 
-    constructor(
-        public headerService: NxHeaderService,
-        scrollMechanics: NxScrollMechanicsService,
-        private accountService: NxAccountService,
-    ) {
-        scrollMechanics.windowSizeSubject.pipe(untilDestroyed(this)).subscribe(() => {
+    action$: Observable<'login' | 'logout' | 'none'> = this.headerService.currentLocation$.pipe(
+        combineLatestWith(this.store.select('account')),
+        map(([currentLocation, account]) => {
+            const path = currentLocation?.path;
+            if (path === '/account') {
+                return 'logout';
+            } else {
+                return account.currentUser ? 'none' : 'login';
+            }
+        }),
+    );
+
+    trackWidthSubscription = this.scrollMechanicsService.windowSizeSubject
+        .pipe(combineLatestWith(this.action$), takeUntilDestroyed(), debounceTime(0))
+        .subscribe(() => {
             this.getMainActionWidth();
         });
 
-        this.action$.pipe(untilDestroyed(this)).subscribe(() => {
-            setTimeout(() => {
-                this.getMainActionWidth();
-            }, 0);
-        });
-    }
-
-    getMainActionWidth(): void {
+    private getMainActionWidth(): void {
         let width = 0;
         if (this.mainActionRef?.nativeElement) {
             width = this.mainActionRef.nativeElement.getBoundingClientRect().width;
